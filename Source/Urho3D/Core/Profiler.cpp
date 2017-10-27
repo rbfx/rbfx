64119fa5bdc14dccb356ef5fdadf383e3e1004e3
Rokas Kupstys <rokups@zoho.com>
2017-10-27 16:49:52 +0300

    Core: hide easy_profiler API

--- Source/Urho3D/Core/Profiler.cpp
+++ Source/Urho3D/Core/Profiler.cpp
@@ -23,6 +23,9 @@
 #include "../Precompiled.h"
 #include "../Core/Profiler.h"
 #include "../Core/StringUtils.h"
+#include "../Core/Context.h"
+#if URHO3D_PROFILING
+#   include <easy/profiler.h>
 
 namespace Urho3D
 {
@@ -31,9 +34,6 @@ Profiler::Profiler(Context* context)
     : Object(context)
 {
     SetEnabled(true);
-#if !URHO3D_PROFILING
-    enableEventProfiling_ = false;
-#endif
 }
 
 Profiler::~Profiler()
@@ -42,87 +42,57 @@ Profiler::~Profiler()
 
 void Profiler::SetEnabled(bool enabled)
 {
-#if URHO3D_PROFILING
     ::profiler::setEnabled(enabled);
-#endif
 }
 
 bool Profiler::GetEnabled() const
 {
-#if URHO3D_PROFILING
     return ::profiler::isEnabled();
-#else
-    return false;
-#endif
 }
 
 void Profiler::StartListen(unsigned short port)
 {
-#if URHO3D_PROFILING
     ::profiler::startListen(port);
-#endif
 }
 
 void Profiler::StopListen()
 {
-#if URHO3D_PROFILING
     ::profiler::stopListen();
-#endif
 }
 
 bool Profiler::GetListening() const
 {
-#if URHO3D_PROFILING
     return ::profiler::isListening();
-#else
-    return false;
-#endif
 }
 
 void Profiler::SetEventTracingEnabled(bool enable)
 {
-#if URHO3D_PROFILING
     ::profiler::setEventTracingEnabled(enable);
-#endif
 }
 
 bool Profiler::GetEventTracingEnabled()
 {
-#if URHO3D_PROFILING
     return ::profiler::isEventTracingEnabled();
-#else
-    return false;
-#endif
 }
 
 void Profiler::SetLowPriorityEventTracing(bool isLowPriority)
 {
-#if URHO3D_PROFILING
     ::profiler::setLowPriorityEventTracing(isLowPriority);
-#endif
 }
 
 bool Profiler::GetLowPriorityEventTracing()
 {
-#if URHO3D_PROFILING
     return ::profiler::isLowPriorityEventTracing();
-#else
-    return false;
-#endif
 }
 
 void Profiler::SaveProfilerData(const String& filePath)
 {
-#if URHO3D_PROFILING
     ::profiler::dumpBlocksToFile(filePath.CString());
-#endif
 }
 
 void Profiler::SetEventProfilingEnabled(bool enabled)
 {
-#if URHO3D_PROFILING
     enableEventProfiling_ = enabled;
-#endif
 }
 
 bool Profiler::GetEventProfilingEnabled() const
@@ -130,12 +100,13 @@ bool Profiler::GetEventProfilingEnabled() const
     return enableEventProfiling_;
 }
 
+HashMap<unsigned, ::profiler::BaseBlockDescriptor*> blockDescriptorCache_;
+
 void Profiler::BeginBlock(const char* name, const char* file, int line, unsigned int argb, unsigned char status)
 {
-#if URHO3D_PROFILING
     // Line used as starting hash value for efficiency.
     // This is likely to not play well with hot code reload.
-    unsigned hash = StringHash::Calculate(file, (unsigned)line);
+    unsigned hash = StringHash::Calculate(file, (unsigned)line);    // TODO: calculate hash at compile time
     HashMap<unsigned, ::profiler::BaseBlockDescriptor*>::Iterator it = blockDescriptorCache_.Find(hash);
     const ::profiler::BaseBlockDescriptor* desc = 0;
     if (it == blockDescriptorCache_.End())
@@ -147,14 +118,29 @@ void Profiler::BeginBlock(const char* name, const char* file, int line, unsigned
     else
         desc = it->second_;
     ::profiler::beginNonScopedBlock(desc, name);
-#endif
 }
 
 void Profiler::EndBlock()
 {
-#if URHO3D_PROFILING
     ::profiler::endBlock();
-#endif
+}
+
+void Profiler::RegisterCurentThread(const char* name)
+{
+
+}
+
+ProfilerBlock::ProfilerBlock(const char* name, const char* file, int line, unsigned int argb,
+    unsigned char status)
+{
+    Profiler::BeginBlock(name, file, line, argb, status);
+}
+
+ProfilerBlock::~ProfilerBlock()
+{
+    Profiler::EndBlock();
 }
 
 }
+
+#endif
