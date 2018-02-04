8ce19e8fe5e3973f5a6c04a3d37572915093f2ea
Rokas Kupstys <rokups@zoho.com>
2018-02-04 18:06:52 +0200

    CMake: Fix symlinking and packaging resources.

--- CMake/Modules/UrhoPackaging.cmake
+++ CMake/Modules/UrhoPackaging.cmake
@@ -21,11 +21,10 @@
 #
 
 # Resource packaging
-file (GLOB RESOURCE_DIRS ${Urho3D_SOURCE_DIR}/bin/*Data)
-file (GLOB AUTOLOAD_RESOURCE_DIRS ${Urho3D_SOURCE_DIR}/bin/Autoload/*)
-file (GLOB PROJECT_RESOURCE_DIRS ${CMAKE_SOURCE_DIR}/bin/*Data)
-list (APPEND RESOURCE_DIRS ${AUTOLOAD_RESOURCE_DIRS} ${PROJECT_RESOURCE_DIRS})
+file (GLOB RESOURCE_DIRS ${Urho3D_SOURCE_DIR}/bin/*Data ${CMAKE_SOURCE_DIR}/bin/*Data)
+file (GLOB AUTOLOAD_DIRS ${Urho3D_SOURCE_DIR}/bin/Autoload/* ${CMAKE_SOURCE_DIR}/bin/Autoload/*)
 file (MAKE_DIRECTORY ${CMAKE_BINARY_DIR}/bin/Cache)
+file (MAKE_DIRECTORY ${CMAKE_BINARY_DIR}/bin/Autoload)
 if (URHO3D_PACKAGING)
     if (CMAKE_CROSSCOMPILING)
         include (ExternalProject)
@@ -48,33 +47,55 @@ if (URHO3D_PACKAGING)
         message(FATAL_ERROR "CMake misconfiguration")
     endif ()
     if (NOT ANDROID)
-        foreach (DIR ${RESOURCE_DIRS})
-            get_filename_component (NAME ${DIR} NAME)
-            set (RESOURCE_PATHNAME ${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/${NAME}.pak)
-            list (APPEND PKG_COMMANDS COMMAND "${PACKAGE_TOOL}" "${DIR}" "${RESOURCE_PATHNAME}" -q -c)
-        endforeach ()
-    endif ()
-    add_custom_target(PackageResources ${PKG_COMMANDS})
-    if (CMAKE_CROSSCOMPILING)
-        add_dependencies(PackageResources Urho3D-Native)
-    endif ()
-    if (EMSCRIPTEN)
-        if (NOT EXISTS ${CMAKE_BINARY_DIR}/Source/pak-loader.js)
-            file (WRITE ${CMAKE_BINARY_DIR}/Source/pak-loader.js "var Module;if(typeof Module==='undefined')Module=eval('(function(){try{return Module||{}}catch(e){return{}}})()');var s=document.createElement('script');s.src='${CMAKE_PROJECT_NAME}.js';document.body.appendChild(s);Module['preRun'].push(function(){Module['addRunDependency']('${CMAKE_PROJECT_NAME}.js.loader')});s.onload=function(){Module['removeRunDependency']('${CMAKE_PROJECT_NAME}.js.loader')};")
+        add_custom_target(PackageResources)
+        if (CMAKE_CROSSCOMPILING)
+            add_dependencies(PackageResources Urho3D-Native)
         endif ()
-        foreach (DIR ${RESOURCE_DIRS})
-            get_filename_component (NAME ${DIR} NAME)
-            list (APPEND PAK_NAMES ${NAME}.pak)
+
+        foreach (DIR ${RESOURCE_DIRS} ${CMAKE_BINARY_DIR}/bin/Cache)
+            if (EXISTS ${DIR})
+                file (GLOB RES_FILES ${DIR}/*)
+                if (RES_FILES)
+                    get_filename_component (NAME ${DIR} NAME)
+                    set (RESOURCE_PATHNAME ${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/${NAME}.pak)
+                    add_custom_command(TARGET PackageResources POST_BUILD
+                        COMMAND "${PACKAGE_TOOL}" "${DIR}" "${RESOURCE_PATHNAME}" -q -c
+                        COMMENT "Packaging ${NAME}.pak"
+                    )
+                endif ()
+            endif ()
         endforeach ()
-        if (CMAKE_BUILD_TYPE STREQUAL Debug AND EMSCRIPTEN_EMCC_VERSION VERSION_GREATER 1.32.2)
-            set (SEPARATE_METADATA --separate-metadata)
+
+        foreach (DIR ${AUTOLOAD_DIRS})
+            file (GLOB RES_FILES ${DIR} *)
+            if (RES_FILES)
+                get_filename_component (NAME ${DIR} NAME)
+                set (RESOURCE_PATHNAME ${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/Autoload/${NAME}.pak)
+                add_custom_command(TARGET PackageResources POST_BUILD
+                    COMMAND "${PACKAGE_TOOL}" "${DIR}" "${RESOURCE_PATHNAME}" -q -c
+                    COMMENT "Packaging Autoload/${NAME}.pak"
+                )
+            endif ()
+        endforeach ()
+
+        if (EMSCRIPTEN)
+            if (NOT EXISTS ${CMAKE_BINARY_DIR}/Source/pak-loader.js)
+                file (WRITE ${CMAKE_BINARY_DIR}/Source/pak-loader.js "var Module;if(typeof Module==='undefined')Module=eval('(function(){try{return Module||{}}catch(e){return{}}})()');var s=document.createElement('script');s.src='${CMAKE_PROJECT_NAME}.js';document.body.appendChild(s);Module['preRun'].push(function(){Module['addRunDependency']('${CMAKE_PROJECT_NAME}.js.loader')});s.onload=function(){Module['removeRunDependency']('${CMAKE_PROJECT_NAME}.js.loader')};")
+            endif ()
+            foreach (DIR ${RESOURCE_DIRS})
+                get_filename_component (NAME ${DIR} NAME)
+                list (APPEND PAK_NAMES ${NAME}.pak)
+            endforeach ()
+            if (CMAKE_BUILD_TYPE STREQUAL Debug AND EMSCRIPTEN_EMCC_VERSION VERSION_GREATER 1.32.2)
+                set (SEPARATE_METADATA --separate-metadata)
+            endif ()
+            set (SHARED_RESOURCE_JS ${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/${CMAKE_PROJECT_NAME}.js)
+            add_custom_target (PackageResourcesWeb
+                COMMAND ${EMPACKAGER} ${SHARED_RESOURCE_JS}.data --preload ${PAK_NAMES} --js-output=${SHARED_RESOURCE_JS} --use-preload-cache ${SEPARATE_METADATA}
+                WORKING_DIRECTORY ${CMAKE_RUNTIME_OUTPUT_DIRECTORY}
+                COMMENT "Generating shared data file"
+            )
+            add_dependencies(PackageResourcesWeb PackageResources)
         endif ()
-        set (SHARED_RESOURCE_JS ${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/${CMAKE_PROJECT_NAME}.js)
-        add_custom_target (PackageResourcesWeb
-            COMMAND ${EMPACKAGER} ${SHARED_RESOURCE_JS}.data --preload ${PAK_NAMES} --js-output=${SHARED_RESOURCE_JS} --use-preload-cache ${SEPARATE_METADATA}
-            WORKING_DIRECTORY ${CMAKE_RUNTIME_OUTPUT_DIRECTORY}
-            COMMENT "Generating shared data file"
-        )
-        add_dependencies(PackageResourcesWeb PackageResources)
     endif ()
 endif ()
