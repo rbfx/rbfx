e9d2f5ef8f58f55b61ae00d6782525ec14c0fbaa
Rokas Kupstys <rokups@zoho.com>
2018-03-18 14:53:34 +0200

    Graphics: Remove declarations of functions that do not exist.

--- Source/Urho3D/Graphics/Graphics.h
+++ Source/Urho3D/Graphics/Graphics.h
@@ -573,16 +573,6 @@ public:
 
     /// Get the SDL_Window as a void* to avoid having to include the graphics implementation
     void* GetSDLWindow() { return window_; }
-    /// Returns the index of the display containing the center of the window on success or a negative error code on failure.
-    int GetCurrentMonitor();
-    /// Returns number of monitors currently connected.
-    int GetNumMonitors();
-    /// Returns true if window is maximized or runs in full screen mode.
-    bool GetMaximized();
-    /// Returns resolution of monitor. monitorId should be less or equal to result of GetNumMonitors().
-    IntVector2 GetMonitorResolution(int monitorId) const;
-    /// Raises window if it was minimized.
-    void RaiseWindow();
 
 private:
     /// Create the application window.
