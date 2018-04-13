f3228ff84bb005b4ff0f82da4b366e2edbfd73cb
TrevorCash <cashtrevor@msn.com>
2018-04-13 12:08:13 -0600

    wip

--- Source/Urho3D/Scene/UnknownComponent.h
+++ Source/Urho3D/Scene/UnknownComponent.h
@@ -27,24 +27,26 @@
 namespace Urho3D
 {
 
-/// Placeholder for allowing unregistered components to be loaded & saved along with scenes.
+/// component that stored data nessecary to recreate another component.
 class URHO3D_API UnknownComponent : public Component
 {
+	URHO3D_OBJECT(UnknownComponent, Component)
 public:
+
     /// Construct.
     explicit UnknownComponent(Context* context);
 
     /// Register object factory.
     static void RegisterObject(Context* context);
-
+   
+    /// Initialize the type name. Called by Node when loading.
+    void SetStoredTypeName(const String& typeName);
+    /// Initialize the type hash only when type name not known. Called by Node when loading.
+    void SetStoredType(StringHash typeHash);
     /// Return type of the stored component.
-    StringHash GetType() const override { return typeHash_; }
-
+    StringHash GetStoredType() const  { return typeHashStored_; }
     /// Return type name of the stored component.
-    const String& GetTypeName() const override { return typeName_; }
-
-    /// Return attribute descriptions, or null if none defined.
-    const Vector<AttributeInfo>* GetAttributes() const override { return &xmlAttributeInfos_; }
+    const String& GetStoredTypeName() const  { return typeNameStored_; }
 
     /// Load from binary data. Return true if successful.
     bool Load(Deserializer& source) override;
@@ -59,47 +61,11 @@ public:
     /// Save as JSON data. Return true if successful.
     bool SaveJSON(JSONValue& dest) const override;
 
-    /// Initialize the type name. Called by Node when loading.
-    void SetTypeName(const String& typeName);
-    /// Initialize the type hash only when type name not known. Called by Node when loading.
-    void SetType(StringHash typeHash);
-
-    /// Return the XML format attributes. Empty when loaded with binary serialization.
-    const Vector<String>& GetXMLAttributes() const { return xmlAttributes_; }
-
-    /// Return the binary attributes. Empty when loaded with XML serialization.
-    const PODVector<unsigned char>& GetBinaryAttributes() const { return binaryAttributes_; }
-
-    /// Return whether was loaded using XML data.
-    bool GetUseXML() const { return useXML_; }
-
-    /// Return static type.
-    static Urho3D::StringHash GetTypeStatic()
-    {
-        static const StringHash typeStatic("UnknownComponent");
-        return typeStatic;
-    }
-    /// Return static type name.
-    static const Urho3D::String& GetTypeNameStatic()
-    {
-        static const String typeNameStatic("UnknownComponent");
-        return typeNameStatic;
-    }
-
 private:
     /// Type of stored component.
-    StringHash typeHash_;
+    StringHash typeHashStored_;
     /// Type name of the stored component.
-    String typeName_;
-    /// XML format attribute infos.
-    Vector<AttributeInfo> xmlAttributeInfos_;
-    /// XML format attribute data (as strings)
-    Vector<String> xmlAttributes_;
-    /// Binary attributes.
-    PODVector<unsigned char> binaryAttributes_;
-    /// Flag of whether was loaded using XML/JSON data.
-    bool useXML_;
-
+    String typeNameStored_;
 };
 
 }
