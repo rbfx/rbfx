94fbafeae7633eee75b6cf28a12a0a122ac94b57
Rokas Kupstys <rokups@zoho.com>
2018-06-06 18:34:57 +0300

    CSharp: New IntRect API.

--- Source/Urho3D/CSharp/Math/Rect.cs
+++ Source/Urho3D/CSharp/Math/Rect.cs
@@ -69,5 +69,27 @@ namespace Urho3D
 		}
 
 		public static readonly IntRect Zero = new IntRect(0, 0, 0, 0);
+
+	    public IntRect(IntVector2 min, IntVector2 max)
+	    {
+	        Left = min.X;
+	        Top = min.Y;
+	        Right = max.X;
+	        Bottom = max.Y;
+	    }
+
+	    public int Width => Right - Left;
+	    public int Height => Bottom - Top;
+	    public IntVector2 Size => new IntVector2(Width, Height);
+
+	    public static bool operator !=(IntRect lhs, IntRect rhs)
+	    {
+	        return lhs.Top != rhs.Top || lhs.Right != rhs.Right || lhs.Bottom != rhs.Bottom || lhs.Left != rhs.Left;
+	    }
+
+	    public static bool operator ==(IntRect lhs, IntRect rhs)
+	    {
+	        return !(lhs != rhs);
+	    }
 	}
 }
