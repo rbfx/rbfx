21b0dca5f65d59bb222da817970de8225b3db041
Rokas Kupstys <rokups@zoho.com>
2018-06-10 15:30:31 +0300

    CSharp: Yield all attributes, not just first one.

--- Source/Urho3D/CSharp/Script/Reflection.cs
+++ Source/Urho3D/CSharp/Script/Reflection.cs
@@ -28,12 +28,15 @@ namespace Urho3D.CSharp
 {
     public static class ReflectionExtensions
     {
-        public static IEnumerable<Tuple<Type, T>> GetTypesWithAttribute<T>(this Assembly assembly) {
+        public static IEnumerable<Tuple<Type, T>> GetTypesWithAttribute<T>(this Assembly assembly)
+        {
             foreach(var type in assembly.GetTypes())
             {
                 var attributes = type.GetCustomAttributes(typeof(T), true);
-                if (attributes.Length > 0)
-                    yield return new Tuple<Type, T>(type, (T)attributes[0]);
+                foreach (var attribute in attributes)
+                {
+                    yield return new Tuple<Type, T>(type, (T) attribute);
+                }
             }
         }
     }
