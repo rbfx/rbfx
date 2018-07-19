1be282470cf1216ff358ff5b687e7ce48c16f5a4
Rokas Kupstys <rokups@zoho.com>
2018-07-19 12:24:40 +0300

    CMake: Remove deploy_qt_dlls() macro because it is no longer necessary.

--- CMake/Modules/UrhoCommon.cmake
+++ CMake/Modules/UrhoCommon.cmake
@@ -222,31 +222,6 @@ macro (add_sample TARGET)
     endif ()
 endmacro ()
 
-# Macro deploys Qt dlls to folder where target executable is located.
-# Macro arguments:
-#  target - name of target which links to Qt.
-macro(deploy_qt_dlls target)
-    if (WIN32)
-        if (TARGET ${target})
-            # Find Qt dir
-            foreach (dir ${CMAKE_PREFIX_PATH})
-                if (EXISTS ${dir}/bin/windeployqt.exe)
-                    set(windeployqt "${dir}/bin/windeployqt.exe")
-                    break()
-                endif ()
-            endforeach ()
-            if (windeployqt)
-                add_custom_command(
-                    TARGET ${target}
-                    POST_BUILD
-                    COMMAND "${windeployqt}" --no-quick-import --no-translations --no-system-d3d-compiler --no-compiler-runtime --no-webkit2 --no-angle --no-opengl-sw $<TARGET_FILE_DIR:${target}>
-                    VERBATIM
-                )
-            endif ()
-        endif ()
-    endif ()
-endmacro()
-
 # Groups sources into subfolders.
 macro(group_sources)
     file (GLOB_RECURSE children LIST_DIRECTORIES true RELATIVE ${CMAKE_CURRENT_SOURCE_DIR} ${CMAKE_CURRENT_SOURCE_DIR}/**)
