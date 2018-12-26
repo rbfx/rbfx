2c248dc86d28a53523d1914e7032a020bc7cd9f7
Rokas Kupstys <rokups@zoho.com>
2018-12-26 16:47:32 +0200

    Scene: Remove type name bruteforcing from UnknownComponent.

--- Source/Urho3D/Scene/UnknownComponent.cpp
+++ Source/Urho3D/Scene/UnknownComponent.cpp
@@ -35,62 +35,6 @@
 namespace Urho3D
 {
 
-static HashMap<StringHash, String> unknownTypeToName;
-static String letters("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ");
-
-static String GenerateNameFromType(StringHash typeHash)
-{
-    if (unknownTypeToName.Contains(typeHash))
-        return unknownTypeToName[typeHash];
-
-    String test;
-
-#if URHO3D_HASH_DEBUG
-    StringHashRegister* registry = StringHash::GetGlobalStringHashRegister();
-    if (registry->Contains(typeHash))
-    {
-        test = registry->GetString(typeHash);
-        unknownTypeToName[typeHash] = test;
-        return test;
-    }
-#endif
-
-    // Begin brute-force search
-    unsigned numLetters = letters.Length();
-    unsigned combinations = numLetters;
-    bool found = false;
-
-    for (unsigned i = 1; i < 6; ++i)
-    {
-        test.Resize(i);
-
-        for (unsigned j = 0; j < combinations; ++j)
-        {
-            unsigned current = j;
-
-            for (unsigned k = 0; k < i; ++k)
-            {
-                test[k] = letters[current % numLetters];
-                current /= numLetters;
-            }
-
-            if (StringHash(test) == typeHash)
-            {
-                found = true;
-                break;
-            }
-        }
-
-        if (found)
-            break;
-
-        combinations *= numLetters;
-    }
-
-    unknownTypeToName[typeHash] = test;
-    return test;
-}
-
 UnknownComponent::UnknownComponent(Context* context) :
     Component(context),
     useXML_(false)
@@ -257,7 +201,15 @@ void UnknownComponent::SetTypeName(const String& typeName)
 
 void UnknownComponent::SetType(StringHash typeHash)
 {
-    typeName_ = GenerateNameFromType(typeHash);
+#if URHO3D_HASH_DEBUG
+    StringHashRegister* registry = StringHash::GetGlobalStringHashRegister();
+    if (registry->Contains(typeHash))
+        typeName_ = registry->GetString(typeHash);
+#endif
+
+    if (typeName_.Empty())
+        typeName_ = typeHash.ToString();
+
     typeHash_ = typeHash;
 }
 
