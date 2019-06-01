df302e922493716b01f05d76f3557cbd6bc5958b
Eugene Kozlov <nekoeuge@gmail.com>
2019-06-01 12:50:14 +0300

    Add Color::ChannelMask and related functions and constants.

--- Source/Urho3D/Math/Color.h
+++ Source/Urho3D/Math/Color.h
@@ -33,6 +33,23 @@ namespace Urho3D
 class URHO3D_API Color
 {
 public:
+    /// Mask describing color channels.
+    struct ChannelMask
+    {
+        /// Red channel mask. If zero, red channel is set to 0.
+        unsigned red_;
+        /// Green channel mask. If zero, green channel is set to 0.
+        unsigned green_;
+        /// Blue channel mask. If zero, blue channel is set to 0.
+        unsigned blue_;
+        /// Alpha channel mask. If zero, alpha channel is set to 1.
+        unsigned alpha_;
+    };
+    /// Mask for 0xAABBGGRR layout.
+    static const ChannelMask ABGR;
+    /// Mask for 0xAARRGGBB layout.
+    static const ChannelMask ARGB;
+
     /// Construct with default values (opaque white.)
     Color() noexcept :
         r_(1.0f),
@@ -81,6 +98,9 @@ public:
     {
     }
 
+    /// Construct from 32-bit integer. Default format is 0xAABBGGRR.
+    explicit Color(unsigned color, ChannelMask mask = ABGR) { FromUIntMask(color, mask); }
+
     /// Assign from another color.
     Color& operator =(const Color& rhs) noexcept = default;
 
@@ -117,12 +137,16 @@ public:
 
     /// Return color packed to a 32-bit integer, with R component in the lowest 8 bits. Components are clamped to [0, 1] range.
     unsigned ToUInt() const;
+    /// Return color packed to a 32-bit integer with arbitrary channel mask. Components are clamped to [0, 1] range.
+    unsigned ToUIntMask(ChannelMask mask) const;
     /// Return HSL color-space representation as a Vector3; the RGB values are clipped before conversion but not changed in the process.
     Vector3 ToHSL() const;
     /// Return HSV color-space representation as a Vector3; the RGB values are clipped before conversion but not changed in the process.
     Vector3 ToHSV() const;
     /// Set RGBA values from packed 32-bit integer, with R component in the lowest 8 bits (format 0xAABBGGRR).
     void FromUInt(unsigned color);
+    /// Set RGBA values from packed 32-bit integer with arbitrary channel mask.
+    void FromUIntMask(unsigned color, ChannelMask mask);
     /// Set RGBA values from specified HSL values and alpha.
     void FromHSL(float h, float s, float l, float a = 1.0f);
     /// Set RGBA values from specified HSV values and alpha.
@@ -187,8 +211,8 @@ public:
     ea::string ToString() const;
 
     /// Return color packed to a 32-bit integer, with B component in the lowest 8 bits. Components are clamped to [0, 1] range.
-    unsigned ToUIntArgb() const;
-    
+    unsigned ToUIntArgb() const { return ToUIntMask(ARGB); }
+
     /// Return hash value for HashSet & HashMap.
     unsigned ToHash() const { return ToUInt(); }
 
