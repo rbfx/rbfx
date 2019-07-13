ec8cddd4cccf5ba6d81bd7a59220a890478b8309
Rokas Kupstys <rokups@zoho.com>
2019-07-13 09:21:23 +0300

    CI: Make web builds script work in downstream projects when engine is not a main project.

--- .ci/emscripten.sh
+++ .ci/emscripten.sh
@@ -5,9 +5,10 @@ apt-get update
 apt-get -y install uuid-dev
 
 SOURCE_DIR=$(pwd)
+CI_DIR=$(dirname "$(readlink -f "$0")")
 mkdir -p cmake-build
 mkdir -p SDK
 
 cd cmake-build
-cmake -DURHO3D_SAMPLES=ON -DCMAKE_BUILD_TYPE=MinSizeRel -DCMAKE_INSTALL_PREFIX=$SOURCE_DIR/SDK -DCMAKE_TOOLCHAIN_FILE=CMake/Toolchains/Emscripten.cmake $SOURCE_DIR
+cmake -DURHO3D_SAMPLES=ON -DCMAKE_BUILD_TYPE=MinSizeRel -DCMAKE_INSTALL_PREFIX=$SOURCE_DIR/SDK -DCMAKE_TOOLCHAIN_FILE=$CI_DIR/../CMake/Toolchains/Emscripten.cmake $SOURCE_DIR
 cmake --build . --target 11_Physics -- -j $(nproc --all)
