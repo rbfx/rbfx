12de857f2c58ff368d8b660469fae2dc881ee32c
Eugene Kozlov <nekoeuge@gmail.com>
2019-08-04 01:09:07 +0300

    Extract archive serialization interface into Object. Minor changes.

--- Source/Urho3D/Scene/Serializable.h
+++ Source/Urho3D/Scene/Serializable.h
@@ -63,7 +63,7 @@ public:
     virtual const ea::vector<AttributeInfo>* GetNetworkAttributes() const;
 
     /// Serialize from/to archive. Return true if successful.
-    virtual bool Serialize(Archive& archive);
+    bool Serialize(Archive& archive) override;
     /// Serialize content from/to archive. Return true if successful.
     bool Serialize(Archive& archive, ArchiveBlock& block);
 
