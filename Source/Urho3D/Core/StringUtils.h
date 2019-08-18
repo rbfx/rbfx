92b952e38d88e04f2ea94f165f4cfa76e0555d5a
Rokas Kupstys <rokups@zoho.com>
2019-08-18 11:51:29 +0300

    CMake: Warnings triage.

--- Source/Urho3D/Core/StringUtils.h
+++ Source/Urho3D/Core/StringUtils.h
@@ -23,7 +23,13 @@
 #pragma once
 
 #include "../Core/Variant.h"
+#if _MSC_VER
+#   pragma warning(push, 0)
+#endif
 #include <fmt/format.h>
+#if _MSC_VER
+#   pragma warning(pop)
+#endif
 
 namespace Urho3D
 {
