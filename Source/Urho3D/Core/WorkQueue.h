a484431d9e14c084c29ab009bfa4b20abdd87d68
Rokas Kupstys <rokups@zoho.com>
2019-09-02 16:59:33 +0300

    Core: Creating work item with a lambda function returns SharedPtr<> instead of raw pointer avoiding theoretical race condition.

--- Source/Urho3D/Core/WorkQueue.h
+++ Source/Urho3D/Core/WorkQueue.h
@@ -86,7 +86,7 @@ public:
     /// Add a work item and resume worker threads.
     void AddWorkItem(const SharedPtr<WorkItem>& item);
     /// Add a work item and resume worker threads.
-    WorkItem* AddWorkItem(std::function<void()> workFunction, unsigned priority = 0);
+    SharedPtr<WorkItem> AddWorkItem(std::function<void()> workFunction, unsigned priority = 0);
     /// Remove a work item before it has started executing. Return true if successfully removed.
     bool RemoveWorkItem(SharedPtr<WorkItem> item);
     /// Remove a number of work items before they have started executing. Return the number of items successfully removed.
