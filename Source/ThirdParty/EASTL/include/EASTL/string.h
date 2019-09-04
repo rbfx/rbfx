cddef6ebd6f15e5d4ce3f63146070b26c4fb5684
Rokas Kupstys <rokups@zoho.com>
2019-09-04 09:45:13 +0300

    ThirdParty: Fix starts_with and ends_with in string when called with a char parameter.

--- Source/ThirdParty/EASTL/include/EASTL/string.h
+++ Source/ThirdParty/EASTL/include/EASTL/string.h
@@ -824,7 +824,7 @@ namespace eastl
 		bool starts_with(value_type c, bool caseSensitive=true) const
 		{
 			value_type str[2]{c, 0};
-			return starts_with(view_type(str, 2), caseSensitive);
+			return starts_with(view_type(str, 1), caseSensitive);
 		}
 
 		bool starts_with(const value_type* substring, bool caseSensitive=true) const
@@ -850,7 +850,7 @@ namespace eastl
 		bool ends_with(value_type c, bool caseSensitive=true) const
 		{
 			value_type str[2]{c, 0};
-			return ends_with(view_type(str, 2), caseSensitive);
+			return ends_with(view_type(str, 1), caseSensitive);
 		}
 
 		bool ends_with(const value_type* substring, bool caseSensitive=true) const
