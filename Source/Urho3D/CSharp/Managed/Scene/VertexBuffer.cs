a53ef6ca8fcfaec4293073df45b6c59ed2882c83
Moustafa Khalil <mostafa90@gmail.com>
2019-11-24 23:18:00 +0400

    wrapping VertexBuffer, additional functions to make things easies in C# API, SetSize, SetData =>float,short

---
+++ Source/Urho3D/CSharp/Managed/Scene/VertexBuffer.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Urho3DNet
+{
+    public partial class VertexBuffer
+    {
+        public bool SetSize(uint vertexCount, VertexMask mask, bool isDynamic)
+        {
+            return SetSize(vertexCount, (uint)mask, isDynamic);
+        }
+
+        public bool SetData(float[] vertexData)
+        {
+            //// Initialize unmanaged memory to hold the array.
+            int vertexDatasize = Marshal.SizeOf(vertexData[0]) * vertexData.Length;
+            IntPtr vertexDatapnt = Marshal.AllocHGlobal(vertexDatasize);
+            return SetData(vertexDatapnt);
+        }
+
+        public bool SetData(short[] indexData)
+        {
+            //// Initialize unmanaged memory to hold the array.
+            int indexDatasize = Marshal.SizeOf(indexData[0]) * indexData.Length;
+            IntPtr indexDatapnt = Marshal.AllocHGlobal(indexDatasize);
+            return SetData(indexDatapnt);
+        }
+
+       
+    }
+}
