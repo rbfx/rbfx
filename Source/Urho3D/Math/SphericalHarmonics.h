c9427a44f87e124cdd974d6a6f473640d546db58
Eugene Kozlov <nekoeuge@gmail.com>
2019-12-23 01:42:13 +0300

    Optimize SH for GPU evaluation.

--- Source/Urho3D/Math/SphericalHarmonics.h
+++ Source/Urho3D/Math/SphericalHarmonics.h
@@ -24,6 +24,7 @@
 
 #include "../Math/Color.h"
 #include "../Math/Vector3.h"
+#include "../Math/Vector4.h"
 
 namespace Urho3D
 {
@@ -81,7 +82,7 @@ struct SphericalHarmonicsColor9
         static const float c1 = 2.0f / 3.0f;
         static const float c2 = 0.25f;
 
-        SphericalHarmonics9 sh{ dir };
+        SphericalHarmonics9 sh{ dir.Normalized() };
         sh.values_[0] *= c0;
         sh.values_[1] *= c1;
         sh.values_[2] *= c1;
@@ -128,6 +129,83 @@ struct SphericalHarmonicsColor9
 
     /// Zero harmonics.
     static const SphericalHarmonicsColor9 ZERO;
+};
+
+/// Spherical harmonics, optimized for dot products.
+struct SphericalHarmonicsDot9
+{
+    /// Construct default.
+    SphericalHarmonicsDot9() = default;
+
+    /// Construct from spherical harmonics.
+    explicit SphericalHarmonicsDot9(SphericalHarmonicsColor9 sh)
+    {
+        // Apply weights
+        static const float c0 = 1.0f;
+        static const float c1 = 2.0f / 3.0f;
+        static const float c2 = 0.25f;
+
+        sh.values_[0] *= c0 * 0.282095f;
+        sh.values_[1] *= c1 * 0.488603f; // y
+        sh.values_[2] *= c1 * 0.488603f; // z
+        sh.values_[3] *= c1 * 0.488603f; // x
+        sh.values_[4] *= c2 * 1.092548f; // x * y
+        sh.values_[5] *= c2 * 1.092548f; // y * z
+        sh.values_[6] *= c2 * 0.315392f; // 3 * z * z - 1
+        sh.values_[7] *= c2 * 1.092548f; // x * z
+        sh.values_[8] *= c2 * 0.546274f; // x * x - y * y
+
+        sh.values_[0] = sh.values_[0] - sh.values_[6];
+        sh.values_[6] *= 3.0f;
+
+        // (Nx, Ny, Nz, 1)
+        Ar_ = Vector4(sh.values_[3].x_, sh.values_[1].x_, sh.values_[2].x_, sh.values_[0].x_);
+        Ag_ = Vector4(sh.values_[3].y_, sh.values_[1].y_, sh.values_[2].y_, sh.values_[0].y_);
+        Ab_ = Vector4(sh.values_[3].z_, sh.values_[1].z_, sh.values_[2].z_, sh.values_[0].z_);
+
+        // (Nx*Ny, Ny*Nz, Nz*Nz, Nz*Nx)
+        Br_ = Vector4(sh.values_[4].x_, sh.values_[5].x_, sh.values_[6].x_, sh.values_[7].x_);
+        Bg_ = Vector4(sh.values_[4].y_, sh.values_[5].y_, sh.values_[6].y_, sh.values_[7].y_);
+        Bb_ = Vector4(sh.values_[4].z_, sh.values_[5].z_, sh.values_[6].z_, sh.values_[7].z_);
+
+        // Nx*Nx - Ny*Ny
+        C_ = sh.values_[8];
+    }
+
+    /// Evaluate.
+    Vector3 Evaluate(const Vector3& dir) const
+    {
+        const Vector4 l01{ dir, 1.0f };
+        const Vector4 l2{ dir.x_ * dir.y_, dir.y_ * dir.z_, dir.z_ * dir.z_, dir.z_ * dir.x_ };
+        const float l2xxyy = dir.x_ * dir.x_ - dir.y_ * dir.y_;
+
+        Vector3 result;
+        result.x_ = Ar_.DotProduct(l01);
+        result.y_ = Ag_.DotProduct(l01);
+        result.z_ = Ab_.DotProduct(l01);
+        result.x_ += Br_.DotProduct(l2);
+        result.y_ += Bg_.DotProduct(l2);
+        result.z_ += Bb_.DotProduct(l2);
+        result += C_ * l2xxyy;
+        return result;
+    }
+
+    /// Dot product with (Nx, Ny, Nz, 1), red channel.
+    Vector4 Ar_;
+    /// Dot product with (Nx, Ny, Nz, 1), green channel.
+    Vector4 Ag_;
+    /// Dot product with (Nx, Ny, Nz, 1), blue channel.
+    Vector4 Ab_;
+    /// Dot product with (Nx*Ny, Ny*Nz, Nz*Nz, Nz*Nx), red channel.
+    Vector4 Br_;
+    /// Dot product with (Nx*Ny, Ny*Nz, Nz*Nz, Nz*Nx), green channel.
+    Vector4 Bg_;
+    /// Dot product with (Nx*Ny, Ny*Nz, Nz*Nz, Nz*Nx), blue channel.
+    Vector4 Bb_;
+    /// Color, multiplied by (Nx*Nx - Ny*Ny).
+    Vector3 C_;
+    /// Padding. Always 1.0f;
+    float padding_{ 1.0f };
 };
 
 }
