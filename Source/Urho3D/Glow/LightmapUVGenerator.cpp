9ef9b9b0facd3224fd39c2e3e64581ce99cdada8
Eugene Kozlov <nekoeuge@gmail.com>
2019-12-28 03:27:50 +0300

    Replace thekla with xatlas. Minor fixes and changes in stitching.

--- Source/Urho3D/Glow/LightmapUVGenerator.cpp
+++ Source/Urho3D/Glow/LightmapUVGenerator.cpp
@@ -22,80 +22,50 @@
 
 #include "../Glow/LightmapUVGenerator.h"
 
-#include <thekla/thekla_atlas.h>
+#include <xatlas.h>
 
 namespace Urho3D
 {
-
-namespace
-{
-
-struct LightmapVertexMapping
-{
-    unsigned geometryIndex_{};
-    unsigned lod_{};
-    unsigned index_{};
-};
-
-}
 
 const ea::string LightmapUVGenerationSettings::LightmapSizeKey{ "LightmapSize" };
 const ea::string LightmapUVGenerationSettings::LightmapDensityKey{ "LightmapDensity" };
 
 bool GenerateLightmapUV(ModelView& modelView, const LightmapUVGenerationSettings& settings)
 {
-    const auto& sourceGeometries = modelView.GetGeometries();
-
-    ea::vector<LightmapVertexMapping> atlasMapping;
-    ea::vector<Thekla::Atlas_Input_Vertex> atlasVertices;
-    ea::vector<Thekla::Atlas_Input_Face> atlasFaces;
+    // Create atlas
+    const auto releaseAtlas = [](xatlas::Atlas* atlas)
+    {
+        xatlas::Destroy(atlas);
+    };
+    ea::unique_ptr<xatlas::Atlas, decltype(releaseAtlas)> atlas(xatlas::Create(), releaseAtlas);
 
     // Fill input mesh
     // TODO: Do something more clever about connectivity
+    auto& sourceGeometries = modelView.GetGeometries();
+    ea::vector<ea::pair<unsigned, unsigned>> meshToGeometryLodMapping;
     unsigned geometryIndex = 0;
     for (const GeometryView& geometryView : sourceGeometries)
     {
         unsigned lodIndex = 0;
         for (const GeometryLODView& geometryLodView : geometryView.lods_)
         {
-            const unsigned vertexStart = atlasVertices.size();
-
-            unsigned vertexIndex = 0;
-            for (const ModelVertex& vertex : geometryLodView.vertices_)
+            if (!geometryLodView.vertices_.empty())
             {
-                Thekla::Atlas_Input_Vertex atlasVertex;
+                xatlas::MeshDecl meshDecl;
+                meshDecl.vertexCount = geometryLodView.vertices_.size();
+                meshDecl.vertexPositionData = &geometryLodView.vertices_[0].position_;
+                meshDecl.vertexPositionStride = sizeof(ModelVertex);
+                meshDecl.vertexNormalData = &geometryLodView.vertices_[0].normal_;
+                meshDecl.vertexNormalStride = sizeof(ModelVertex);
+                meshDecl.indexData = geometryLodView.indices_.data();
+                meshDecl.indexCount = geometryLodView.indices_.size();
+                meshDecl.indexFormat = xatlas::IndexFormat::UInt32;
 
-                atlasVertex.first_colocal = atlasVertices.size();
+                const xatlas::AddMeshError::Enum error = xatlas::AddMesh(atlas.get(), meshDecl);
+                if (error != xatlas::AddMeshError::Success)
+                    return false;
 
-                atlasVertex.position[0] = vertex.position_.x_;
-                atlasVertex.position[1] = vertex.position_.y_;
-                atlasVertex.position[2] = vertex.position_.z_;
-
-                atlasVertex.normal[0] = vertex.normal_.x_;
-                atlasVertex.normal[1] = vertex.normal_.y_;
-                atlasVertex.normal[2] = vertex.normal_.z_;
-
-                atlasVertex.uv[0] = vertex.uv_[0].x_;
-                atlasVertex.uv[1] = vertex.uv_[0].y_;
-
-                atlasVertices.push_back(atlasVertex);
-                atlasMapping.push_back({ geometryIndex, lodIndex, vertexIndex });
-
-                ++vertexIndex;
-            }
-
-            for (unsigned faceIndex = 0; faceIndex < geometryLodView.indices_.size() / 3; ++faceIndex)
-            {
-                Thekla::Atlas_Input_Face atlasFace;
-
-                atlasFace.material_index = geometryIndex;
-                for (int i = 0; i < 3; ++i)
-                {
-                    const unsigned index = geometryLodView.indices_[faceIndex * 3 + i];
-                    atlasFace.vertex_index[i] = static_cast<int>(vertexStart + index);
-                }
-
-                atlasFaces.push_back(atlasFace);
+                meshToGeometryLodMapping.emplace_back(geometryIndex, lodIndex);
             }
 
             ++lodIndex;
@@ -104,105 +74,47 @@ bool GenerateLightmapUV(ModelView& modelView, const LightmapUVGenerationSettings
         ++geometryIndex;
     }
 
-    // Generate output mesh
-    Thekla::Atlas_Input_Mesh atlasMesh;
-    atlasMesh.vertex_array = atlasVertices.data();
-    atlasMesh.vertex_count = atlasVertices.size();
-    atlasMesh.face_array = atlasFaces.data();
-    atlasMesh.face_count = atlasFaces.size();
+    // Generate things
+    xatlas::PackOptions packOptions;
+    packOptions.texelsPerUnit = settings.minDensity_;
 
-    Thekla::Atlas_Options atlasOptions;
-    Thekla::atlas_set_default_options(&atlasOptions);
-    atlasOptions.mapper_options.preserve_boundary = true;
-    atlasOptions.mapper_options.preserve_uvs = false;
+    xatlas::AddMeshJoin(atlas.get());
+    xatlas::Generate(atlas.get(), {}, nullptr, packOptions);
 
-    // disable brute force packing quality, as it has a number of notes about performance
-    // and it is turned off in Thekla example in repo as well. I am also seeing some meshes
-    // having problems packing with it and hanging on import
-    atlasOptions.packer_options.witness.packing_quality = 1;
+    // Copy output
+    const IntVector2 atlasSize{ static_cast<int>(atlas->width), static_cast<int>(atlas->height) };
 
-    atlasOptions.packer_options.witness.texels_per_unit = settings.minDensity_;
-    atlasOptions.packer_options.witness.conservative = true;
+    const float uScale = 1.f / atlas->width;
+    const float vScale = 1.f / atlas->height;
 
-    Thekla::Atlas_Error error = Thekla::Atlas_Error_Success;
-    Thekla::Atlas_Output_Mesh* outputMesh = Thekla::atlas_generate(&atlasMesh, &atlasOptions, &error);
-
-    if (error != Thekla::Atlas_Error_Success)
+    for (unsigned meshIndex = 0; meshIndex < atlas->meshCount; ++meshIndex)
     {
-        return false;
-    }
+        const unsigned geometryIndex = meshToGeometryLodMapping[meshIndex].first;
+        const unsigned lodIndex = meshToGeometryLodMapping[meshIndex].second;
+        GeometryLODView& geometryLodView = sourceGeometries[geometryIndex].lods_[lodIndex];
+        const xatlas::Mesh& mesh = atlas->meshes[meshIndex];
 
-    // Reserve geometries
-    ea::vector<GeometryView> newGeometries;
-    newGeometries.resize(sourceGeometries.size());
-    for (unsigned i = 0; i < sourceGeometries.size(); ++i)
-        newGeometries[i].lods_.resize(sourceGeometries[i].lods_.size());
-
-    // Copy vertices
-    const IntVector2 atlasSize{ outputMesh->atlas_width, outputMesh->atlas_height };
-
-    const float uScale = 1.f / outputMesh->atlas_width;
-    const float vScale = 1.f / outputMesh->atlas_height;
-
-    ea::vector<LightmapVertexMapping> outputMeshVerticesMapping;
-    outputMeshVerticesMapping.reserve(outputMesh->vertex_count);
-
-    for (int i = 0; i < outputMesh->vertex_count; ++i)
-    {
-        const Thekla::Atlas_Output_Vertex& outputVertex = outputMesh->vertex_array[i];
-        const LightmapVertexMapping& sourceMapping = atlasMapping[outputVertex.xref];
-
-        const GeometryLODView& sourceGeometry = sourceGeometries[sourceMapping.geometryIndex_].lods_[sourceMapping.lod_];
-        GeometryLODView& newGeometry = newGeometries[sourceMapping.geometryIndex_].lods_[sourceMapping.lod_];
-
-        // Create new mapping with new index
-        LightmapVertexMapping newMapping = sourceMapping;
-        newMapping.index_ = newGeometry.vertices_.size();
-
-        // Create new vertex with UV
-        ModelVertex newVertex = sourceGeometry.vertices_[sourceMapping.index_];
-        newVertex.uv_[settings.uvChannel_].x_ = uScale * outputVertex.uv[0];
-        newVertex.uv_[settings.uvChannel_].y_ = vScale * outputVertex.uv[1];
-
-        // Append vertex and register mapping
-        newGeometry.vertices_.push_back(newVertex);
-        outputMeshVerticesMapping.push_back(newMapping);
-    }
-
-    // Copy indices
-    for (unsigned faceIndex = 0; faceIndex < outputMesh->index_count / 3; faceIndex++)
-    {
-        const unsigned index0 = static_cast<unsigned>(outputMesh->index_array[faceIndex * 3]);
-        const unsigned index1 = static_cast<unsigned>(outputMesh->index_array[faceIndex * 3 + 1]);
-        const unsigned index2 = static_cast<unsigned>(outputMesh->index_array[faceIndex * 3 + 2]);
-
-        const LightmapVertexMapping& map0 = outputMeshVerticesMapping[index0];
-        const LightmapVertexMapping& map1 = outputMeshVerticesMapping[index1];
-        const LightmapVertexMapping& map2 = outputMeshVerticesMapping[index2];
-
-        // Find geometry and LOD
-        const unsigned geometryIndex = map0.geometryIndex_;
-        const unsigned lod = map0.lod_;
-        if (map1.geometryIndex_ != geometryIndex || map2.geometryIndex_ != geometryIndex || map1.lod_ != lod || map2.lod_ != lod)
+        ea::vector<ModelVertex> newVertices;
+        for (unsigned vertexIndex = 0; vertexIndex < mesh.vertexCount; ++vertexIndex)
         {
-            assert(0);
-            return false;
+            const xatlas::Vertex& vertex = mesh.vertexArray[vertexIndex];
+            ModelVertex newVertex = geometryLodView.vertices_[vertex.xref];
+            newVertex.uv_[settings.uvChannel_].x_ = uScale * vertex.uv[0];
+            newVertex.uv_[settings.uvChannel_].y_ = vScale * vertex.uv[1];
+            newVertices.push_back(newVertex);
         }
 
-        // Copy faces
-        GeometryLODView& newGeometry = newGeometries[geometryIndex].lods_[lod];
-        for (unsigned index : { map0.index_, map1.index_, map2.index_ })
-            newGeometry.indices_.push_back(index);
+        ea::vector<unsigned> newIndices;
+        newIndices.assign(mesh.indexArray, mesh.indexArray + mesh.indexCount);
+
+        geometryLodView.vertices_ = ea::move(newVertices);
+        geometryLodView.indices_ = ea::move(newIndices);
     }
 
-    // Build vertex format
+    // Finalize
     ModelVertexFormat vertexFormat = modelView.GetVertexFormat();
     vertexFormat.uv_[settings.uvChannel_] = TYPE_VECTOR2;
 
-    // Finalize
-    Thekla::atlas_free(outputMesh);
-
-    modelView.SetGeometries(newGeometries);
     modelView.SetVertexFormat(vertexFormat);
 
     modelView.AddMetadata(LightmapUVGenerationSettings::LightmapSizeKey, atlasSize);
