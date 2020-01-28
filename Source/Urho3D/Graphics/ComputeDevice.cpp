351d7e89b953affe7839cdcc290064bee93884cc
JSandusky <j.sandusky@hotmail.com>
2020-01-27 23:05:25 -0500

    Compute interface and start of DX11 implementation

---
+++ Source/Urho3D/Graphics/ComputeDevice.cpp
@@ -0,0 +1,25 @@
+#include "ComputeDevice.h"
+
+#include "../Graphics/ShaderVariation.h"
+
+namespace Urho3D
+{
+
+ComputeDevice::ComputeDevice(Context* context, Graphics* graphics) :
+    Object(context),
+    graphics_(graphics),
+    constantBuffersDirty_(false),
+    uavsDirty_(false),
+    samplersDirty_(false),
+    texturesDirty_(false),
+    programDirty_(false)
+{
+
+}
+
+ComputeDevice::~ComputeDevice()
+{
+
+}
+
+}
