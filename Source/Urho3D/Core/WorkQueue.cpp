1bc49a9c41982482544ad030c746a0c5805381a1
Eugene Kozlov <nekoeuge@gmail.com>
2020-02-03 16:58:35 +0300

    Improve WorkQueue lambda override.

--- Source/Urho3D/Core/WorkQueue.cpp
+++ Source/Urho3D/Core/WorkQueue.cpp
@@ -174,11 +174,11 @@ void WorkQueue::AddWorkItem(const SharedPtr<WorkItem>& item)
     }
 }
 
-SharedPtr<WorkItem> WorkQueue::AddWorkItem(std::function<void()> workFunction, unsigned priority)
+SharedPtr<WorkItem> WorkQueue::AddWorkItem(std::function<void(unsigned threadIndex)> workFunction, unsigned priority)
 {
     SharedPtr<WorkItem> item = GetFreeItem();
     item->workLambda_ = std::move(workFunction);
-    item->workFunction_ = [](const WorkItem* item, unsigned) { item->workLambda_(); };
+    item->workFunction_ = [](const WorkItem* item, unsigned threadIndex) { item->workLambda_(threadIndex); };
     item->priority_ = priority;
     AddWorkItem(item);
     return item;
