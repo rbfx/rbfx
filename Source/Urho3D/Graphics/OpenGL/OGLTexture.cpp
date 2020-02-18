9f20a4a58ca75c7e877fef3b6c62d7f1585870f8
Jonathan Sandusky <jonathan@jsandusky.com>
2020-02-18 16:29:55 -0500

    Initial implementation of OpenGL compute, ready to begin confirmation testing

--- Source/Urho3D/Graphics/OpenGL/OGLTexture.cpp
+++ Source/Urho3D/Graphics/OpenGL/OGLTexture.cpp
@@ -389,4 +389,14 @@ unsigned Texture::GetDSVFormat(unsigned format)
     return 0;
 }
 
+bool Texture::IsComputeWriteable(unsigned format)
+{
+    switch (format)
+    {
+    case GL_RGBA:
+        return true;
+    }
+    return false;
+}
+
 }
