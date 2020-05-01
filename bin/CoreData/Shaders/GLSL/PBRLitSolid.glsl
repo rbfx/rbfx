d23755014a8610c2b80b0b684775078f97f42302
Eugene Kozlov <nekoeuge@gmail.com>
2020-05-01 14:13:50 +0300

    Fix NOUV handling in glsl shaders.

--- bin/CoreData/Shaders/GLSL/PBRLitSolid.glsl
+++ bin/CoreData/Shaders/GLSL/PBRLitSolid.glsl
@@ -57,13 +57,17 @@ void VS()
         vColor = iColor;
     #endif
 
+    #ifdef NOUV
+        vTexCoord.xy = vec2(0.0, 0.0);
+    #else
+        vTexCoord.xy = GetTexCoord(iTexCoord);
+    #endif
+
     #if defined(NORMALMAP) || defined(DIRBILLBOARD)
         vec4 tangent = GetWorldTangent(modelMatrix);
         vec3 bitangent = cross(tangent.xyz, vNormal) * tangent.w;
-        vTexCoord = vec4(GetTexCoord(iTexCoord), bitangent.xy);
+        vTexCoord.zw = bitangent.xy;
         vTangent = vec4(tangent.xyz, bitangent.z);
-    #else
-        vTexCoord = GetTexCoord(iTexCoord);
     #endif
 
     // Ambient & per-vertex lighting
