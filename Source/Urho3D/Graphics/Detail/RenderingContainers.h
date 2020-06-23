743b7ea343b1b68b8eaa20aa3b2f808997ee8824
Eugene Kozlov <nekoeuge@gmail.com>
2020-06-24 01:13:38 +0300

    Extract SceneBatchCollector into separate files.

--- Source/Urho3D/Graphics/Detail/RenderingContainers.h
+++ Source/Urho3D/Graphics/Detail/RenderingContainers.h
@@ -23,6 +23,7 @@
 #pragma once
 
 #include "../../Math/NumericRange.h"
+#include "../../Graphics/SceneBatchCollector.h"
 
 #include <EASTL/vector.h>
 
@@ -44,49 +45,6 @@ using ThreadedGeometryCollection = ThreadedVector<Drawable*>;
 /// Collection of lights in the Scene. Can be used from multiple threads.
 using ThreadedLightCollection = ThreadedVector<Light*>;
 
-/// Min and max Z value of drawable(s).
-using DrawableZRange = NumericRange<float>;
-
-/// Min and max Z value of scene. Can be used from multiple threads.
-class SceneZRange
-{
-public:
-    /// Clear in the beginning of the frame.
-    void Clear(unsigned numThreads)
-    {
-        threadRanges_.clear();
-        threadRanges_.resize(numThreads);
-        sceneRangeDirty_ = true;
-    }
-
-    /// Accumulate min and max Z value.
-    void Accumulate(unsigned threadIndex, const DrawableZRange& range)
-    {
-        threadRanges_[threadIndex] |= range;
-    }
-
-    /// Get value.
-    const DrawableZRange& Get()
-    {
-        if (sceneRangeDirty_)
-        {
-            sceneRangeDirty_ = false;
-            sceneRange_ = {};
-            for (const DrawableZRange& range : threadRanges_)
-                sceneRange_ |= range;
-        }
-        return sceneRange_;
-    }
-
-private:
-    /// Min and max Z value per thread.
-    ea::vector<DrawableZRange> threadRanges_;
-    /// Min and max Z value for Scene.
-    DrawableZRange sceneRange_;
-    /// Whether the Scene range is dirty.
-    bool sceneRangeDirty_{};
-};
-
 /// Per-viewport drawable data, indexed via drawable index. Doesn't persist across frames.
 struct TransientDrawableDataIndex
 {
@@ -99,8 +57,6 @@ struct TransientDrawableDataIndex
         DrawableUpdated = 1 << 1,
         /// Whether the drawable has geometry visible from the main camera.
         DrawableVisibleGeometry = 1 << 2,
-        /// Whether the drawable is lit using forward rendering.
-        ForwardLit = 1 << 3,
     };
 
     /// Traits.
