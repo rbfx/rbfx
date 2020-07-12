f56fc51dd5549ab9251809d7ff60a48e4a73ba86
Eugene Kozlov <nekoeuge@gmail.com>
2020-07-12 20:38:26 +0300

    Extract code into separate files.

---
+++ Source/Urho3D/Graphics/DrawableLightAccumulator.h
@@ -0,0 +1,120 @@
+//
+// Copyright (c) 2017-2020 the rbfx project.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+
+#pragma once
+
+#include "../Graphics/Light.h"
+#include "../Math/SphericalHarmonics.h"
+
+#include <EASTL/fixed_vector.h>
+#include <EASTL/sort.h>
+#include <EASTL/vector_map.h>
+
+namespace Urho3D
+{
+
+/// Context used for light accumulation.
+struct DrawableLightDataAccumulationContext
+{
+    /// Max number of pixel lights
+    unsigned maxPixelLights_{ 1 };
+    /// Light importance.
+    LightImportance lightImportance_{};
+    /// Light index.
+    unsigned lightIndex_{};
+    /// Array of lights to be indexed.
+    const ea::vector<Light*>* lights_;
+};
+
+/// Accumulated light data for drawable.
+/// MaxPixelLights: Max number of per-pixel lights supported. Important lights may override this limit.
+/// MaxVertexLights: Max number of per-vertex lights supported.
+template <unsigned MaxPixelLights, unsigned MaxVertexLights>
+struct DrawableLightAccumulator
+{
+    /// Max number of lights that don't require allocations.
+    static const unsigned NumElements = ea::max(MaxPixelLights + 1, 4u) + MaxVertexLights;
+    /// Container for lights.
+    using Container = ea::vector_map<float, unsigned, ea::less<float>, ea::allocator,
+        ea::fixed_vector<ea::pair<float, unsigned>, NumElements>>;
+    /// Container for vertex lights.
+    using VertexLightContainer = ea::array<unsigned, MaxVertexLights>;
+
+    /// Reset accumulator.
+    void Reset()
+    {
+        lights_.clear();
+        numImportantLights_ = 0;
+    }
+
+    /// Accumulate light.
+    void AccumulateLight(const DrawableLightDataAccumulationContext& ctx, float penalty)
+    {
+        // Count important lights
+        if (ctx.lightImportance_ == LI_IMPORTANT)
+        {
+            penalty = -1.0f;
+            ++numImportantLights_;
+        }
+
+        // Add new light
+        lights_.emplace(penalty, ctx.lightIndex_);
+
+        // If too many lights, drop the least important one
+        firstVertexLight_ = ea::max(ctx.maxPixelLights_, numImportantLights_);
+        const unsigned maxLights = MaxVertexLights + firstVertexLight_;
+        if (lights_.size() > maxLights)
+        {
+            // TODO: Update SH
+            lights_.pop_back();
+        }
+    }
+
+    /// Return per-vertex lights.
+    VertexLightContainer GetVertexLights() const
+    {
+        VertexLightContainer vertexLights;
+        for (unsigned i = 0; i < 4; ++i)
+        {
+            const unsigned index = i + firstVertexLight_;
+            vertexLights[i] = index < lights_.size() ? lights_.at(index).second : M_MAX_UNSIGNED;
+        }
+        return vertexLights;
+    }
+
+    /// Return per-pixel lights.
+    ea::span<const ea::pair<float, unsigned>> GetPixelLights() const
+    {
+        return { lights_.data(), firstVertexLight_ };
+    }
+
+    /// Container of per-pixel and per-pixel lights.
+    Container lights_;
+    /// Accumulated SH lights.
+    SphericalHarmonicsDot9 sh_;
+    /// Number of important lights.
+    unsigned numImportantLights_{};
+    /// First vertex light.
+    unsigned firstVertexLight_{};
+};
+
+}
--- Source/Urho3D/Graphics/SceneBatchCollector.h
+++ Source/Urho3D/Graphics/SceneBatchCollector.h
@@ -23,10 +23,12 @@
 #pragma once
 
 #include "../Core/ThreadedVector.h"
+#include "../Graphics/DrawableLightAccumulator.h"
 #include "../Graphics/Graphics.h"
 #include "../Graphics/Light.h"
 #include "../Graphics/Material.h"
 #include "../Graphics/Technique.h"
+#include "../Graphics/SceneBatch.h"
 #include "../Math/NumericRange.h"
 #include "../Math/SphericalHarmonics.h"
 
@@ -140,240 +142,6 @@ struct ScenePassDescription
     ea::string additionalLightPassName_;
 };
 
-/// Context used for light accumulation.
-struct DrawableLightDataAccumulationContext
-{
-    /// Max number of pixel lights
-    unsigned maxPixelLights_{ 1 };
-    /// Light importance.
-    LightImportance lightImportance_{};
-    /// Light index.
-    unsigned lightIndex_{};
-    /// Array of lights to be indexed.
-    const ea::vector<Light*>* lights_;
-};
-
-/// Accumulated light data for drawable.
-/// MaxPixelLights: Max number of per-pixel lights supported. Important lights may override this limit.
-/// MaxVertexLights: Max number of per-vertex lights supported.
-template <unsigned MaxPixelLights, unsigned MaxVertexLights>
-struct DrawableLightData
-{
-    /// Max number of lights that don't require allocations.
-    static const unsigned NumElements = ea::max(MaxPixelLights + 1, 4u) + MaxVertexLights;
-    /// Container for lights.
-    using Container = ea::vector_map<float, unsigned, ea::less<float>, ea::allocator,
-        ea::fixed_vector<ea::pair<float, unsigned>, NumElements>>;
-    /// Container for vertex lights.
-    using VertexLightContainer = ea::array<unsigned, MaxVertexLights>;
-
-    /// Reset accumulator.
-    void Reset()
-    {
-        lights_.clear();
-        numImportantLights_ = 0;
-    }
-
-    /// Accumulate light.
-    void AccumulateLight(const DrawableLightDataAccumulationContext& ctx, float penalty)
-    {
-        // Count important lights
-        if (ctx.lightImportance_ == LI_IMPORTANT)
-        {
-            penalty = -1.0f;
-            ++numImportantLights_;
-        }
-
-        // Add new light
-        lights_.emplace(penalty, ctx.lightIndex_);
-
-        // If too many lights, drop the least important one
-        firstVertexLight_ = ea::max(ctx.maxPixelLights_, numImportantLights_);
-        const unsigned maxLights = MaxVertexLights + firstVertexLight_;
-        if (lights_.size() > maxLights)
-        {
-            // TODO: Update SH
-            lights_.pop_back();
-        }
-    }
-
-    /// Return per-vertex lights.
-    VertexLightContainer GetVertexLights() const
-    {
-        VertexLightContainer vertexLights;
-        for (unsigned i = 0; i < 4; ++i)
-        {
-            const unsigned index = i + firstVertexLight_;
-            vertexLights[i] = index < lights_.size() ? lights_.at(index).second : M_MAX_UNSIGNED;
-        }
-        return vertexLights;
-    }
-
-    /// Return per-pixel lights.
-    ea::span<const ea::pair<float, unsigned>> GetPixelLights() const
-    {
-        return { lights_.data(), firstVertexLight_ };
-    }
-
-    /// Container of per-pixel and per-pixel lights.
-    Container lights_;
-    /// Accumulated SH lights.
-    SphericalHarmonicsDot9 sh_;
-    /// Number of important lights.
-    unsigned numImportantLights_{};
-    /// First vertex light.
-    unsigned firstVertexLight_{};
-};
-
-/// Base or lit base scene batch for specific sub-pass.
-struct SceneBatch
-{
-    /// Drawable index.
-    unsigned drawableIndex_{};
-    /// Source batch index.
-    unsigned sourceBatchIndex_{};
-    /// Geometry type used.
-    GeometryType geometryType_{};
-    /// Drawable to be rendered.
-    Drawable* drawable_{};
-    /// Geometry to be rendered.
-    Geometry* geometry_{};
-    /// Material to be rendered.
-    Material* material_{};
-    /// Material pass to be rendered.
-    Pass* pass_{};
-    /// Pipeline state.
-    PipelineState* pipelineState_{};
-
-    /// Return source batch.
-    const SourceBatch& GetSourceBatch() const { return drawable_->GetBatches()[sourceBatchIndex_]; }
-};
-
-/// Additional light scene batch for specific sub-pass.
-struct LightSceneBatch
-{
-    /// Base scene batch.
-    SceneBatch* sceneBatch_{};
-    /// Pass override.
-    Pass* pass_{};
-    /// Pipeline state override.
-    PipelineState* pipelineState_{};
-    /// Light.
-    Light* light_{};
-};
-
-/// Scene batch sorted by pipeline state, material and geometry. Also sorted front to back.
-struct SceneBatchSortedByState
-{
-    /// Sorting value for pipeline state.
-    unsigned long long pipelineStateKey_{};
-    /// Sorting value for material and geometry.
-    unsigned long long materialGeometryKey_{};
-    /// Sorting distance.
-    float distance_{};
-    /// Batch to be sorted.
-    const SceneBatch* sceneBatch_{};
-
-    /// Construct default.
-    SceneBatchSortedByState() = default;
-
-    /// Construct from batch.
-    explicit SceneBatchSortedByState(const SceneBatch* batch)
-        : sceneBatch_(batch)
-    {
-        const SourceBatch& sourceBatch = batch->GetSourceBatch();
-
-        // 8: render order
-        // 32: shader variation
-        // 24: pipeline state
-        const unsigned long long renderOrder = batch->material_->GetRenderOrder();
-        const unsigned long long shaderHash = batch->pipelineState_->GetShaderHash();
-        const unsigned pipelineStateHash = MakeHash(batch->pipelineState_);
-        pipelineStateKey_ |= renderOrder << 56;
-        pipelineStateKey_ |= shaderHash << 24;
-        pipelineStateKey_ |= (pipelineStateHash & 0xffffff) ^ (pipelineStateHash >> 24);
-
-        // 32: material
-        // 32: geometry
-        unsigned long long materialHash = MakeHash(batch->material_);
-        materialGeometryKey_ |= (materialHash ^ sourceBatch.lightmapIndex_) << 32;
-        materialGeometryKey_ |= MakeHash(batch->geometry_);
-
-        distance_ = sourceBatch.distance_;
-    }
-
-    /// Compare sorted batches.
-    bool operator < (const SceneBatchSortedByState& rhs) const
-    {
-        if (pipelineStateKey_ != rhs.pipelineStateKey_)
-            return pipelineStateKey_ < rhs.pipelineStateKey_;
-        if (materialGeometryKey_ != rhs.materialGeometryKey_)
-            return materialGeometryKey_ < rhs.materialGeometryKey_;
-        return distance_ > rhs.distance_;
-    }
-};
-
-/// Scene batch sorted by render order and back to front.
-struct SceneBatchSortedBackToFront
-{
-    /// Render order.
-    unsigned char renderOrder_{};
-    /// Sorting distance.
-    float distance_{};
-    /// Batch to be sorted.
-    const SceneBatch* sceneBatch_{};
-
-    /// Construct default.
-    SceneBatchSortedBackToFront() = default;
-
-    /// Construct from batch.
-    explicit SceneBatchSortedBackToFront(const SceneBatch* batch)
-        : renderOrder_(batch->material_->GetRenderOrder())
-        , sceneBatch_(batch)
-    {
-        const SourceBatch& sourceBatch = batch->GetSourceBatch();
-        distance_ = sourceBatch.distance_;
-    }
-
-    /// Compare sorted batches.
-    bool operator < (const SceneBatchSortedBackToFront& rhs) const
-    {
-        if (renderOrder_ != rhs.renderOrder_)
-            return renderOrder_ < rhs.renderOrder_;
-        return distance_ < rhs.distance_;
-    }
-};
-
-/// Light batch sorted by light, pipeline state, material and geometry.
-struct LightBatchSortedByState : public SceneBatchSortedByState
-{
-    /// Light batch.
-    const LightSceneBatch* lightBatch_{};
-    /// Light.
-    Light* light_{};
-
-    /// Construct default.
-    LightBatchSortedByState() = default;
-
-    /// Construct from batch.
-    explicit LightBatchSortedByState(const LightSceneBatch* lightBatch)
-        : SceneBatchSortedByState(lightBatch->sceneBatch_)
-        , lightBatch_(lightBatch)
-        , light_(lightBatch->light_)
-    {
-    }
-
-    /// Compare sorted batches.
-    bool operator < (const LightBatchSortedByState& rhs) const
-    {
-        if (light_ != rhs.light_)
-            return light_ < rhs.light_;
-        if (pipelineStateKey_ != rhs.pipelineStateKey_)
-            return pipelineStateKey_ < rhs.pipelineStateKey_;
-        return materialGeometryKey_ < rhs.materialGeometryKey_;
-    }
-};
-
 /// Pipeline state factory for scene.
 class ScenePipelineStateFactory
 {
@@ -419,7 +187,7 @@ public:
     /// Return main light.
     Light* GetMainLight() const { return mainLightIndex_ != M_MAX_UNSIGNED ? visibleLights_[mainLightIndex_] : nullptr; }
     /// Return base batches for given pass.
-    const ea::vector<SceneBatch>& GetBaseBatches(const ea::string& pass) const;
+    const ea::vector<BaseSceneBatch>& GetBaseBatches(const ea::string& pass) const;
     /// Return sorted base batches for given pass.
     template <class T> void GetSortedBaseBatches(const ea::string& pass, ea::vector<T>& sortedBatches) const;
     /// Return light batches for given pass.
@@ -478,10 +246,10 @@ private:
     void CollectSceneBatches();
     /// Convert scene batches from intermediate batches to unlit base batches.
     void CollectSceneUnlitBaseBatches(SubPassPipelineStateCache& subPassCache,
-        const ThreadedVector<IntermediateSceneBatch>& intermediateBatches, ea::vector<SceneBatch>& sceneBatches);
+        const ThreadedVector<IntermediateSceneBatch>& intermediateBatches, ea::vector<BaseSceneBatch>& sceneBatches);
     /// Convert scene batches from intermediate batches to lit base batches and light batches.
     void CollectSceneLitBaseBatches(SubPassPipelineStateCache& baseSubPassCache, SubPassPipelineStateCache& lightSubPassCache,
-        const ThreadedVector<IntermediateSceneBatch>& intermediateBatches, ea::vector<SceneBatch>& baseSceneBatches,
+        const ThreadedVector<IntermediateSceneBatch>& intermediateBatches, ea::vector<BaseSceneBatch>& baseSceneBatches,
         ThreadedVector<LightSceneBatch>& lightSceneBatches);
 
     /// Max number of pixel lights per drawable. Important lights may override this limit.
@@ -517,7 +285,7 @@ private:
     /// Passes.
     ea::vector<PassData> passes_;
     /// Base batches lookup table.
-    ea::unordered_map<unsigned, ea::vector<SceneBatch>*> baseBatchesLookup_;
+    ea::unordered_map<unsigned, ea::vector<BaseSceneBatch>*> baseBatchesLookup_;
     /// Light batches lookup table.
     ea::unordered_map<unsigned, ThreadedVector<LightSceneBatch>*> lightBatchesLookup_;
 
@@ -535,7 +303,7 @@ private:
     /// Transient data index.
     TransientDrawableIndex transient_;
     /// Drawable lighting data index.
-    ea::vector<DrawableLightData<MaxPixelLights, MaxVertexLights>> drawableLighting_;
+    ea::vector<DrawableLightAccumulator<MaxPixelLights, MaxVertexLights>> drawableLighting_;
 
     /// Per-light caches.
     ea::unordered_map<WeakPtr<Light>, ea::unique_ptr<LightData>> cachedLightData_;
@@ -543,7 +311,7 @@ private:
     ea::vector<LightData*> visibleLightsData_;
 
     /// Temporary collection for pipeline state cache misses (base batches).
-    ThreadedVector<SceneBatch*> baseSceneBatchesWithoutPipelineStates_;
+    ThreadedVector<BaseSceneBatch*> baseSceneBatchesWithoutPipelineStates_;
     /// Temporary collection for pipeline state cache misses (light batches).
     ThreadedVector<unsigned> lightSceneBatchesWithoutPipelineStates_;
 };
@@ -551,7 +319,7 @@ private:
 template <class T>
 void SceneBatchCollector::GetSortedBaseBatches(const ea::string& pass, ea::vector<T>& sortedBatches) const
 {
-    const ea::vector<SceneBatch>& baseBatches = GetBaseBatches(pass);
+    const ea::vector<BaseSceneBatch>& baseBatches = GetBaseBatches(pass);
     const unsigned numBatches = baseBatches.size();
     sortedBatches.resize(numBatches);
     for (unsigned i = 0; i < numBatches; ++i)
