Mike-Gao/Urho3D

Rake 'install' task.

Rakefile
@@ -69,6 +69,15 @@ task :build, [:target] => [:cmake] do |_, args|
   system "ccache -s" if ENV['USE_CCACHE']
 end
 
+desc 'Install the software'
+task :install, [:dir] => [:init] do |_, args|
+  if ENV['PLATFORM'] == 'android'
+    Rake::Task['gradle'].invoke('publishToMavenLocal')
+    next
+  end
+  system "#{args[:dir] && !ENV['OS'] ? "DESTDIR=#{args[:dir]}" : ''} #{build_target('install')}" or abort
+end
+
 desc 'Test the software'
 task :test => [:init] do
   if ENV['PLATFORM'] == 'android'

rbfx/rbfx

Rakefile
@@ -95,6 +95,15 @@ task :doc => [:init] do
   system build_target('doc') or abort
 end
 
+desc 'Install the software'
+task :install, [:prefix] => [:init] do |_, args|
+  if ENV['PLATFORM'] == 'android'
+    Rake::Task['gradle'].invoke('publishToMavenLocal')
+    next
+  end
+  system "#{args[:prefix] && !ENV['OS'] ? "DESTDIR=#{args[:prefix]}" : ''} #{build_target('install')}" or abort
+end
+
 desc 'Package build artifact'
 task :package => [:init] do
   if ENV['PLATFORM'] == 'android'