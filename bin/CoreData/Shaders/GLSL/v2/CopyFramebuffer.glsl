e71a20faed51f8475b40f0fcc1fc351bdefe0b28
Eugene Kozlov <nekoeuge@gmail.com>
2020-11-07 23:34:06 +0300

    Implement deferred rendering for DX11 backend.

--- bin/CoreData/Shaders/GLSL/v2/CopyFramebuffer.glsl
+++ bin/CoreData/Shaders/GLSL/v2/CopyFramebuffer.glsl
@@ -1,20 +1,31 @@
-#include "Uniforms.glsl"
-#include "Samplers.glsl"
-#include "Transform.glsl"
-#include "ScreenPos.glsl"
+#define GEOM_STATIC
+#define LAYOUT_HAS_POSITION
 
-varying vec2 vScreenPos;
+#include "_Config.glsl"
+#include "_Uniforms.glsl"
+#include "_VertexLayout.glsl"
+#include "_VertexTransform.glsl"
+#include "_VertexScreenPos.glsl"
+#include "_PixelOutput.glsl"
 
-void VS()
+uniform sampler2D sDiffMap;
+
+VERTEX_SHADER_OUT(vec2 vScreenPos)
+
+#ifdef STAGE_VERTEX_SHADER
+void main()
 {
     mat4 modelMatrix = iModelMatrix;
     vec3 worldPos = GetWorldPos(modelMatrix);
     gl_Position = GetClipPos(worldPos);
     vScreenPos = GetScreenPosPreDiv(gl_Position);
 }
+#endif
 
-void PS()
+#ifdef STAGE_PIXEL_SHADER
+void main()
 {
     gl_FragColor = texture2D(sDiffMap, vScreenPos);
 }
+#endif
 
--- bin/CoreData/Shaders/GLSL/v2/_VertexScreenPos.glsl
+++ bin/CoreData/Shaders/GLSL/v2/_VertexScreenPos.glsl
@@ -1,4 +1,4 @@
-#ifdef COMPILEVS
+#ifdef STAGE_VERTEX_SHADER
 mat3 GetCameraRot()
 {
     return mat3(cViewInv[0][0], cViewInv[0][1], cViewInv[0][2],
@@ -10,7 +10,11 @@ vec4 GetScreenPos(vec4 clipPos)
 {
     return vec4(
         clipPos.x * cGBufferOffsets.z + cGBufferOffsets.x * clipPos.w,
+#ifdef URHO3D_FLIP_FRAMEBUFFER
+        -clipPos.y * cGBufferOffsets.w + cGBufferOffsets.y * clipPos.w,
+#else
         clipPos.y * cGBufferOffsets.w + cGBufferOffsets.y * clipPos.w,
+#endif
         0.0,
         clipPos.w);
 }
@@ -19,14 +23,22 @@ vec2 GetScreenPosPreDiv(vec4 clipPos)
 {
     return vec2(
         clipPos.x / clipPos.w * cGBufferOffsets.z + cGBufferOffsets.x,
+#ifdef URHO3D_FLIP_FRAMEBUFFER
+        -clipPos.y / clipPos.w * cGBufferOffsets.w + cGBufferOffsets.y);
+#else
         clipPos.y / clipPos.w * cGBufferOffsets.w + cGBufferOffsets.y);
+#endif
 }
 
 vec2 GetQuadTexCoord(vec4 clipPos)
 {
     return vec2(
         clipPos.x / clipPos.w * 0.5 + 0.5,
+#ifdef URHO3D_FLIP_FRAMEBUFFER
+        -clipPos.y / clipPos.w * 0.5 + 0.5);
+#else
         clipPos.y / clipPos.w * 0.5 + 0.5);
+#endif
 }
 
 vec2 GetQuadTexCoordNoFlip(vec3 worldPos)
