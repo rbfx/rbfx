bea64d4a7e5940c08b0f091d2005d933b88557bd
Rokas Kupstys <rokups@zoho.com>
2020-12-09 12:28:21 +0200

    CI: Previous commit should have xtended to iOS as well.

--- script/ci_build.sh
+++ script/ci_build.sh
@@ -50,7 +50,7 @@ generators_windows=('-G' 'Visual Studio 16 2019')
 generators_linux=('-G' 'Ninja')
 generators_web=('-G' 'Ninja')
 generators_macos=('-G' 'Xcode' '-T' 'buildsystem=1')
-generators_ios=('-G' 'Xcode')
+generators_ios=('-G' 'Xcode' '-T' 'buildsystem=1')
 
 toolchains_ios=(
     '-DCMAKE_TOOLCHAIN_FILE=cmake/Toolchains/IOS.cmake'
