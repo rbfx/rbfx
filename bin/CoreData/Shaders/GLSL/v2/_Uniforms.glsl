dd992ad2008255cb525251421ba2681bf12684d1
Eugene Kozlov <nekoeuge@gmail.com>
2021-01-14 10:55:16 +0300

    Fix Web rendering.

--- bin/CoreData/Shaders/GLSL/v2/_Uniforms.glsl
+++ bin/CoreData/Shaders/GLSL/v2/_Uniforms.glsl
@@ -2,92 +2,92 @@
 #define _UNIFORMS_GLSL_
 
 CBUFFER_BEGIN(0, Frame)
-    CBUFFER_UNIFORM(float cDeltaTime)
-    CBUFFER_UNIFORM(float cElapsedTime)
+    CBUFFER_UNIFORM(mediump float cDeltaTime)
+    CBUFFER_UNIFORM(mediump float cElapsedTime)
 CBUFFER_END()
 
 CBUFFER_BEGIN(1, Camera)
-    CBUFFER_UNIFORM(vec3 cCameraPos)
-    CBUFFER_UNIFORM(float cNearClip)
-    CBUFFER_UNIFORM(float cFarClip)
-    CBUFFER_UNIFORM(vec4 cDepthMode)
-    CBUFFER_UNIFORM(vec3 cFrustumSize)
-    CBUFFER_UNIFORM(vec4 cGBufferOffsets)
-    CBUFFER_UNIFORM(mat4 cView)
-    CBUFFER_UNIFORM(mat4 cViewInv)
-    CBUFFER_UNIFORM(mat4 cViewProj)
-    CBUFFER_UNIFORM(vec4 cClipPlane)
-    CBUFFER_UNIFORM(vec4 cDepthReconstruct)
-    CBUFFER_UNIFORM(vec2 cGBufferInvSize)
+    CBUFFER_UNIFORM(mediump vec3 cCameraPos) // TODO(renderer): Increase precision
+    CBUFFER_UNIFORM(mediump float cNearClip)
+    CBUFFER_UNIFORM(mediump float cFarClip)
+    CBUFFER_UNIFORM(mediump vec4 cDepthMode)
+    CBUFFER_UNIFORM(mediump vec3 cFrustumSize)
+    CBUFFER_UNIFORM(mediump vec4 cGBufferOffsets)
+    CBUFFER_UNIFORM(highp mat4 cView)
+    CBUFFER_UNIFORM(highp mat4 cViewInv)
+    CBUFFER_UNIFORM(highp mat4 cViewProj)
+    CBUFFER_UNIFORM(highp vec4 cClipPlane)
+    CBUFFER_UNIFORM(mediump vec4 cDepthReconstruct)
+    CBUFFER_UNIFORM(mediump vec2 cGBufferInvSize)
 CBUFFER_END()
 
 CBUFFER_BEGIN(2, Zone)
-    CBUFFER_UNIFORM(vec4 cAmbientColor)
-    CBUFFER_UNIFORM(vec4 cFogParams)
-    CBUFFER_UNIFORM(vec3 cFogColor)
+    CBUFFER_UNIFORM(mediump vec4 cAmbientColor)
+    CBUFFER_UNIFORM(mediump vec4 cFogParams)
+    CBUFFER_UNIFORM(mediump vec3 cFogColor)
 CBUFFER_END()
 
 CBUFFER_BEGIN(3, Light)
-    CBUFFER_UNIFORM(vec4 cLightPos)
-    CBUFFER_UNIFORM(vec3 cLightDir)
-    CBUFFER_UNIFORM(vec4 cNormalOffsetScale)
+    CBUFFER_UNIFORM(mediump vec4 cLightPos) // TODO(renderer): Increase precision
+    CBUFFER_UNIFORM(mediump vec3 cLightDir)
+    CBUFFER_UNIFORM(mediump vec4 cNormalOffsetScale)
 #ifdef NUMVERTEXLIGHTS
-    CBUFFER_UNIFORM(vec4 cVertexLights[4 * 3])
+    CBUFFER_UNIFORM_VS(highp vec4 cVertexLights[4 * 3])
 #endif
-    CBUFFER_UNIFORM(mat4 cLightMatrices[4])
+    CBUFFER_UNIFORM(mediump mat4 cLightMatrices[4])
 
-    CBUFFER_UNIFORM(vec4 cLightColor)
-    CBUFFER_UNIFORM(vec4 cShadowCubeAdjust)
-    CBUFFER_UNIFORM(vec2 cShadowCubeUVBias)
-    CBUFFER_UNIFORM(vec4 cShadowDepthFade)
-    CBUFFER_UNIFORM(vec2 cShadowIntensity)
-    CBUFFER_UNIFORM(vec2 cShadowMapInvSize)
-    CBUFFER_UNIFORM(vec4 cShadowSplits)
+    CBUFFER_UNIFORM(mediump vec4 cLightColor)
+    CBUFFER_UNIFORM(mediump vec4 cShadowCubeAdjust)
+    CBUFFER_UNIFORM(mediump vec2 cShadowCubeUVBias)
+    CBUFFER_UNIFORM(mediump vec4 cShadowDepthFade)
+    CBUFFER_UNIFORM(mediump vec2 cShadowIntensity)
+    CBUFFER_UNIFORM(mediump vec2 cShadowMapInvSize)
+    CBUFFER_UNIFORM(mediump vec4 cShadowSplits)
 #ifdef VSM_SHADOW
-    CBUFFER_UNIFORM(vec2 cVSMShadowParams)
+    CBUFFER_UNIFORM(mediump vec2 cVSMShadowParams)
 #endif
 #ifdef PBR
-    CBUFFER_UNIFORM(float cLightRad)
-    CBUFFER_UNIFORM(float cLightLength)
+    CBUFFER_UNIFORM(mediump float cLightRad)
+    CBUFFER_UNIFORM(mediump float cLightLength)
 #endif
 CBUFFER_END()
 
 #ifndef CUSTOM_MATERIAL_CBUFFER
 CBUFFER_BEGIN(4, Material)
-    CBUFFER_UNIFORM(vec4 cUOffset)
-    CBUFFER_UNIFORM(vec4 cVOffset)
-    CBUFFER_UNIFORM(vec4 cLMOffset)
+    CBUFFER_UNIFORM(mediump vec4 cUOffset)
+    CBUFFER_UNIFORM(mediump vec4 cVOffset)
+    CBUFFER_UNIFORM(mediump vec4 cLMOffset)
 
-    CBUFFER_UNIFORM(vec4 cMatDiffColor)
-    CBUFFER_UNIFORM(vec3 cMatEmissiveColor)
-    CBUFFER_UNIFORM(vec3 cMatEnvMapColor)
-    CBUFFER_UNIFORM(vec4 cMatSpecColor)
+    CBUFFER_UNIFORM(mediump vec4 cMatDiffColor)
+    CBUFFER_UNIFORM(mediump vec3 cMatEmissiveColor)
+    CBUFFER_UNIFORM(mediump vec3 cMatEnvMapColor)
+    CBUFFER_UNIFORM(mediump vec4 cMatSpecColor)
 #ifdef PBR
-    CBUFFER_UNIFORM(float cRoughness)
-    CBUFFER_UNIFORM(float cMetallic)
+    CBUFFER_UNIFORM(mediump float cRoughness)
+    CBUFFER_UNIFORM(mediump float cMetallic)
 #endif
 CBUFFER_END()
 #endif
 
-#ifdef COMPILEVS
+#ifdef STAGE_VERTEX_SHADER
 CBUFFER_BEGIN(5, Object)
-    CBUFFER_UNIFORM(mat4 cModel)
+    CBUFFER_UNIFORM(highp mat4 cModel)
 #ifdef SPHERICALHARMONICS
-    CBUFFER_UNIFORM(vec4 cSHAr)
-    CBUFFER_UNIFORM(vec4 cSHAg)
-    CBUFFER_UNIFORM(vec4 cSHAb)
-    CBUFFER_UNIFORM(vec4 cSHBr)
-    CBUFFER_UNIFORM(vec4 cSHBg)
-    CBUFFER_UNIFORM(vec4 cSHBb)
-    CBUFFER_UNIFORM(vec4 cSHC)
+    CBUFFER_UNIFORM(mediump vec4 cSHAr)
+    CBUFFER_UNIFORM(mediump vec4 cSHAg)
+    CBUFFER_UNIFORM(mediump vec4 cSHAb)
+    CBUFFER_UNIFORM(mediump vec4 cSHBr)
+    CBUFFER_UNIFORM(mediump vec4 cSHBg)
+    CBUFFER_UNIFORM(mediump vec4 cSHBb)
+    CBUFFER_UNIFORM(mediump vec4 cSHC)
 #else
-    CBUFFER_UNIFORM(vec4 cAmbient)
+    CBUFFER_UNIFORM(mediump vec4 cAmbient)
 #endif
 #ifdef GEOM_BILLBOARD
-    CBUFFER_UNIFORM(mat3 cBillboardRot)
+    CBUFFER_UNIFORM(mediump mat3 cBillboardRot)
 #endif
 #ifdef GEOM_SKINNED
-    CBUFFER_UNIFORM(vec4 cSkinMatrices[MAXBONES * 3])
+    CBUFFER_UNIFORM(highp vec4 cSkinMatrices[MAXBONES * 3])
 #endif
 CBUFFER_END()
 #endif
