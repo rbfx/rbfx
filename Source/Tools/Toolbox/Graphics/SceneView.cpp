d4a560f2f6203cff9e716beea0cf8336224be1ce
RIJX <info@rijx.com>
2021-02-18 17:18:30 +0100

    Fix SetSize returning too early in constructor

--- Source/Tools/Toolbox/Graphics/SceneView.cpp
+++ Source/Tools/Toolbox/Graphics/SceneView.cpp
@@ -36,12 +36,11 @@ namespace Urho3D
 {
 
 SceneView::SceneView(Context* context, const IntRect& rect)
-    : rect_(rect)
+    : rect_()
 {
     scene_ = SharedPtr<Scene>(new Scene(context));
     scene_->CreateComponent<Octree>();
     viewport_ = SharedPtr<Viewport>(new Viewport(context, scene_, nullptr));
-    viewport_->SetRect(IntRect(IntVector2::ZERO, rect_.Size()));
     CreateObjects();
     texture_ = SharedPtr<Texture2D>(new Texture2D(context));
     // Make sure viewport is not using default renderpath. That would cause issues when renderpath
