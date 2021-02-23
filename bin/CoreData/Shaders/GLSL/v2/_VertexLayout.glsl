1c6de1688787ded0eeaa79fed5541e9a7c523815
Eugene Kozlov <nekoeuge@gmail.com>
2021-02-23 11:34:47 +0300

    GLSL: Cleanup configuration

--- bin/CoreData/Shaders/GLSL/v2/_VertexLayout.glsl
+++ bin/CoreData/Shaders/GLSL/v2/_VertexLayout.glsl
@@ -9,32 +9,26 @@
 
 // Vertex position
 VERTEX_INPUT(vec4 iPos)
-
-// Vertex blending for skeletal animation
 #ifdef URHO3D_GEOMETRY_SKINNED
     VERTEX_INPUT(vec4 iBlendWeights)
     VERTEX_INPUT(ivec4_attrib iBlendIndices)
 #endif
+#ifdef URHO3D_NEED_SECONDARY_TEXCOORD
+    VERTEX_INPUT(vec2 iTexCoord1)
+#endif
 
-// Tangent, Normal, Binormal
+// Optional parameters
+#ifdef URHO3D_VERTEX_HAS_TEXCOORD0
+    VERTEX_INPUT(vec2 iTexCoord)
+#endif
+#ifdef URHO3D_VERTEX_HAS_COLOR
+    VERTEX_INPUT(vec4 iColor)
+#endif
 #ifdef URHO3D_VERTEX_HAS_NORMAL
     VERTEX_INPUT(vec3 iNormal)
 #endif
 #ifdef URHO3D_VERTEX_HAS_TANGENT
     VERTEX_INPUT(vec4 iTangent)
-#endif
-
-// Texture coordinates
-#ifdef URHO3D_VERTEX_HAS_TEXCOORD0
-    VERTEX_INPUT(vec2 iTexCoord)
-#endif
-#ifdef URHO3D_VERTEX_HAS_TEXCOORD1
-    VERTEX_INPUT(vec2 iTexCoord1)
-#endif
-
-// Vertex color
-#ifdef URHO3D_VERTEX_HAS_COLOR
-    VERTEX_INPUT(vec4 iColor)
 #endif
 
 #endif // URHO3D_VERTEX_SHADER
