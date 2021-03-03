d76ee6b291ca74be80d58a1942113c7b4cd5e76b
Eugene Kozlov <nekoeuge@gmail.com>
2021-03-03 16:49:39 +0300

    Cleanup pipeline settings

--- Source/Urho3D/RenderPipeline/CommonSettings.h
+++ Source/Urho3D/RenderPipeline/CommonSettings.h
@@ -29,20 +29,20 @@
 namespace Urho3D
 {
 
-/// Drawable processor settings.
 struct DrawableProcessorSettings
 {
-    /// Material quality.
     MaterialQuality materialQuality_{ QUALITY_HIGH };
-    /// Max number of vertex lights.
+    // TODO(renderer): Make maxVertexLights_ contribute to pipeline state
     unsigned maxVertexLights_{ 4 };
-    /// Max number of pixel lights.
     unsigned maxPixelLights_{ 4 };
 
-    /// Calculate pipeline state hash.
-    unsigned CalculatePipelineStateHash() const { return 0; }
+    /// Utility operators
+    /// @{
+    unsigned CalculatePipelineStateHash() const
+    {
+        return 0;
+    }
 
-    /// Compare settings.
     bool operator==(const DrawableProcessorSettings& rhs) const
     {
         return materialQuality_ == rhs.materialQuality_
@@ -50,63 +50,54 @@ struct DrawableProcessorSettings
             && maxPixelLights_ == rhs.maxPixelLights_;
     }
 
-    /// Compare settings.
     bool operator!=(const DrawableProcessorSettings& rhs) const { return !(*this == rhs); }
+    /// @}
 };
 
-/// Instancing buffer compositor settings.
 struct InstancingBufferSettings
 {
-    /// Whether to enable instanching.
     // TODO(renderer): Make true when implemented
-    bool enable_{ false };
-    /// First UV element that can be used by instancing.
-    unsigned firstUnusedTexCoord_{};
-    /// Number of elements reserved for internal usage.
-    unsigned numReservedElems_{};
+    bool enableInstancing_{ false };
+    unsigned firstInstancingTexCoord_{};
+    unsigned numInstancingTexCoords_{};
 
-    /// Calculate pipeline state hash.
+    /// Utility operators
+    /// @{
     unsigned CalculatePipelineStateHash() const
     {
         unsigned hash = 0;
-        CombineHash(hash, enable_);
-        CombineHash(hash, firstUnusedTexCoord_);
-        CombineHash(hash, numReservedElems_);
+        CombineHash(hash, enableInstancing_);
+        CombineHash(hash, firstInstancingTexCoord_);
+        CombineHash(hash, numInstancingTexCoords_);
         return hash;
     }
 
-    /// Compare settings.
     bool operator==(const InstancingBufferSettings& rhs) const
     {
-        return enable_ == rhs.enable_
-            && firstUnusedTexCoord_ == rhs.firstUnusedTexCoord_
-            && numReservedElems_ == rhs.numReservedElems_;
+        return enableInstancing_ == rhs.enableInstancing_
+            && firstInstancingTexCoord_ == rhs.firstInstancingTexCoord_
+            && numInstancingTexCoords_ == rhs.numInstancingTexCoords_;
     }
 
-    /// Compare settings.
     bool operator!=(const InstancingBufferSettings& rhs) const { return !(*this == rhs); }
+    /// @}
 };
 
-/// Ambient lighting mode.
-enum class AmbientMode
+enum class DrawableAmbientMode
 {
     Constant,
     Flat,
     Directional,
 };
 
-/// Batch renderer settings.
 struct BatchRendererSettings
 {
-    /// Whether to apply gamma correction.
     bool gammaCorrection_{};
+    DrawableAmbientMode ambientMode_{ DrawableAmbientMode::Directional };
+    Vector2 varianceShadowMapParams_{ 0.0000001f, 0.9f };
 
-    /// Ambient mode.
-    AmbientMode ambientMode_{ AmbientMode::Directional };
-    /// Variance shadow map parameters.
-    Vector2 vsmShadowParams_{ 0.0000001f, 0.9f };
-
-    /// Calculate pipeline state hash.
+    /// Utility operators
+    /// @{
     unsigned CalculatePipelineStateHash() const
     {
         unsigned hash = 0;
@@ -115,111 +106,110 @@ struct BatchRendererSettings
         return hash;
     }
 
-    /// Compare settings.
     bool operator==(const BatchRendererSettings& rhs) const
     {
         return gammaCorrection_ == rhs.gammaCorrection_
             && ambientMode_ == rhs.ambientMode_
-            && vsmShadowParams_ == rhs.vsmShadowParams_;
+            && varianceShadowMapParams_ == rhs.varianceShadowMapParams_;
     }
 
-    /// Compare settings.
     bool operator!=(const BatchRendererSettings& rhs) const { return !(*this == rhs); }
+    /// @}
 };
 
-/// Shadow map allocator settings.
 struct ShadowMapAllocatorSettings
 {
-    /// Whether to use Variance Shadow Maps
-    bool varianceShadowMap_{};
-    /// Multisampling level of Variance Shadow Maps.
+    bool enableVarianceShadowMaps_{};
     int varianceShadowMapMultiSample_{ 1 };
-    /// Whether to use low precision 16-bit depth maps.
-    bool lowPrecisionShadowMaps_{};
-    /// Size of shadow map atlas page.
-    unsigned shadowMapPageSize_{ 2048 };
+    bool use16bitShadowMaps_{};
+    unsigned shadowAtlasPageSize_{ 2048 };
 
-    /// Calculate pipeline state hash.
+    /// Utility operators
+    /// @{
     unsigned CalculatePipelineStateHash() const
     {
         unsigned hash = 0;
-        CombineHash(hash, varianceShadowMap_);
-        CombineHash(hash, lowPrecisionShadowMaps_);
+        CombineHash(hash, enableVarianceShadowMaps_);
+        CombineHash(hash, use16bitShadowMaps_);
         return hash;
     }
 
-    /// Compare settings.
     bool operator==(const ShadowMapAllocatorSettings& rhs) const
     {
-        return varianceShadowMap_ == rhs.varianceShadowMap_
+        return enableVarianceShadowMaps_ == rhs.enableVarianceShadowMaps_
             && varianceShadowMapMultiSample_ == rhs.varianceShadowMapMultiSample_
-            && lowPrecisionShadowMaps_ == rhs.lowPrecisionShadowMaps_
-            && shadowMapPageSize_ == rhs.shadowMapPageSize_;
+            && use16bitShadowMaps_ == rhs.use16bitShadowMaps_
+            && shadowAtlasPageSize_ == rhs.shadowAtlasPageSize_;
     }
 
-    /// Compare settings.
     bool operator!=(const ShadowMapAllocatorSettings& rhs) const { return !(*this == rhs); }
+    /// @}
 };
 
-/// Scene processor settings.
-struct SceneProcessorSettings
+struct OcclusionBufferSettings
 {
-    /// Default VSM shadow params.
-    static const Vector2 DefaultVSMShadowParams;
-
-    /// Drawable processor settings.
-    DrawableProcessorSettings drawableProcessing_;
-    /// Shadow map allocator settings.
-    ShadowMapAllocatorSettings shadowMap_;
-    /// Instancing buffer settings.
-    InstancingBufferSettings instancing_;
-    /// Batch rendering settings.
-    BatchRendererSettings rendering_;
-
-    /// Whether to render shadows.
-    bool enableShadows_{ true };
-    /// Whether to enable deferred rendering.
-    bool deferred_{ false };
-
-    /// Whether to render occlusion triangles in multiple threads.
     bool threadedOcclusion_{};
-    /// Max number of occluder triangles.
     unsigned maxOccluderTriangles_{ 5000 };
-    /// Occlusion buffer width.
     unsigned occlusionBufferSize_{ 256 };
-    /// Occluder screen size threshold.
     float occluderSizeThreshold_{ 0.025f };
 
-    /// Calculate pipeline state hash.
+    /// Utility operators
+    /// @{
     unsigned CalculatePipelineStateHash() const
     {
-        unsigned hash = 0;
-        CombineHash(hash, drawableProcessing_.CalculatePipelineStateHash());
-        CombineHash(hash, shadowMap_.CalculatePipelineStateHash());
-        CombineHash(hash, instancing_.CalculatePipelineStateHash());
-        CombineHash(hash, rendering_.CalculatePipelineStateHash());
-        CombineHash(hash, enableShadows_);
-        CombineHash(hash, deferred_);
-        return hash;
+        return 0;
     }
 
-    /// Compare settings.
-    bool operator==(const SceneProcessorSettings& rhs) const
+    bool operator==(const OcclusionBufferSettings& rhs) const
     {
-        return drawableProcessing_ == rhs.drawableProcessing_
-            && shadowMap_ == rhs.shadowMap_
-            && instancing_ == rhs.instancing_
-            && rendering_ == rhs.rendering_
-            && enableShadows_ == rhs.enableShadows_
-            && deferred_ == rhs.deferred_
-            && threadedOcclusion_ == rhs.threadedOcclusion_
+        return threadedOcclusion_ == rhs.threadedOcclusion_
             && maxOccluderTriangles_ == rhs.maxOccluderTriangles_
             && occlusionBufferSize_ == rhs.occlusionBufferSize_
             && occluderSizeThreshold_ == rhs.occluderSizeThreshold_;
     }
 
-    /// Compare settings.
+    bool operator!=(const OcclusionBufferSettings& rhs) const { return !(*this == rhs); }
+    /// @}
+};
+
+struct SceneProcessorSettings
+    : public DrawableProcessorSettings
+    , public OcclusionBufferSettings
+    , public ShadowMapAllocatorSettings
+    , public InstancingBufferSettings
+    , public BatchRendererSettings
+{
+    bool enableShadows_{ true };
+    bool deferredLighting_{ false };
+
+    /// Utility operators
+    /// @{
+    unsigned CalculatePipelineStateHash() const
+    {
+        unsigned hash = 0;
+        CombineHash(hash, DrawableProcessorSettings::CalculatePipelineStateHash());
+        CombineHash(hash, OcclusionBufferSettings::CalculatePipelineStateHash());
+        CombineHash(hash, ShadowMapAllocatorSettings::CalculatePipelineStateHash());
+        CombineHash(hash, InstancingBufferSettings::CalculatePipelineStateHash());
+        CombineHash(hash, BatchRendererSettings::CalculatePipelineStateHash());
+        CombineHash(hash, enableShadows_);
+        CombineHash(hash, deferredLighting_);
+        return hash;
+    }
+
+    bool operator==(const SceneProcessorSettings& rhs) const
+    {
+        return DrawableProcessorSettings::operator==(rhs)
+            && OcclusionBufferSettings::operator==(rhs)
+            && ShadowMapAllocatorSettings::operator==(rhs)
+            && InstancingBufferSettings::operator==(rhs)
+            && BatchRendererSettings::operator==(rhs)
+            && enableShadows_ == rhs.enableShadows_
+            && deferredLighting_ == rhs.deferredLighting_;
+    }
+
     bool operator!=(const SceneProcessorSettings& rhs) const { return !(*this == rhs); }
+    /// @}
 };
 
 }
