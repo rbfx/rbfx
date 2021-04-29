01c0bb07e07fa96a7cf82358b636e9879584d5af
Eugene Kozlov <nekoeuge@gmail.com>
2021-04-29 18:45:09 +0300

    Simplify URHO3D_LEGACY_RENDERER handling.

--- cmake/Modules/UrhoOptions.cmake
+++ cmake/Modules/UrhoOptions.cmake
@@ -160,7 +160,11 @@ if (URHO3D_GLES2 OR URHO3D_GLES3)
 endif ()
 
 cmake_dependent_option(URHO3D_SPIRV "Enable universal GLSL shaders for other GAPIs via glslang and SpirV" ON "URHO3D_D3D11" OFF)
+# Whether to use legacy renderer. DX11 doesn't support legacy renderer. DX9 supports only legacy renderer.
 cmake_dependent_option(URHO3D_LEGACY_RENDERER "Use legacy renderer by default" OFF "URHO3D_OPENGL" OFF)
+if (URHO3D_D3D9)
+    set (URHO3D_LEGACY_RENDERER ON)
+endif ()
 
 if (URHO3D_CSHARP)
     set (URHO3D_MONOLITHIC_HEADER ON)   # Used by wrapper code
