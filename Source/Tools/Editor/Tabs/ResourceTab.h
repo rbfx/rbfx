4d8e1bf701f56d24b710804b2b1cd2c144a3c1eb
Rokas Kupstys <rokups@zoho.com>
2021-05-05 19:02:44 +0300

    Editor: Rescan resources folder only when navigating or files in that folder change.

--- Source/Tools/Editor/Tabs/ResourceTab.h
+++ Source/Tools/Editor/Tabs/ResourceTab.h
@@ -69,6 +69,10 @@ protected:
     /// Select current item in attribute inspector.
     void SelectCurrentItemInspector();
     ///
+    void OnEndFrame(StringHash, VariantMap&);
+    ///
+    void OnResourceUpdated(const FileChange& change);
+    ///
     void ScanAssets();
     ///
     void OpenResource(const ea::string& resourceName);
@@ -99,8 +103,6 @@ protected:
     bool rescan_ = true;
     /// Flag requesting to scroll to selection on next frame.
     bool scrollToCurrent_ = false;
-    /// Timer for periodic selected resource path rescan.
-    Timer rescanTimer_;
     /// State cache.
     ValueCache cache_{context_};
     /// Frame at which rename started. Non-zero means rename is in-progress.
