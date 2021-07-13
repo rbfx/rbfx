73299a9678cbdd4ecc4f88c6edb5799f778c8750
Eugene Kozlov <nekoeuge@gmail.com>
2021-07-14 00:57:02 +0300

    Fix broken debugging in MSVC

--- cmake/Modules/CCache.cmake
+++ cmake/Modules/CCache.cmake
@@ -50,6 +50,6 @@ if (CCACHE_FOUND AND ENV{CCACHE_DIR})
     endif ()
 endif ()
 
-if (MSVC AND NOT ENV{CLCACHE_DIR} STREQUAL "")
+if (MSVC AND (DEFINED ENV{CLCACHE_DIR} AND NOT ENV{CLCACHE_DIR} STREQUAL ""))
     ucm_replace_flag("/Z[iI]" "" REGEX CONFIG Debug RelWithDebInfo)
 endif ()
