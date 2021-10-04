rbfx/rbfx

commit 444115d31f603314f5298f02f965bfce46f1cac0

Editor: Rework node reordering, add component reordering. Scene: Fix component reordering when specified index is after current index. Scene: Add node reordering (similar api to already existing component reordering).

Source/Tools/Editor/Tabs/Scene/SceneTab.cpp
@@ -529,9 +529,15 @@ void SceneTab::RenderHierarchy()
     if (performRangeSelectionFrame_ != ui::GetFrameCount())
         selectionRect_ = {ui::GetMousePos(), ui::GetMousePos()};
 
+    // Clear ID of currently reordered item.
+    if (!ui::IsMouseDown(MOUSEB_LEFT))
+        reorderingId_ = M_MAX_UNSIGNED;
+
     if (auto* scene = GetScene())
     {
+        ImGuiStyle& style = ui::GetStyle();
         ui::PushStyleVar(ImGuiStyleVar_IndentSpacing, 10);
+        ui::PushStyleVar(ImGuiStyleVar_ItemSpacing, ImVec2(style.ItemSpacing.x, 0));
         RenderNodeTree(scene);
         ui::PopStyleVar();
     }
@@ -553,7 +559,8 @@ void SceneTab::RenderNodeTree(Node* node)
 
     ImGuiTreeNodeFlags flags = ImGuiTreeNodeFlags_OpenOnArrow
         | ImGuiTreeNodeFlags_OpenOnDoubleClick
-        | ImGuiTreeNodeFlags_SpanAvailWidth;
+        | ImGuiTreeNodeFlags_SpanAvailWidth
+        | ImGuiTreeNodeFlags_AllowItemOverlap;
     if (node->GetParent() == nullptr)
         flags |= ImGuiTreeNodeFlags_DefaultOpen;
 
@@ -604,6 +611,27 @@ void SceneTab::RenderNodeTree(Node* node)
             }
         }
         ui::EndDragDropTarget();
+    }
+
+    // Node reordering.
+    Node* parent = node->GetParent();
+    float utility_buttons_width = ui::CalcTextSize(ICON_FA_ARROWS_ALT_V).x;
+    if (parent != nullptr && (ui::IsItemHovered() || reorderingId_ == node->GetID()))
+    {
+        ui::SameLine();
+        ui::SetCursorPosX(ui::GetContentRegionMax().x - utility_buttons_width - style.ItemInnerSpacing.x);
+        ui::SmallButton(ICON_FA_ARROWS_ALT_V);
+        if (ui::IsItemActive())
+        {
+            reorderingId_ = node->GetID();
+            ImVec2 mouse_pos = ui::GetMousePos();
+            if (mouse_pos.y < ui::GetItemRectMin().y)
+                parent->ReorderChild(node, parent->GetChildIndex(node) - 1);
+            else if (mouse_pos.y > ui::GetItemRectMax().y)
+                parent->ReorderChild(node, parent->GetChildIndex(node) + 1);
+        }
+        else if (ui::IsItemHovered())
+            ui::SetTooltip("Reorder");
     }
 
     if (!opened)
@@ -673,7 +701,7 @@ void SceneTab::RenderNodeTree(Node* node)
                 ui::SameLine();
 
                 bool selected = selectedComponents_.contains(component);
-                ui::Selectable(component->GetTypeName().c_str(), selected);
+                ui::Selectable(component->GetTypeName().c_str(), selected, ImGuiSelectableFlags_AllowItemOverlap);
 
                 if (ui::IsItemHovered(ImGuiHoveredFlags_AllowWhenBlockedByPopup))
                 {
@@ -694,6 +722,24 @@ void SceneTab::RenderNodeTree(Node* node)
                     }
                 }
 
+                // Component reordering.
+                if (ui::IsItemHovered() || reorderingId_ == component->GetID())
+                {
+                    ui::SameLine();
+                    ui::SetCursorPosX(ui::GetContentRegionMax().x - utility_buttons_width - style.ItemInnerSpacing.x);
+                    ui::SmallButton(ICON_FA_ARROWS_ALT_V);
+                    if (ui::IsItemActive())
+                    {
+                        reorderingId_ = component->GetID();
+                        ImVec2 mouse_pos = ui::GetMousePos();
+                        if (mouse_pos.y < ui::GetItemRectMin().y)
+                            node->ReorderComponent(component, node->GetComponentIndex(component) - 1);
+                        else if (mouse_pos.y > ui::GetItemRectMax().y)
+                            node->ReorderComponent(component, node->GetComponentIndex(component) + 1);
+                    }
+                    else if (ui::IsItemHovered())
+                        ui::SetTooltip("Reorder");
+                }
 
                 RenderNodeContextMenu();
 
@@ -720,27 +766,6 @@ void SceneTab::RenderNodeTree(Node* node)
 
                 //recursive call.
                 RenderNodeTree(child);
-
-                // Reorder nodes: add after current child node.
-                ui::SetCursorPosY(ui::GetCursorPosY() - 2 * style.PointSize);
-                ui::PushStyleVar(ImGuiStyleVar_ItemSpacing, ImVec2(style.ItemSpacing.x, 0));
-                ui::Dummy(ImVec2(ui::GetItemRectSize().x, 3 * style.PointSize));
-                ui::PopStyleVar();
-                if (ui::BeginDragDropTarget())
-                {
-                    // TODO: This is a workaround for imgui expanding drop rect by fixed amount that we cant control.
-                    float point_size_bkp = style.PointSize;
-                    style.PointSize = 0;
-                    const Variant& payload = ui::AcceptDragDropVariant(Node::GetTypeStatic().ToString());
-                    style.PointSize = point_size_bkp;
-                    if (!payload.IsEmpty())
-                    {
-                        SharedPtr<Node> dropped(dynamic_cast<Node*>(payload.GetPtr()));
-                        node->AddChild(dropped);
-                        dropped->SetIndexInParent(node->GetChildIndex(child) + 1);
-                    }
-                    ui::EndDragDropTarget();
-                }
             }
         }
         ui::TreePop();

Source/Tools/Editor/Tabs/Scene/SceneTab.h
@@ -242,6 +242,8 @@ protected:
     ImDrawListSplitter viewportSplitter_{};
     /// Distance from the camera that manipulator will rotate around.
     float rotateAroundDistance_ = 1;
+    /// ID of node or component that is currently being reordered.
+    unsigned reorderingId_ = M_MAX_UNSIGNED;
 };
 
 };

Source/Urho3D/Scene/Node.cpp
@@ -1011,23 +1011,6 @@ void Node::AddChild(Node* node, unsigned index)
     }
 }
 
-void Node::SetIndexInParent(unsigned index)
-{
-    Node* parent = GetParent();
-    if (parent == nullptr)
-        return;
-    
-    SharedPtr<Node> nodeShared(this);
-    size_t old_index = parent->children_.index_of(nodeShared);
-    if (old_index == index)
-        return;
-    if (old_index > index)
-        parent->children_.erase_at(old_index);
-    parent->children_.insert_at(index, nodeShared);
-    if (old_index < index)
-        parent->children_.erase_at(old_index);
-}
-
 void Node::RemoveChild(Node* node)
 {
     if (!node)
@@ -1245,21 +1228,58 @@ void Node::RemoveAllComponents()
     RemoveComponents(true, true);
 }
 
+void Node::ReorderChild(Node* child, unsigned index)
+{
+    if (!child || child->GetParent() != this)
+        return;
+
+    if (index >= children_.size())
+        return;
+
+    // Need shared ptr to insert. Also, prevent destruction when removing first
+    SharedPtr<Node> childShared(child);
+    unsigned i = children_.index_of(childShared);
+
+    if (i > index)
+    {
+        // Inserting node before it's old index. No special handling needed.
+        children_.erase_at(i);
+        children_.insert_at(index, childShared);
+    }
+    else if (i < index)
+    {
+        // Inserting node after it's current position. To maintain expected
+        // position we insert it at index offset by 1, then erasing old node
+        // shifts all indices down by one.
+        children_.insert_at(index + 1, childShared);
+        children_.erase_at(i);
+    }
+}
+
 void Node::ReorderComponent(Component* component, unsigned index)
 {
     if (!component || component->GetNode() != this)
         return;
 
-    for (auto i = components_.begin(); i != components_.end(); ++i)
+    if (index >= components_.size())
+        return;
+
+    SharedPtr<Component> componentShared(component);
+    unsigned i = components_.index_of(componentShared);
+
+    if (i > index)
     {
-        if (i->Get() == component)
-        {
-            // Need shared ptr to insert. Also, prevent destruction when removing first
-            SharedPtr<Component> componentShared(component);
-            components_.erase(i);
-            components_.insert_at(index, componentShared);
-            return;
-        }
+        // Inserting node before it's old index. No special handling needed.
+        components_.erase_at(i);
+        components_.insert_at(index, componentShared);
+    }
+    else if (i < index)
+    {
+        // Inserting component after it's current position. To maintain expected
+        // position we insert it at index offset by 1, then erasing old component
+        // shifts all indices down by one.
+        components_.insert_at(index + 1, componentShared);
+        components_.erase_at(i);
     }
 }
 

Source/Urho3D/Scene/Node.h
@@ -315,8 +315,6 @@ public:
     Node* CreateTemporaryChild(const ea::string& name = EMPTY_STRING, CreateMode mode = REPLICATED, unsigned id = 0);
     /// Add a child scene node at a specific index. If index is not explicitly specified or is greater than current children size, append the new child at the end.
     void AddChild(Node* node, unsigned index = M_MAX_UNSIGNED);
-    /// Move node to specified index in the parent node.
-    void SetIndexInParent(unsigned index = M_MAX_UNSIGNED);
     /// Remove a child scene node.
     void RemoveChild(Node* node);
     /// Remove all child scene nodes.
@@ -341,6 +339,8 @@ public:
     void RemoveComponents(StringHash type);
     /// Remove all components from this node.
     void RemoveAllComponents();
+    /// Adjust index order of an existing child node in this node.
+    void ReorderChild(Node* child, unsigned index);
     /// Adjust index order of an existing component in this node.
     void ReorderComponent(Component* component, unsigned index);
     /// Clone scene node, components and child nodes. Return the clone.

commit a524370429803acdc296d14b0d08417e06b20713

Editor: Fix a crash introduced in 444115d.

Source/Tools/Editor/Tabs/Scene/SceneTab.cpp
@@ -539,7 +539,7 @@ void SceneTab::RenderHierarchy()
         ui::PushStyleVar(ImGuiStyleVar_IndentSpacing, 10);
         ui::PushStyleVar(ImGuiStyleVar_ItemSpacing, ImVec2(style.ItemSpacing.x, 0));
         RenderNodeTree(scene);
-        ui::PopStyleVar();
+        ui::PopStyleVar(2);
     }
 
     // Perform range selection on next frame. Only then we can have a rectangle covering selection ends