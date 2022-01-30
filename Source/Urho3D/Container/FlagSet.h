6e46abaa99da8a2b3c7678df6150611324bb9459
Eugene Kozlov <nekoeuge@gmail.com>
2022-01-30 14:42:06 +0300

    Make FlagSet constexpr.

--- Source/Urho3D/Container/FlagSet.h
+++ Source/Urho3D/Container/FlagSet.h
@@ -29,10 +29,10 @@ namespace Urho3D
 
 /// Make bitwise operators (| & ^ ~) automatically construct FlagSet from Enum.
 #define URHO3D_AUTOMATIC_FLAGSET(Enum) \
-    inline Urho3D::FlagSet<Enum> operator | (const Enum lhs, const Enum rhs) { return Urho3D::FlagSet<Enum>(lhs) | rhs; } \
-    inline Urho3D::FlagSet<Enum> operator & (const Enum lhs, const Enum rhs) { return Urho3D::FlagSet<Enum>(lhs) & rhs; } \
-    inline Urho3D::FlagSet<Enum> operator ^ (const Enum lhs, const Enum rhs) { return Urho3D::FlagSet<Enum>(lhs) ^ rhs; } \
-    inline Urho3D::FlagSet<Enum> operator ~ (const Enum rhs) { return ~Urho3D::FlagSet<Enum>(rhs); }
+    constexpr inline Urho3D::FlagSet<Enum> operator | (const Enum lhs, const Enum rhs) { return Urho3D::FlagSet<Enum>(lhs) | rhs; } \
+    constexpr inline Urho3D::FlagSet<Enum> operator & (const Enum lhs, const Enum rhs) { return Urho3D::FlagSet<Enum>(lhs) & rhs; } \
+    constexpr inline Urho3D::FlagSet<Enum> operator ^ (const Enum lhs, const Enum rhs) { return Urho3D::FlagSet<Enum>(lhs) ^ rhs; } \
+    constexpr inline Urho3D::FlagSet<Enum> operator ~ (const Enum rhs) { return ~Urho3D::FlagSet<Enum>(rhs); }
 
 /// Declare FlagSet for specific enum and create operators for automatic FlagSet construction.
 #define URHO3D_FLAGSET(enumName, flagsetName) \
@@ -51,129 +51,129 @@ public:
 
 public:
     /// Ctor by integer.
-    explicit FlagSet(Integer value)
+    constexpr explicit FlagSet(Integer value)
         : value_(value)
     {
     }
     /// Empty constructor.
-    FlagSet() = default;
+    constexpr FlagSet() = default;
 
     /// Copy constructor.
-    FlagSet(const FlagSet& another) = default;
+    constexpr FlagSet(const FlagSet& another) = default;
 
     /// Construct from Enum value.
-    FlagSet(const Enum value)
+    constexpr FlagSet(const Enum value)
         : value_(static_cast<Integer>(value))
     {
     }
 
     /// Assignment operator from flagset.
-    FlagSet& operator = (const FlagSet& rhs) = default;
+    constexpr FlagSet& operator = (const FlagSet& rhs) = default;
 
     /// Bitwise AND against Enum value.
-    FlagSet& operator &= (const Enum value)
+    constexpr FlagSet& operator &= (const Enum value)
     {
         value_ &= static_cast<Integer>(value);
         return *this;
     }
 
     /// Bitwise AND against flagset value.
-    FlagSet& operator &= (const FlagSet value)
+    constexpr FlagSet& operator &= (const FlagSet value)
     {
         value_ &= value.value_;
         return *this;
     }
 
     /// Bitwise OR against Enum value.
-    FlagSet& operator |= (const Enum value)
+    constexpr FlagSet& operator |= (const Enum value)
     {
         value_ |= static_cast<Integer>(value);
         return *this;
     }
 
     /// Bitwise OR against flagset value.
-    FlagSet& operator |= (const FlagSet value)
+    constexpr FlagSet& operator |= (const FlagSet value)
     {
         value_ |= value.value_;
         return *this;
     }
 
     /// Bitwise XOR against Enum value.
-    FlagSet& operator ^= (const Enum value)
+    constexpr FlagSet& operator ^= (const Enum value)
     {
         value_ ^= static_cast<Integer>(value);
         return *this;
     }
 
     /// Bitwise XOR against flagset value.
-    FlagSet& operator ^= (const FlagSet value)
+    constexpr FlagSet& operator ^= (const FlagSet value)
     {
         value_ ^= value.value_;
         return *this;
     }
 
     /// Bitwise AND against Enum value.
-    FlagSet operator & (const Enum value) const
+    constexpr FlagSet operator & (const Enum value) const
     {
         return FlagSet(value_ & static_cast<Integer>(value));
     }
 
     /// Bitwise AND against flagset value.
-    FlagSet operator & (const FlagSet value) const
+    constexpr FlagSet operator & (const FlagSet value) const
     {
         return FlagSet(value_ & value.value_);
     }
 
     /// Bitwise OR against Enum value.
-    FlagSet operator | (const Enum value) const
+    constexpr FlagSet operator | (const Enum value) const
     {
         return FlagSet(value_ | static_cast<Integer>(value));
     }
 
     /// Bitwise OR against flagset value.
-    FlagSet operator | (const FlagSet value) const
+    constexpr FlagSet operator | (const FlagSet value) const
     {
         return FlagSet(value_ | value.value_);
     }
 
     /// Bitwise XOR against Enum value.
-    FlagSet operator ^ (const Enum value) const
+    constexpr FlagSet operator ^ (const Enum value) const
     {
         return FlagSet(value_ ^ static_cast<Integer>(value));
     }
 
     /// Bitwise XOR against flagset value.
-    FlagSet operator ^ (const FlagSet value) const
+    constexpr FlagSet operator ^ (const FlagSet value) const
     {
         return FlagSet(value_ ^ value.value_);
     }
 
     /// Bitwise negation.
-    FlagSet operator ~ () const
+    constexpr FlagSet operator ~ () const
     {
         return FlagSet(~value_);
     }
 
     /// Boolean negation.
-    bool operator ! () const
+    constexpr bool operator ! () const
     {
         return !value_;
     }
 
     /// Returns true if any flag is set.
-    explicit operator bool () const
+    constexpr explicit operator bool () const
     {
         return value_ != 0;
     }
 
     /// Cast to underlying type of enum.
-    operator Integer() const
+    constexpr operator Integer() const
     {
         return value_;
     }
 
     /// Cast to enum value.
-    explicit operator Enum() const
+    constexpr explicit operator Enum() const
     {
         return static_cast<Enum>(value_);
     }
@@ -185,44 +185,44 @@ public:
     }
 
     /// Equality check against enum value.
-    bool operator ==(Enum rhs) const
+    constexpr bool operator ==(Enum rhs) const
     {
         return value_ == static_cast<Integer>(rhs);
     }
 
     /// Equality check against another flagset.
-    bool operator ==(FlagSet rhs) const
+    constexpr bool operator ==(FlagSet rhs) const
     {
         return value_ == rhs.value_;
     }
 
     /// Non-equality check against enum value.
-    bool operator !=(Enum rhs) const
+    constexpr bool operator !=(Enum rhs) const
     {
         return !(*this == rhs);
     }
 
     /// Non-equality check against another flagset value.
-    bool operator !=(FlagSet rhs) const
+    constexpr bool operator !=(FlagSet rhs) const
     {
         return !(*this == rhs);
     }
 
     /// Return true if specified enum value is set.
-    inline bool Test(const Enum value) const
+    constexpr bool Test(const Enum value) const
     {
         return Test(static_cast<Integer>(value));
     }
 
     /// Return true if specified bits are set.
-    inline bool Test(const Integer flags) const
+    constexpr bool Test(const Integer flags) const
     {
         return (value_ & flags) == flags && (flags != 0 || value_ == flags);
     }
 
     /// Set or unset specified subset of flags
     /// @{
-    void Set(const Integer flags, bool enabled = true)
+    constexpr void Set(const Integer flags, bool enabled = true)
     {
         if (enabled)
             value_ |= flags;
@@ -230,20 +230,20 @@ public:
             value_ &= ~flags;
     }
 
-    void Set(const Enum value, bool enabled = true)
+    constexpr void Set(const Enum value, bool enabled = true)
     {
         Set(static_cast<Integer>(value), enabled);
     }
     /// @}
 
     /// Return underlying integer (constant).
-    Integer AsInteger() const { return value_; }
+    constexpr Integer AsInteger() const { return value_; }
 
     /// Return underlying integer (non-constant).
-    Integer& AsInteger() { return value_; }
+    constexpr Integer& AsInteger() { return value_; }
 
     /// Return hash value.
-    unsigned ToHash() const { return static_cast<unsigned>(value_); }
+    constexpr unsigned ToHash() const { return static_cast<unsigned>(value_); }
 
 protected:
     /// Value.
