fa38416d4cedac537786a8947319d8bad7fcc194
Gleb Lebedev <gleb@gleblebedev.com>
2022-01-31 21:43:12 +0000

    Sample

---
+++ bin/CoreData/Shaders/GLSL/v2/Refraction.glsl
@@ -0,0 +1,53 @@
+#define URHO3D_PIXEL_NEED_EYE_VECTOR
+#define URHO3D_SURFACE_NEED_BACKGROUND_COLOR
+#ifdef URHO3D_HAS_READABLE_DEPTH
+    #define URHO3D_SURFACE_NEED_BACKGROUND_DEPTH
+#endif
+#define URHO3D_SURFACE_NEED_NORMAL
+#define URHO3D_SURFACE_NEED_NORMAL_IN_TANGENT_SPACE
+#define URHO3D_CUSTOM_MATERIAL_UNIFORMS
+
+#ifndef NORMALMAP
+    #define NORMALMAP
+#endif
+
+#ifndef ENVCUBEMAP
+    #define ENVCUBEMAP
+#endif
+
+#include "_Config.glsl"
+#include "_Uniforms.glsl"
+
+UNIFORM_BUFFER_BEGIN(4, Material)
+    DEFAULT_MATERIAL_UNIFORMS
+    UNIFORM(half cNoiseStrength)
+UNIFORM_BUFFER_END(4, Material)
+
+#include "_Material.glsl"
+
+#ifdef URHO3D_VERTEX_SHADER
+void main()
+{
+    VertexTransform vertexTransform = GetVertexTransform();
+    FillVertexOutputs(vertexTransform);
+}
+#endif
+
+#ifdef URHO3D_PIXEL_SHADER
+void main()
+{
+    SurfaceData surfaceData;
+
+    FillSurfaceCommon(surfaceData);
+    FillSurfaceNormal(surfaceData);
+    FillSurfaceAlbedoSpecular(surfaceData);
+
+    // Apply noise to screen position used for background sampling
+    half2 distortionDistance = surfaceData.normalInTangentSpace.xy * (cNoiseStrength * surfaceData.albedo.a);
+    surfaceData.screenPos += distortionDistance;
+
+    FillSurfaceBackground(surfaceData);
+
+    gl_FragColor = vec4(surfaceData.backgroundColor,1);
+}
+#endif
