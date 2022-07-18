980d52457a95b8135328eac947fa04f165d0ea1d
Eugene Kozlov <nekoeuge@gmail.com>
2022-07-18 12:26:36 +0300

    Extract code to SceneHierarchyWidget.

--- Source/Tools/Editor/Foundation/SceneViewTab/SceneHierarchy.cpp
+++ Source/Tools/Editor/Foundation/SceneViewTab/SceneHierarchy.cpp
@@ -56,21 +56,28 @@ void Foundation_SceneHierarchy(Context* context, SceneViewTab* sceneViewTab)
 
 SceneHierarchy::SceneHierarchy(SceneViewTab* sceneViewTab)
     : SceneViewAddon(sceneViewTab)
+    , widget_(MakeShared<SceneHierarchyWidget>(context_))
 {
 }
 
 void SceneHierarchy::WriteIniSettings(ImGuiTextBuffer& output)
 {
-    WriteIntToIni(output, "SceneHierarchy.ShowComponents", showComponents_);
-    WriteIntToIni(output, "SceneHierarchy.ShowTemporary", showTemporary_);
+    const SceneHierarchySettings& settings = widget_->GetSettings();
+    WriteIntToIni(output, "SceneHierarchy.ShowComponents", settings.showComponents_);
+    WriteIntToIni(output, "SceneHierarchy.ShowTemporary", settings.showTemporary_);
+    WriteStringToIni(output, "SceneHierarchy.FilterByName", settings.filterByName_);
 }
 
 void SceneHierarchy::ReadIniSettings(const char* line)
 {
+    SceneHierarchySettings settings;
     if (const auto value = ReadIntFromIni(line, "SceneHierarchy.ShowComponents"))
-        showComponents_ = *value != 0;
+        settings.showComponents_ = *value != 0;
     if (const auto value = ReadIntFromIni(line, "SceneHierarchy.ShowTemporary"))
-        showTemporary_ = *value != 0;
+        settings.showTemporary_ = *value != 0;
+    if (const auto value = ReadStringFromIni(line, "SceneHierarchy.FilterByName"))
+        settings.filterByName_ = *value;
+    widget_->SetSettings(settings);
 }
 
 void SceneHierarchy::RenderContent()
@@ -80,26 +87,7 @@ void SceneHierarchy::RenderContent()
         return;
 
     RenderToolbar(*activePage);
-
-    BeginRangeSelection();
-
-    const ImGuiStyle& style = ui::GetStyle();
-    ui::PushStyleVar(ImGuiStyleVar_ItemSpacing, ImVec2(style.ItemSpacing.x, 0));
-    if (search_.lastQuery_.empty())
-    {
-        RenderNode(*activePage, activePage->scene_);
-    }
-    else
-    {
-        for (Node* node : search_.lastResults_)
-        {
-            if (node && (showTemporary_ || !node->IsTemporaryEffective()))
-                RenderNode(*activePage, node);
-        }
-    }
-    ui::PopStyleVar();
-
-    EndRangeSelection(*activePage);
+    widget_->RenderContent(activePage->scene_, activePage->selection_);
 }
 
 void SceneHierarchy::RenderContextMenuItems()
@@ -129,201 +117,20 @@ void SceneHierarchy::ApplyHotkeys(HotkeyManager* hotkeyManager)
 
 void SceneHierarchy::RenderToolbar(SceneViewPage& page)
 {
-    if (Widgets::ToolbarButton(ICON_FA_CLOCK, "Show Temporary Nodes & Components", showTemporary_))
-        showTemporary_ = !showTemporary_;
-    if (Widgets::ToolbarButton(ICON_FA_DIAGRAM_PROJECT, "Show Components", showComponents_))
-        showComponents_ = !showComponents_;
+    SceneHierarchySettings settings = widget_->GetSettings();
+
+    if (Widgets::ToolbarButton(ICON_FA_CLOCK, "Show Temporary Nodes & Components", settings.showTemporary_))
+        settings.showTemporary_ = !settings.showTemporary_;
+    if (Widgets::ToolbarButton(ICON_FA_DIAGRAM_PROJECT, "Show Components", settings.showComponents_))
+        settings.showComponents_ = !settings.showComponents_;
 
     ui::BeginDisabled();
     Widgets::ToolbarButton(ICON_FA_MAGNIFYING_GLASS);
     ui::EndDisabled();
 
-    const bool sceneChanged = search_.lastScene_ != page.scene_;
-    const bool queryChanged = ui::InputText("##Rename", &search_.currentQuery_);
-    if (queryChanged || sceneChanged)
-        UpdateSearchResults(page);
-}
+    ui::InputText("##Rename", &settings.filterByName_);
 
-void SceneHierarchy::RenderNode(SceneViewPage& page, Node* node)
-{
-    if (!showTemporary_ && node->IsTemporary())
-        return;
-
-    UpdateActiveObjectVisibility(page, node);
-
-    const bool isEmpty = node->GetChildren().empty() && (!showComponents_ || node->GetComponents().empty());
-    ImGuiTreeNodeFlags flags = ImGuiTreeNodeFlags_OpenOnArrow
-        | ImGuiTreeNodeFlags_OpenOnDoubleClick
-        | ImGuiTreeNodeFlags_SpanAvailWidth
-        | ImGuiTreeNodeFlags_AllowItemOverlap;
-    if (node->GetParent() == nullptr)
-        flags |= ImGuiTreeNodeFlags_DefaultOpen;
-    if (page.selection_.IsSelected(node))
-        flags |= ImGuiTreeNodeFlags_Selected;
-    if (isEmpty)
-        flags |= ImGuiTreeNodeFlags_Leaf;
-
-    const IdScopeGuard guard(static_cast<void*>(node));
-    const bool opened = ui::TreeNodeEx(GetNodeTitle(node).c_str(), flags);
-    ProcessRangeSelection(node, opened);
-
-    if ((ui::IsItemClicked(MOUSEB_LEFT) || ui::IsItemClicked(MOUSEB_RIGHT)) && !ui::IsItemToggledOpen())
-    {
-        const bool toggleSelect = ui::IsKeyDown(KEY_CTRL);
-        const bool rangeSelect = ui::IsKeyDown(KEY_SHIFT);
-        ProcessObjectSelected(page, node, toggleSelect, rangeSelect);
-    }
-
-    if (opened)
-    {
-        if (showComponents_)
-        {
-            for (Component* component : node->GetComponents())
-                RenderComponent(page, component);
-        }
-
-        for (Node* child : node->GetChildren())
-            RenderNode(page, child);
-
-        ui::TreePop();
-    }
-}
-
-void SceneHierarchy::RenderComponent(SceneViewPage& page, Component* component)
-{
-    if (component->IsTemporary() && !showTemporary_)
-        return;
-
-    UpdateActiveObjectVisibility(page, component);
-
-    ImGuiTreeNodeFlags flags = ImGuiTreeNodeFlags_OpenOnArrow
-        | ImGuiTreeNodeFlags_OpenOnDoubleClick
-        | ImGuiTreeNodeFlags_SpanAvailWidth
-        | ImGuiTreeNodeFlags_AllowItemOverlap
-        | ImGuiTreeNodeFlags_Leaf;
-    if (page.selection_.IsSelected(component))
-        flags |= ImGuiTreeNodeFlags_Selected;
-
-    const IdScopeGuard guard(static_cast<void*>(component));
-    const bool opened = ui::TreeNodeEx(component->GetTypeName().c_str(), flags);
-    ProcessRangeSelection(component, opened);
-
-    if (ui::IsItemClicked(MOUSEB_LEFT) || ui::IsItemClicked(MOUSEB_RIGHT))
-    {
-        const bool toggleSelect = ui::IsKeyDown(KEY_CTRL);
-        const bool rangeSelect = ui::IsKeyDown(KEY_SHIFT);
-        ProcessObjectSelected(page, component, toggleSelect, rangeSelect);
-    }
-
-    if (opened)
-        ui::TreePop();
-}
-
-void SceneHierarchy::ProcessObjectSelected(SceneViewPage& page, Object* object, bool toggle, bool range)
-{
-    SceneSelection& selection = page.selection_;
-    Object* activeObject = selection.GetActiveObject();
-
-    if (toggle)
-        selection.SetSelected(object, !selection.IsSelected(object));
-    else if (range && activeObject && wasActiveObjectVisible_ && activeObject != object)
-        rangeSelection_.pendingRequest_ = RangeSelectionRequest{WeakPtr<Object>(activeObject), WeakPtr<Object>(object)};
-    else
-    {
-        selection.Clear();
-        selection.SetSelected(object, true);
-    }
-}
-
-void SceneHierarchy::UpdateActiveObjectVisibility(SceneViewPage& page, Object* currentItem)
-{
-    if (page.selection_.GetActiveObject() == currentItem)
-        isActiveObjectVisible_ = true;
-}
-
-void SceneHierarchy::BeginRangeSelection()
-{
-    wasActiveObjectVisible_ = isActiveObjectVisible_;
-    isActiveObjectVisible_ = false;
-    rangeSelection_.result_.clear();
-    rangeSelection_.isActive_ = false;
-    rangeSelection_.currentRequest_ = rangeSelection_.pendingRequest_;
-    rangeSelection_.pendingRequest_ = ea::nullopt;
-}
-
-void SceneHierarchy::ProcessRangeSelection(Object* currentObject, bool open)
-{
-    if (!rangeSelection_.currentRequest_)
-        return;
-
-    const WeakPtr<Object> weakObject{currentObject};
-    const bool isBorder = rangeSelection_.currentRequest_->IsBorder(currentObject);
-
-    if (!rangeSelection_.isActive_ && isBorder)
-    {
-        rangeSelection_.isActive_ = true;
-        rangeSelection_.result_.push_back(weakObject);
-    }
-    else if (rangeSelection_.isActive_ && isBorder)
-    {
-        rangeSelection_.result_.push_back(weakObject);
-        rangeSelection_.isActive_ = false;
-        rangeSelection_.currentRequest_ = ea::nullopt;
-    }
-    else if (rangeSelection_.isActive_)
-    {
-        rangeSelection_.result_.push_back(weakObject);
-    }
-}
-
-void SceneHierarchy::EndRangeSelection(SceneViewPage& page)
-{
-    rangeSelection_.currentRequest_ = ea::nullopt;
-
-    if (!rangeSelection_.isActive_)
-    {
-        for (Object* object : rangeSelection_.result_)
-            page.selection_.SetSelected(object, true);
-    }
-}
-
-void SceneHierarchy::UpdateSearchResults(SceneViewPage& page)
-{
-    const bool sceneChanged = search_.lastScene_ != page.scene_;
-    search_.lastScene_ = page.scene_;
-
-    // Early return if search was canceled
-    if (search_.currentQuery_.empty())
-    {
-        search_.lastResults_.clear();
-        search_.lastQuery_.clear();
-        return;
-    }
-
-    const bool resultsExpired = sceneChanged
-        || search_.lastResults_.empty()
-        || !search_.lastQuery_.contains(search_.currentQuery_);
-    search_.lastQuery_ = search_.currentQuery_;
-
-    if (resultsExpired)
-    {
-        ea::vector<Node*> children;
-        page.scene_->GetChildren(children, true);
-
-        search_.lastResults_.clear();
-        for (Node* child : children)
-        {
-            if (child->GetName().contains(search_.currentQuery_, false))
-                search_.lastResults_.emplace_back(child);
-        }
-    }
-    else
-    {
-        ea::erase_if(search_.lastResults_, [&](const WeakPtr<Node>& node)
-        {
-            return !node || !node->GetName().contains(search_.currentQuery_, false);
-        });
-    }
+    widget_->SetSettings(settings);
 }
 
 }
