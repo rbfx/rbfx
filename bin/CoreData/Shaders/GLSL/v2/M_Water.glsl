39c704cae024f6a50de354ceeeded4c7fe716c14
Gleb <gleb@gleblebedev.com>
2022-08-12 23:18:39 +0100

    SSAO and Water tweaks

--- bin/CoreData/Shaders/GLSL/v2/M_Water.glsl
+++ bin/CoreData/Shaders/GLSL/v2/M_Water.glsl
@@ -21,6 +21,7 @@
 UNIFORM_BUFFER_BEGIN(4, Material)
     DEFAULT_MATERIAL_UNIFORMS
     UNIFORM(half2 cNoiseSpeed)
+    UNIFORM(half2 cSaturationLightnessFactor)
     UNIFORM(half cNoiseStrength)
 UNIFORM_BUFFER_END(4, Material)
 
@@ -36,6 +37,45 @@ void main()
 #endif
 
 #ifdef URHO3D_PIXEL_SHADER
+//Color space conversion functions from https://github.com/tobspr/GLSL-Color-Spaces/blob/master/ColorSpaces.inc.glsl (MIT license)
+
+// Converts a value from linear RGB to HCV (Hue, Chroma, Value)
+vec3 LinearToHCV(vec3 rgb)
+{
+    // Based on work by Sam Hocevar and Emil Persson
+    vec4 P = (rgb.g < rgb.b) ? vec4(rgb.bg, -1.0, 2.0/3.0) : vec4(rgb.gb, 0.0, -1.0/3.0);
+    vec4 Q = (rgb.r < P.x) ? vec4(P.xyw, rgb.r) : vec4(rgb.r, P.yzx);
+    float C = Q.x - min(Q.w, Q.y);
+    float H = abs((Q.w - Q.y) / (6.0 * C + 1e-10) + Q.z);
+    return vec3(H, C, Q.x);
+}
+
+// Converts from linear rgb to HSL
+vec3 LinearToHSL(vec3 rgb)
+{
+    vec3 HCV = LinearToHCV(rgb);
+    float L = HCV.z - HCV.y * 0.5;
+    float S = HCV.y / (1.0 - abs(L * 2.0 - 1.0) + 1e-10);
+    return vec3(HCV.x, S, L);
+}
+
+// Converts from pure Hue to linear RGB
+vec3 HueToLinear(float hue)
+{
+    float R = abs(hue * 6.0 - 3.0) - 1.0;
+    float G = 2.0 - abs(hue * 6.0 - 2.0);
+    float B = 2.0 - abs(hue * 6.0 - 4.0);
+    return clamp(vec3(R,G,B), 0.0, 1.0);
+}
+
+// Converts from HSL to linear RGB
+vec3 HSLToLinear(vec3 hsl)
+{
+    vec3 rgb = HueToLinear(hsl.x);
+    float C = (1.0 - abs(2.0 * hsl.z - 1.0)) * hsl.y;
+    return (rgb - 0.5) * C + hsl.z;
+}
+
 void main()
 {
     SurfaceData surfaceData;
@@ -50,6 +90,8 @@ void main()
 
 #ifndef URHO3D_ADDITIVE_BLENDING
     FillSurfaceBackground(surfaceData);
+    vec3 hsl = clamp(LinearToHSL(surfaceData.backgroundColor) * vec3(1.0, cSaturationLightnessFactor), 0.0, 1.0);
+    surfaceData.backgroundColor = HSLToLinear(hsl);
 #endif
 
     // Water doesn't accept diffuse lighting, set albedo to zero
