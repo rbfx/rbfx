0a3060f852b670d24657baae753c7e5158240085
crystaldev3d <95735962+crystaldev3d@users.noreply.github.com>
2022-08-28 14:33:20 +0700

    Add Create Prefab (#441)
    * Add Create Prefab
    
    * Update SceneViewTab.cpp
    
    * Add DragAndDrop prefabs to SceneView
    
    * Select just spawned Prefab and add Undo action
    
    * Update SceneViewTab.cpp
    
    * Scene addon DragAndDrop prefabs
    
    * use ProcessInput instead Render
    
    * Add drag and drop for mdl and material files
    
    * Take into account the size of the BoundingBox when placing the mdl file
    
    * Resolve conflicts.
    
    * Cleanup code
    
    Co-authored-by: Eugene Kozlov <nekoeuge@gmail.com>

---
+++ Source/Editor/Foundation/SceneViewTab/SceneDragAndDropMaterial.h
@@ -0,0 +1,71 @@
+//
+// Copyright (c) 2017-2020 the rbfx project.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+
+#pragma once
+
+#include "../../Core/SettingsManager.h"
+#include "../../Foundation/SceneViewTab.h"
+
+namespace Urho3D
+{
+
+void Foundation_SceneDragAndDropMaterial(Context* context, SceneViewTab* sceneViewTab);
+
+/// Addon to update materials via drag&drop.
+class SceneDragAndDropMaterial : public SceneViewAddon
+{
+    URHO3D_OBJECT(SceneDragAndDropMaterial, SceneViewAddon);
+
+public:
+    explicit SceneDragAndDropMaterial(SceneViewTab* owner);
+
+    /// Implement SceneViewAddon.
+    /// @{
+    ea::string GetUniqueName() const override { return "DragAndDropMaterial"; }
+    bool IsDragDropPayloadSupported(SceneViewPage& page, DragDropPayload* payload) const override;
+    void BeginDragDrop(SceneViewPage& page, DragDropPayload* payload) override;
+    void UpdateDragDrop(DragDropPayload* payload) override;
+    void CompleteDragDrop(DragDropPayload* payload) override;
+    void CancelDragDrop() override;
+    /// @}
+
+private:
+    struct MaterialAssignment
+    {
+        WeakPtr<Drawable> drawable_;
+        unsigned subObject_{};
+        Variant oldMaterial_;
+        Variant newMaterial_;
+    };
+
+    void ClearAssignment();
+    void CreateAssignment(Drawable* drawable, unsigned subObject);
+
+    ea::pair<Drawable*, unsigned> QueryHoveredGeometry(Scene* scene, const Ray& cameraRay);
+
+    WeakPtr<SceneViewPage> currentPage_;
+
+    SharedPtr<Material> material_;
+    MaterialAssignment temporaryAssignment_;
+};
+
+} // namespace Urho3D
---
+++ Source/Editor/Foundation/SceneViewTab/SceneDragAndDropPrefab.h
@@ -0,0 +1,71 @@
+//
+// Copyright (c) 2017-2020 the rbfx project.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+
+#pragma once
+
+#include "../../Core/SettingsManager.h"
+#include "../../Foundation/SceneViewTab.h"
+
+namespace Urho3D
+{
+
+void Foundation_SceneDragAndDropPrefab(Context* context, SceneViewTab* sceneViewTab);
+
+/// Addon to create new nodes via drag&drop.
+class SceneDragAndDropPrefab : public SceneViewAddon
+{
+    URHO3D_OBJECT(SceneDragAndDropPrefab, SceneViewAddon);
+
+public:
+    explicit SceneDragAndDropPrefab(SceneViewTab* owner);
+
+    /// Implement SceneViewAddon.
+    /// @{
+    ea::string GetUniqueName() const override { return "DragAndDropPrefab"; }
+    bool IsDragDropPayloadSupported(SceneViewPage& page, DragDropPayload* payload) const override;
+    void BeginDragDrop(SceneViewPage& page, DragDropPayload* payload) override;
+    void UpdateDragDrop(DragDropPayload* payload) override;
+    void CompleteDragDrop(DragDropPayload* payload) override;
+    void CancelDragDrop() override;
+    /// @}
+
+private:
+    static constexpr float DefaultDistance = 10.0f; // TODO: Make configurable
+
+    struct HitResult
+    {
+        bool hit_{};
+        float distance_{};
+        Vector3 position_;
+        Vector3 normal_;
+    };
+
+    void CreateNodeFromPrefab(Scene* scene, const ResourceFileDescriptor& desc);
+    void CreateNodeFromModel(Scene* scene, const ResourceFileDescriptor& desc);
+
+    HitResult QueryHoveredGeometry(Scene* scene, const Ray& cameraRay);
+
+    SharedPtr<Node> temporaryNode_;
+    WeakPtr<SceneViewPage> currentPage_;
+};
+
+} // namespace Urho3D
