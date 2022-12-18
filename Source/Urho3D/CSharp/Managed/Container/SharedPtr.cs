d8192c0afb215a13c1388337aeef5de6f412d5ee
Gleb <gleb@gleblebedev.com>
2022-12-18 22:33:44 +0000

    no IEquatable

--- Source/Urho3D/CSharp/Managed/Container/SharedPtr.cs
+++ Source/Urho3D/CSharp/Managed/Container/SharedPtr.cs
@@ -26,7 +26,7 @@ using System.Diagnostics;
 
 namespace Urho3DNet
 {
-    public class SharedPtr<T> : IDisposable, IEquatable<SharedPtr<T>> where T : RefCounted
+    public class SharedPtr<T> : IDisposable where T : RefCounted
     {
         /// <summary>
         /// Construct immutable SharedPtr object.
@@ -37,11 +37,13 @@ namespace Urho3DNet
         {
             if (other?.IsNotExpired == true)
             {
-                _value = other;
-                _value.AddRef();
+                _ptr = other;
+                _ptr.AddRef();
             }
             else
-                _value = null;
+            {
+                _ptr = null;
+            }
         }
 
         /// <summary>
@@ -50,7 +52,7 @@ namespace Urho3DNet
         /// </summary>
         ~SharedPtr()
         {
-            if (_value?.IsNotExpired == true)
+            if (_ptr?.IsNotExpired == true)
                 Trace.WriteLine(
                     $"Object of type {typeof(T).FullName} wasn't properly disposed. Please call Dispose on all SharedPtr<{typeof(T).Name}> instances.");
         }
@@ -59,43 +61,15 @@ namespace Urho3DNet
         /// Copy SharedPtr.
         /// </summary>
         /// <param name="other">SharedPtr instance to copy</param>
-        public SharedPtr(SharedPtr<T> other) : this(other?._value)
+        public SharedPtr(SharedPtr<T> other) : this(other?._ptr)
         {
-        }
-
-        /// <summary>Indicates whether the current object is equal to another object of the same type.</summary>
-        /// <param name="other">An object to compare with this object.</param>
-        /// <returns>true if the current object is equal to the <paramref name="other">other</paramref> parameter; otherwise, false.</returns>
-        public bool Equals(SharedPtr<T> other)
-        {
-            if (ReferenceEquals(null, other)) return false;
-            if (ReferenceEquals(this, other)) return true;
-            return EqualityComparer<T>.Default.Equals(_value, other._value);
-        }
-
-        /// <summary>Determines whether the specified object is equal to the current object.</summary>
-        /// <param name="obj">The object to compare with the current object.</param>
-        /// <returns>true if the specified object  is equal to the current object; otherwise, false.</returns>
-        public override bool Equals(object obj)
-        {
-            if (ReferenceEquals(null, obj)) return false;
-            if (ReferenceEquals(this, obj)) return true;
-            if (obj.GetType() != this.GetType()) return false;
-            return Equals((SharedPtr<T>)obj);
-        }
-
-        /// <summary>Serves as the default hash function.</summary>
-        /// <returns>A hash code for the current object.</returns>
-        public override int GetHashCode()
-        {
-            return EqualityComparer<T>.Default.GetHashCode(_value);
         }
 
         /// <summary>
         /// Implicitly convert SharedPtr into value.
         /// </summary>
-        /// <param name="other"></param>
-        public static implicit operator T(SharedPtr<T> @this) => @this._value;
+        /// <param name="@this">SharedPtr object instance.</param>
+        public static implicit operator T(SharedPtr<T> @this) => @this._ptr;
 
         /// <summary>Indicates whether the two objects are equal.</summary>
         public static bool operator ==(SharedPtr<T> left, SharedPtr<T> right)
@@ -112,14 +86,15 @@ namespace Urho3DNet
         /// <summary>
         /// Get the underlying value.
         /// </summary>
-        public T Ptr => _value;
+        public T Ptr => _ptr;
 
         /// <summary>Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.</summary>
         public void Dispose()
         {
-            if (_value != null)
+            if (_ptr != null)
             {
-                _value.ReleaseRef();
+                _ptr.ReleaseRef();
+                _ptr = null;
             }
         }
 
@@ -127,10 +102,10 @@ namespace Urho3DNet
         /// <returns>A string that represents the current object.</returns>
         public override string ToString()
         {
-            return $"{_value}";
+            return $"{_ptr}";
         }
 
-        private readonly T _value;
+        private T _ptr;
     }
 
     public static class SharedPtr
