7acbb2bcc52658e7fa11e0c457ab959a15131b6b
Eugene Kozlov <nekoeuge@gmail.com>
2023-01-20 22:53:50 +0400

    Get rid of loading_ flag in AnimatedModel.

--- Source/Urho3D/Graphics/AnimatedModel.cpp
+++ Source/Urho3D/Graphics/AnimatedModel.cpp
@@ -61,7 +61,6 @@ AnimatedModel::AnimatedModel(Context* context) :
     animationLodDistance_(0.0f),
     updateInvisible_(false),
     isMaster_(true),
-    loading_(false),
     assignBonesPending_(false),
     forceAnimationUpdate_(false)
 {
@@ -103,40 +102,6 @@ void AnimatedModel::RegisterObject(Context* context)
         Variant::emptyVariantVector, AM_FILE | AM_NOEDIT);
     URHO3D_ACCESSOR_ATTRIBUTE("Morphs", GetMorphsAttr, SetMorphsAttr, ea::vector<unsigned char>, Variant::emptyBuffer,
         AM_DEFAULT);
-}
-
-void AnimatedModel::SerializeInBlock(Archive& archive)
-{
-    loading_ = true;
-    BaseClassName::SerializeInBlock(archive);
-    loading_ = false;
-}
-
-bool AnimatedModel::Load(Deserializer& source)
-{
-    loading_ = true;
-    bool success = Component::Load(source);
-    loading_ = false;
-
-    return success;
-}
-
-bool AnimatedModel::LoadXML(const XMLElement& source)
-{
-    loading_ = true;
-    bool success = Component::LoadXML(source);
-    loading_ = false;
-
-    return success;
-}
-
-bool AnimatedModel::LoadJSON(const JSONValue& source)
-{
-    loading_ = true;
-    bool success = Component::LoadJSON(source);
-    loading_ = false;
-
-    return success;
 }
 
 void AnimatedModel::ApplyAttributes()
@@ -521,6 +486,7 @@ void AnimatedModel::SetModel(Model* model, bool createBones)
         geometryBoneMappings_.clear();
         modelAnimator_ = nullptr;
         morphs_.clear();
+        skeletonData_.clear();
         SetBoundingBox(BoundingBox());
         SetSkeleton(Skeleton(), false);
     }
@@ -703,10 +669,10 @@ void AnimatedModel::SetSkeleton(const Skeleton& skeleton, bool createBones)
         // Merge bounding boxes from non-master models
         FinalizeBoneBoundingBoxes();
 
-        ea::vector<Bone>& bones = skeleton_.GetModifiableBones();
         // Create scene nodes for the bones
         if (createBones)
         {
+            ea::vector<Bone>& bones = skeleton_.GetModifiableBones();
             for (auto i = bones.begin(); i != bones.end(); ++i)
             {
                 // Create bones as local, as they are never to be directly synchronized over the network
@@ -761,8 +727,8 @@ void AnimatedModel::SetSkeleton(const Skeleton& skeleton, bool createBones)
 void AnimatedModel::SetModelAttr(const ResourceRef& value)
 {
     auto* cache = GetSubsystem<ResourceCache>();
-    // When loading a scene, set model without creating the bone nodes (will be assigned later during post-load)
-    SetModel(cache->GetResource<Model>(value.name_), !loading_);
+    // Bones will be created on demand in ApplyAttributes()
+    SetModel(cache->GetResource<Model>(value.name_), false);
 }
 
 void AnimatedModel::SetBonesEnabledAttr(const VariantVector& value)
@@ -888,22 +854,24 @@ void AnimatedModel::AssignBoneNodes()
         return;
 
     // Find the bone nodes from the node hierarchy and add listeners
-    ea::vector<Bone>& bones = skeleton_.GetModifiableBones();
-    bool boneFound = false;
-    for (auto i = bones.begin(); i != bones.end(); ++i)
+    bool allBonesFound = true;
+    for (Bone& bone : skeleton_.GetModifiableBones())
     {
-        Node* boneNode = node_->GetChild(i->name_, true);
-        if (boneNode)
+        if (Node* boneNode = node_->GetChild(bone.name_, true))
         {
-            boneFound = true;
             boneNode->AddListener(this);
+            bone.node_ = boneNode;
         }
-        i->node_ = boneNode;
+        else
+        {
+            allBonesFound = false;
+            break;
+        }
     }
 
     // If no bones found, this may be a prefab where the bone information was left out.
     // In that case reassign the skeleton now if possible
-    if (!boneFound && model_)
+    if (!allBonesFound && model_)
         SetSkeleton(model_->GetSkeleton(), true);
 
     // Notify AnimationStateSource so it can reconnect to new bone nodes
