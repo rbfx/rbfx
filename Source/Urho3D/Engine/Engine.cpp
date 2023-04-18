f7e7aadc38c2201836c248e9edd80cab1c36c8ff
Gleb <gleb@gleblebedev.com>
2023-04-18 19:22:03 +0100

    Missing AddFactoryReflection

--- Source/Urho3D/Engine/Engine.cpp
+++ Source/Urho3D/Engine/Engine.cpp
@@ -43,6 +43,8 @@
 #include "../Graphics/Renderer.h"
 #include "../Input/Input.h"
 #include "../Input/FreeFlyController.h"
+#include "../Input/DirectionalPadAdapter.h"
+#include "../Input/PointerAdapter.h"
 #include "../IO/FileSystem.h"
 #include "../IO/VirtualFileSystem.h"
 #include "../IO/MountedDirectory.h"
@@ -260,6 +262,9 @@ bool Engine::Initialize(const StringVariantMap& parameters)
     // Register the rest of the subsystems
     context_->RegisterSubsystem(new Input(context_));
     context_->AddFactoryReflection<FreeFlyController>();
+    context_->AddFactoryReflection<PointerAdapter>();
+    context_->AddFactoryReflection<DirectionAggregator>();
+    context_->AddFactoryReflection<DirectionalPadAdapter>();
 
     context_->RegisterSubsystem(new UI(context_));
 
