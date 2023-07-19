990df379cfc61c803808b82bd83d5dc380f68afb
Eugene Kozlov <nekoeuge@gmail.com>
2023-07-19 21:04:19 +0400

    Remove DiligentGraphics.cpp.

--- Source/Urho3D/Graphics/Graphics.cpp
+++ Source/Urho3D/Graphics/Graphics.cpp
@@ -22,6 +22,7 @@
 
 #include "../Precompiled.h"
 
+#include "../Core/ProcessUtils.h"
 #include "../Core/Profiler.h"
 #include "../Graphics/AnimatedModel.h"
 #include "../Graphics/Animation.h"
@@ -61,7 +62,10 @@
 #include "../IO/VirtualFileSystem.h"
 #include "../IO/Log.h"
 #include "Urho3D/RenderAPI/PipelineState.h"
+#include "Urho3D/RenderAPI/RenderAPIUtils.h"
+#include "Urho3D/RenderAPI/RenderContext.h"
 #include "Urho3D/RenderAPI/RenderDevice.h"
+#include "Urho3D/Resource/ResourceCache.h"
 
 #include <SDL.h>
 
@@ -101,6 +105,126 @@ WindowMode ToWindowMode(bool fullscreen, bool borderless)
 }
 
 unsigned Graphics::maxBonesHWSkinned = 0;
+
+Graphics::Graphics(Context* context)
+    : Object(context)
+    , position_(SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED)
+    , shaderPath_("Shaders/HLSL/")
+    , shaderExtension_(".hlsl")
+    , apiName_("Diligent")
+{
+    // TODO: This can be used to have DPI scaling work on Windows, but it leads to blurry fonts
+    // SDL_SetHint(SDL_HINT_WINDOWS_DPI_SCALING, "1");
+    SDL_SetHint(SDL_HINT_WINDOWS_DPI_AWARENESS, "system");
+    context_->RequireSDL(SDL_INIT_VIDEO);
+}
+
+Graphics::~Graphics()
+{
+    Close();
+
+    context_->ReleaseSDL();
+}
+
+bool Graphics::SetScreenMode(const WindowSettings& windowSettings)
+{
+    URHO3D_PROFILE("SetScreenMode");
+
+    if (!renderDevice_)
+    {
+        try
+        {
+            renderDevice_ = MakeShared<RenderDevice>(context_, settings_, windowSettings);
+            context_->RegisterSubsystem(renderDevice_);
+        }
+        catch (const RuntimeException& ex)
+        {
+            URHO3D_LOGERROR("Failed to create render device: {}", ex.what());
+            return false;
+        }
+
+        renderDevice_->PostInitialize();
+
+        renderDevice_->OnDeviceLost.Subscribe(this, [this]() { SendEvent(E_DEVICELOST); });
+        renderDevice_->OnDeviceRestored.Subscribe(this, [this]() { SendEvent(E_DEVICERESET); });
+
+        apiName_ = ToString(GetRenderBackend());
+    }
+    else
+    {
+        renderDevice_->UpdateWindowSettings(windowSettings);
+    }
+
+    window_ = renderDevice_->GetSDLWindow();
+
+    // Clear the initial window contents to black
+    RenderContext* renderContext = renderDevice_->GetRenderContext();
+    renderContext->SetSwapChainRenderTargets();
+    renderContext->ClearRenderTarget(0, Color::BLACK);
+    renderDevice_->Present();
+
+    OnScreenModeChanged();
+    return true;
+}
+
+void Graphics::Close()
+{
+    context_->RemoveSubsystem<RenderDevice>();
+    renderDevice_ = nullptr;
+}
+
+bool Graphics::TakeScreenShot(Image& destImage)
+{
+    URHO3D_PROFILE("TakeScreenShot");
+    if (!IsInitialized())
+        return false;
+
+    IntVector2 size;
+    ByteVector data;
+    if (!renderDevice_->TakeScreenShot(size, data))
+        return false;
+
+    destImage.SetSize(size.x_, size.y_, 4);
+    destImage.SetData(data.data());
+    return true;
+}
+
+bool Graphics::BeginFrame()
+{
+    if (!IsInitialized())
+        return false;
+
+    if (!GetExternalWindow())
+    {
+        // To prevent a loop of endless device loss and flicker, do not attempt to render when in fullscreen
+        // and the window is minimized
+        if (GetFullscreen() && (SDL_GetWindowFlags(window_) & SDL_WINDOW_MINIMIZED))
+            return false;
+    }
+
+    numPrimitives_ = 0;
+    numBatches_ = 0;
+
+    SendEvent(E_BEGINRENDERING);
+    return true;
+}
+
+void Graphics::EndFrame()
+{
+    if (!IsInitialized())
+        return;
+
+    {
+        URHO3D_PROFILE("Present");
+
+        SendEvent(E_ENDRENDERING);
+
+        renderDevice_->Present();
+    }
+
+    // Clean up too large scratch buffers
+    CleanupScratchBuffers();
+}
 
 void Graphics::SetWindowTitle(const ea::string& windowTitle)
 {
@@ -479,6 +603,180 @@ void Graphics::OnScreenModeChanged()
 void Graphics::SetMaxBones(unsigned numBones)
 {
     maxBonesHWSkinned = numBones;
+}
+
+void Graphics::Clear(ClearTargetFlags flags, const Color& color, float depth, unsigned stencil)
+{
+    URHO3D_ASSERT(renderDevice_);
+
+    RenderContext* renderContext = renderDevice_->GetRenderContext();
+    if (flags.Test(CLEAR_COLOR))
+        renderContext->ClearRenderTarget(0, color);
+    if (flags.Test(CLEAR_DEPTH) || flags.Test(CLEAR_STENCIL))
+        renderContext->ClearDepthStencil(flags, depth, stencil);
+}
+
+void Graphics::SetDefaultTextureFilterMode(TextureFilterMode mode)
+{
+    if (mode != defaultTextureFilterMode_)
+    {
+        defaultTextureFilterMode_ = mode;
+    }
+}
+
+void Graphics::SetDefaultTextureAnisotropy(unsigned level)
+{
+    level = Max(level, 1U);
+
+    if (level != defaultTextureAnisotropy_)
+    {
+        defaultTextureAnisotropy_ = level;
+    }
+}
+
+void Graphics::Restore()
+{
+    if (renderDevice_)
+    {
+        if (!renderDevice_->Restore())
+            Close();
+    }
+}
+
+void Graphics::ResetRenderTargets()
+{
+    URHO3D_ASSERT(renderDevice_);
+
+    RenderContext* renderContext = renderDevice_->GetRenderContext();
+    renderContext->SetSwapChainRenderTargets();
+    renderContext->SetFullViewport();
+}
+
+bool Graphics::IsInitialized() const
+{
+    return renderDevice_ != nullptr;
+}
+
+TextureFormat Graphics::GetFormat(CompressedFormat format) const
+{
+    switch (format)
+    {
+    case CF_RGBA: return TextureFormat::TEX_FORMAT_RGBA8_UNORM;
+    case CF_DXT1: return TextureFormat::TEX_FORMAT_BC1_UNORM;
+    case CF_DXT3: return TextureFormat::TEX_FORMAT_BC2_UNORM;
+    case CF_DXT5: return TextureFormat::TEX_FORMAT_BC3_UNORM;
+    default: return TextureFormat::TEX_FORMAT_UNKNOWN;
+    }
+}
+
+ShaderVariation* Graphics::GetShader(ShaderType type, const ea::string& name, const ea::string& defines) const
+{
+    return GetShader(type, name.c_str(), defines.c_str());
+}
+
+ShaderVariation* Graphics::GetShader(ShaderType type, const char* name, const char* defines) const
+{
+    // Return cached shader
+    if (lastShaderName_ == name && lastShader_)
+        return lastShader_->GetVariation(type, defines);
+
+    auto cache = context_->GetSubsystem<ResourceCache>();
+    lastShader_ = nullptr;
+
+    // Try to load universal shader
+    if (strncmp(universalShaderNamePrefix_.c_str(), name, universalShaderNamePrefix_.size()) == 0)
+    {
+        const ea::string universalShaderName = Format(universalShaderPath_, name);
+        if (cache->Exists(universalShaderName))
+        {
+            lastShader_ = cache->GetResource<Shader>(universalShaderName);
+            lastShaderName_ = name;
+        }
+    }
+
+    // Try to load native shader
+    if (!lastShader_)
+    {
+        const ea::string fullShaderName = shaderPath_ + name + shaderExtension_;
+        // Try to reduce repeated error log prints because of missing shaders
+        if (lastShaderName_ != name || cache->Exists(fullShaderName))
+        {
+            lastShader_ = cache->GetResource<Shader>(fullShaderName);
+            lastShaderName_ = name;
+        }
+    }
+
+    return lastShader_ ? lastShader_->GetVariation(type, defines) : nullptr;
+}
+
+IntVector2 Graphics::GetRenderTargetDimensions() const
+{
+    URHO3D_ASSERT(false);
+    return IntVector2::ZERO;
+}
+
+bool Graphics::IsDeviceLost() const
+{
+    // Direct3D11 graphics context is never considered lost
+    /// \todo The device could be lost in case of graphics adapters getting disabled during runtime. This is not
+    /// currently handled
+    return false;
+}
+
+void Graphics::OnWindowResized()
+{
+    if (!renderDevice_ || GetPlatform() == PlatformId::Web)
+        return;
+
+    renderDevice_->UpdateSwapChainSize();
+
+    using namespace ScreenMode;
+
+    VariantMap& eventData = GetEventDataMap();
+    eventData[P_WIDTH] = GetWidth();
+    eventData[P_HEIGHT] = GetWidth();
+    eventData[P_FULLSCREEN] = GetFullscreen();
+    eventData[P_BORDERLESS] = GetBorderless();
+    eventData[P_RESIZABLE] = GetResizable();
+    SendEvent(E_SCREENMODE, eventData);
+}
+
+void Graphics::OnWindowMoved()
+{
+    if (!renderDevice_ || !window_ || GetFullscreen())
+        return;
+
+    int newX, newY;
+
+    SDL_GetWindowPosition(window_, &newX, &newY);
+    if (newX == position_.x_ && newY == position_.y_)
+        return;
+
+    position_.x_ = newX;
+    position_.y_ = newY;
+
+    URHO3D_LOGTRACEF("Window was moved to %d,%d", position_.x_, position_.y_);
+
+    using namespace WindowPos;
+
+    VariantMap& eventData = GetEventDataMap();
+    eventData[P_X] = position_.x_;
+    eventData[P_Y] = position_.y_;
+    SendEvent(E_WINDOWPOS, eventData);
+}
+
+RenderBackend Graphics::GetRenderBackend() const
+{
+    return renderDevice_ ? renderDevice_->GetBackend() : RenderBackend::OpenGL;
+}
+
+unsigned Graphics::GetMaxBones()
+{
+    /// User-specified number of bones
+    if (maxBonesHWSkinned)
+        return maxBonesHWSkinned;
+
+    return 128;
 }
 
 void RegisterGraphicsLibrary(Context* context)
