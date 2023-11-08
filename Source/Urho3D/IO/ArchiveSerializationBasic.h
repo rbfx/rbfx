0d06401ec02a2dc348719a10e0ce98d9ebf1a937
Eugene Kozlov <nekoeuge@gmail.com>
2023-11-08 23:57:33 +0400

    Add SerializeValue for ea::pair.

--- Source/Urho3D/IO/ArchiveSerializationBasic.h
+++ Source/Urho3D/IO/ArchiveSerializationBasic.h
@@ -389,6 +389,16 @@ void SerializeOptionalValue(Archive& archive, const char* name, T& value, const 
     }
 }
 
+/// Serialize pair type.
+template <class T, class U, class TSerializer = Detail::DefaultSerializer>
+void SerializeValue(
+    Archive& archive, const char* name, ea::pair<T, U>& value, const TSerializer& serializeValue = TSerializer{})
+{
+    const ArchiveBlock block = archive.OpenUnorderedBlock(name);
+    serializeValue(archive, "first", value.first);
+    serializeValue(archive, "second", value.second);
+}
+
 /// Wrapper that consumes ArchiveException and converts it to boolean status.
 template <class T>
 bool ConsumeArchiveException(const T& lambda, bool errorOnException = true)
