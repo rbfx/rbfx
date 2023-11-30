ec3601e26887641b330a9615d39cd7ee42378538
Gleb <gleb@gleblebedev.com>
2023-11-30 22:04:30 +0000

    OpenURL URHO3D_LOGERROR

--- Source/Urho3D/Core/ProcessUtils.cpp
+++ Source/Urho3D/Core/ProcessUtils.cpp
@@ -803,7 +803,12 @@ URHO3D_API unsigned GetCurrentProcessID()
 
 URHO3D_API bool OpenURL(const ea::string& url)
 {
-    return !SDL_OpenURL(url.c_str());
+    const int error = SDL_OpenURL(url.c_str());
+    if (error != 0)
+    {
+        URHO3D_LOGERROR(SDL_GetError());
+    }
+    return !error;
 }
 
 }
