f76ab292c7a48f07f41a9f49f70d4d3c5ad6ba2e
Kest <meowtss@protonmail.com>
2024-01-20 21:05:45 -0500

    Beginning of the Jolt integration

---
+++ Source/Urho3D/Physics/Jolt/JPhysicsWorld.cpp
@@ -0,0 +1,54 @@
+//
+// Copyright (c) 2008-2022 the Urho3D project.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+
+#include "../../Precompiled.h"
+
+#include "JPhysicsWorld.h"
+
+#include <Urho3D/Core/Context.h>
+
+#include <Jolt/RegisterTypes.h>
+#include <Jolt/Core/Factory.h>
+#include <Jolt/Core/TempAllocator.h>
+#include <Jolt/Core/JobSystemThreadPool.h>
+#include <Jolt/Physics/PhysicsSystem.h>
+#include <Jolt/Physics/PhysicsSettings.h>
+
+namespace Urho3D
+{
+
+JPhysicsWorld::JPhysicsWorld(Context* context)
+    : Component(context)
+{
+    
+}
+
+JPhysicsWorld::~JPhysicsWorld()
+{
+}
+
+void JPhysicsWorld::RegisterObject(Context* context)
+{
+    context->AddFactoryReflection<JPhysicsWorld>(Category_JoltPhysics);
+}
+
+}
