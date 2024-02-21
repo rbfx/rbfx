a548c8be230908e078fbe051589fea99f3a3a407
niansa <tuxifan@posteo.de>
2024-02-21 21:47:20 +0100

    Implemented basic structure (and more CMake fixes)

---
+++ Source/Urho3D/SteamAudio/SteamAudio.cpp
@@ -0,0 +1,234 @@
+//
+// Copyright (c) 2008-2024 the Urho3D project.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+
+#include "../Precompiled.h"
+
+#include "../SteamAudio/SteamAudio.h"
+#include "../SteamAudio/SteamSoundSource.h"
+#include "../SteamAudio/SteamSoundListener.h"
+#include "../IO/Log.h"
+#include "../Core/Context.h"
+#include "../Core/CoreEvents.h"
+
+#include <SDL.h>
+
+namespace Urho3D
+{
+
+void SDLSteamAudioCallback(void* userdata, Uint8* stream, int len);
+
+SteamAudio::SteamAudio(Context* context) :
+    Object(context)
+{
+    context_->RequireSDL(SDL_INIT_AUDIO);
+
+    // Set the master to the default value
+    masterGain_ = 1.0f;
+
+    // Register Audio library object factories
+    RegisterSteamAudioLibrary(context_);
+
+    SubscribeToEvent(E_RENDERUPDATE, URHO3D_HANDLER(SteamAudio, HandleRenderUpdate));
+}
+
+SteamAudio::~SteamAudio()
+{
+    Release();
+    context_->ReleaseSDL();
+}
+
+bool SteamAudio::SetMode(int mixRate, SpeakerMode mode)
+{
+    // Clean up first
+    Release();
+
+    // Reset master gain
+    masterGain_ = 1.0f;
+
+    // Convert speaker mode to channel count
+    switch (mode) {
+    case SPK_MONO: channelCount_ = 1; break;
+    case SPK_AUTO:
+    case SPK_STEREO: channelCount_ = 2; break;
+    case SPK_QUADROPHONIC: channelCount_ = 4; break;
+    case SPK_SURROUND_5_1: channelCount_ = 6; break;
+    }
+
+    // Create context
+    IPLContextSettings contextSettings {
+        .version = STEAMAUDIO_VERSION
+    };
+    iplContextCreate(&contextSettings, &phononContext_);
+
+    // Typical audio settings...
+    audioSettings_ = IPLAudioSettings {
+        .samplingRate = mixRate,
+        .frameSize = 1024
+    };
+
+    // Create single frame buffer
+    // This buffer one individual audio frame per channel at a time.
+    finalFrameBuffer_.resize(channelCount_ * audioSettings_.frameSize);
+
+    // Create the HRTF
+    // The HRTF basically describes the "set of filters that is applied to audio in order to spatialize it"
+    IPLHRTFSettings hrtfSettings {
+        .type = IPL_HRTFTYPE_DEFAULT,
+        .volume = 1.0f
+    };
+    iplHRTFCreate(phononContext_, &audioSettings_, &hrtfSettings, &hrtf_);
+
+    // Allocate an output buffer
+    // That buffer is "deinterleaved", which means that it's actually one buffer for each channel
+    phononFrameBuffer_ = IPLAudioBuffer {};
+    iplAudioBufferAllocate(phononContext_, channelCount_, audioSettings_.frameSize, &phononFrameBuffer_);
+
+    // Set up SDL
+    SDL_AudioSpec spec {
+        .freq = audioSettings_.samplingRate,
+        .format = AUDIO_F32,
+        .channels = 2,
+        .samples = static_cast<unsigned short>(audioSettings_.frameSize),
+        .callback = *SDLSteamAudioCallback,
+        .userdata = this
+    };
+
+    // Open the audio channel
+    if (SDL_OpenAudio(&spec, NULL) < 0) {
+        URHO3D_LOGERROR(ea::string("Failed to open SDL2 audio: ")+SDL_GetError());
+        return false;
+    }
+
+    // Start playing audio
+    Play();
+
+    return true;
+}
+
+bool SteamAudio::RefreshMode()
+{
+    return SetMode(audioSettings_.samplingRate, GetSpeakerMode());
+}
+
+void SteamAudio::Close()
+{
+    Release();
+}
+
+void SteamAudio::Update(float timeStep)
+{
+
+}
+
+void SteamAudio::Play()
+{
+    SDL_PauseAudio(0);
+}
+
+void SteamAudio::Stop()
+{
+    SDL_PauseAudio(1);
+}
+
+void SteamAudio::SetMasterGain(float gain)
+{
+    masterGain_ = gain;
+}
+
+void SteamAudio::SetListener(SteamSoundListener *listener)
+{
+    listener_ = listener;
+}
+
+float SteamAudio::GetMasterGain() const
+{
+    return masterGain_;
+}
+
+SpeakerMode SteamAudio::GetSpeakerMode() const
+{
+    switch (channelCount_) {
+    case 1: return SPK_MONO;
+    case 2: return SPK_STEREO;
+    case 4: return SPK_QUADROPHONIC;
+    case 6: return SPK_SURROUND_5_1;
+    default: return SPK_AUTO;
+    }
+}
+
+void SteamAudio::AddSoundSource(SteamSoundSource *soundSource)
+{
+    MutexLock Lock(audioMutex_);
+    soundSources_.push_back(soundSource);
+}
+
+void SteamAudio::RemoveSoundSource(SteamSoundSource *soundSource)
+{
+    MutexLock Lock(audioMutex_);
+    auto i = soundSources_.find(soundSource);
+    if (i != soundSources_.end())
+    {
+        MutexLock lock(audioMutex_);
+        soundSources_.erase(i);
+    }
+}
+
+void SteamAudio::MixOutput(void *dest, unsigned int frames)
+{
+    while (frames--) {
+        //
+    }
+}
+
+void SteamAudio::HandleRenderUpdate(StringHash eventType, VariantMap &eventData)
+{
+    using namespace RenderUpdate;
+
+    Update(eventData[P_TIMESTEP].GetFloat());
+}
+
+void SteamAudio::Release()
+{
+    SDL_CloseAudio();
+    MutexLock Lock(audioMutex_);
+    iplAudioBufferFree(phononContext_, &phononFrameBuffer_);
+    finalFrameBuffer_.clear();
+    iplHRTFRelease(&hrtf_);
+    iplContextRelease(&phononContext_);
+}
+
+void SDLSteamAudioCallback(void* userdata, Uint8 *stream, int len)
+{
+    auto* audio = static_cast<SteamAudio*>(userdata);
+    {
+        MutexLock Lock(audio->GetMutex());
+        audio->MixOutput(stream, len / audio->GetFrameSize());
+    }
+}
+
+
+void RegisterSteamAudioLibrary(Context* context)
+{
+
+}
+
+}
---
+++ Source/Urho3D/SteamAudio/SteamAudio.h
@@ -0,0 +1,128 @@
+//
+// Copyright (c) 2008-2024 the Urho3D project.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+
+#pragma once
+
+#include "../SteamAudio/SteamAudioDefs.h"
+#include "../Core/Object.h"
+
+#include <phonon.h>
+
+namespace Urho3D
+{
+
+class SteamSoundListener;
+class SteamSoundSource;
+
+/// %Audio subsystem.
+class URHO3D_API SteamAudio : public Object
+{
+    URHO3D_OBJECT(SteamAudio, Object);
+
+public:
+    /// Construct.
+    explicit SteamAudio(Context* context);
+    /// Destruct. Terminate the audio thread and free the audio buffer.
+    ~SteamAudio() override;
+
+    /// Initialize sound output with specified buffer length and output mode.
+    bool SetMode(int mixRate, SpeakerMode mode);
+    /// Re-initialize sound output with same parameters.
+    bool RefreshMode();
+    /// Shutdown this audio device, likely because we've lost it.
+    void Close();
+    /// Run update on sound sources. Required for continued playback.
+    void Update(float timeStep);
+    /// Restart sound output.
+    void Play();
+    /// Suspend sound output.
+    void Stop();
+    /// Set master gain.
+    /// @property
+    void SetMasterGain(float gain);
+    /// Set active sound listener for 3D sounds.
+    /// @property
+    void SetListener(SteamSoundListener* listener);
+
+    /// Return byte size of one frame.
+    /// @property
+    unsigned GetFrameSize() const { return audioSettings_.frameSize; }
+
+    /// Return master gain for a specific sound source type. Unknown sound types will return full gain (1).
+    /// @property
+    float GetMasterGain() const;
+
+    /// Return mode of output.
+    /// @property
+    SpeakerMode GetSpeakerMode() const;
+
+    /// Return active sound listener.
+    /// @property
+    SteamSoundListener* GetListener() const { return listener_; }
+
+    /// Return all sound sources.
+    const ea::vector<SteamSoundSource*>& GetSoundSources() const { return soundSources_; }
+
+    /// Add a sound source to keep track of. Called by SoundSource.
+    void AddSoundSource(SteamSoundSource* soundSource);
+    /// Remove a sound source. Called by SoundSource.
+    void RemoveSoundSource(SteamSoundSource* soundSource);
+
+    /// Return audio thread mutex.
+    Mutex& GetMutex() { return audioMutex_; }
+
+    /// Mix sound sources into the buffer.
+    void MixOutput(void* dest, unsigned frames);
+
+private:
+    /// Handle render update event.
+    void HandleRenderUpdate(StringHash eventType, VariantMap& eventData);
+    /// Stop sound output and release the sound buffer.
+    void Release();
+
+    /// Phonon context.
+    IPLContext phononContext_{};
+    /// Phonon audio settings.
+    IPLAudioSettings audioSettings_{};
+    /// Phonon HRTF.
+    IPLHRTF hrtf_{};
+    /// Phonon final output frame buffer.
+    IPLAudioBuffer phononFrameBuffer_{};
+    /// Interleaved output frame buffer for SDL.
+    ea::vector<float> finalFrameBuffer_{};
+    /// Audio thread mutex.
+    Mutex audioMutex_;
+    /// Channel count
+    unsigned channelCount_{};
+    /// Master gain.
+    float masterGain_{};
+    /// Sound sources.
+    ea::vector<SteamSoundSource*> soundSources_;
+    /// Sound listener.
+    WeakPtr<SteamSoundListener> listener_;
+};
+
+/// Register Audio library objects.
+/// @nobind
+void URHO3D_API RegisterSteamAudioLibrary(Context* context);
+
+}
