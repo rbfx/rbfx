62ff2aa337853987de535fb97869212bf5e9da89
rk <rokups@zoho.com>
2024-05-04 15:43:56 +0300

    Packaging: Rework nuget packages.
    - Tool and runtime libraries are split into their own packages.
    - Urho3DNet targets multiple frameworks.
    - Android packages added.

--- packaging/nuget/package.sh
+++ packaging/nuget/package.sh
@@ -1,42 +1,82 @@
 #!/usr/bin/env bash
-for platform in Linux MacOS Windows;
-do
-    if [ ! -e SDK-$platform ];
-    then
-        if [ "$GITHUB_ACTIONS" != "" ];
+
+if [ -e ./nuget.exe ];
+then
+    NUGET=./nuget.exe
+else
+    NUGET=nuget
+fi
+
+package() {
+    mkdir -p ./in
+    for platform in Linux MacOS Windows Android UWP;
+    do
+        if [ ! -e in/$platform ];
         then
-            # CI itself downloads artifacts as unpacked dirs
-            mv ./rbfx-$platform* ./SDK-$platform
-        else
-            # When testing use zips downloaded from CI
-            unzip -n ./rbfx-$platform*.zip -d ./SDK-$platform
+            if [ "$GITHUB_ACTIONS" != "" ];
+            then
+                # CI itself downloads artifacts as unpacked dirs
+                mv ./rbfx-$platform* ./in/$platform
+            else
+                # When testing use zips downloaded from CI
+                unzip -n ./rbfx-$platform*.zip -d ./in/$platform
+            fi
+        fi
+    done
+
+    # Use a default version.
+    version="0.0.99-dev"
+
+    # Try getting a tag.
+    git describe --abbrev=0 --tags 2>/dev/null
+    if [ "$?" -ne "0" ];
+    then
+        # Use a branch name.
+        version=$(git rev-parse --abbrev-ref HEAD)
+        if [[ "$version" = *"nuget/"* ]];
+        then
+            version=$(sed 's|^nuget/v\?||g' <<< $version)
         fi
     fi
-done
 
-# Try getting a tag.
-version=$(git describe --abbrev=0 --tags 2>/dev/null)
-if [ "$?" -ne "0" ];
-then
-    # Use a branch name.
-    version=$(git rev-parse --abbrev-ref HEAD)
-    if [[ "$version" != *"nuget/"* ]];
-    then
-        # Use a default version.
-        version="0.0.99-dev"
-    fi
-fi
-version=$(sed 's|^nuget/v\?||g' <<< $version)
+    case "$version" in
+        *-alpha|*-dev)
+            sha_short=$(git rev-parse --short HEAD)
+            version="$version-$sha_short"
+        ;;
+    esac
 
-case "$version" in
-    *-alpha|*-dev)
-        sha_short=$(git rev-parse --short HEAD)
-        version="$version-$sha_short"
+    echo "Packaging $version"
+
+    ln -s ../../bin/CoreData CoreData
+    ln -s ../../bin/EditorData EditorData
+    ln -s ../../bin/Data Data
+
+    for nuspec in rbfx.*.nuspec;
+    do
+        $NUGET pack -OutputDirectory ./out -Version $version $nuspec
+    done
+
+    rm -r CoreData EditorData Data
+}
+
+local-repository() {
+    rm -rf ./local-repository
+    for pkg in out/rbfx.*.nupkg;
+    do
+        $NUGET add $pkg -source ./local-repository
+    done
+}
+
+case "$1" in
+    package)
+        package
+    ;;
+    local-repo)
+        local-repository
+    ;;
+    *)
+        echo "Usage: $0 package|local-repo"
+        exit 1
     ;;
 esac
-
-echo "Packaging $version"
-nuget pack -OutputDirectory ./out -Version $version rbfx.Urho3DNet.nuspec
-nuget pack -OutputDirectory ./out -Version $version rbfx.Tools.nuspec
-nuget pack -OutputDirectory ./out -Version $version rbfx.CoreData.nuspec
-nuget pack -OutputDirectory ./out -Version $version rbfx.Data.nuspec
