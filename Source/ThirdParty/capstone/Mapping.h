bee874828e15e0dbeef3d1054d1e9c706f638bfc
rk <rokups@zoho.com>
2025-07-21 08:43:52 +0300

    ThirdParty: Add back capstone/zstd/ppqsort and do not use CPM to pull them into tracy.

---
+++ Source/ThirdParty/capstone/Mapping.h
@@ -0,0 +1,287 @@
+/* Capstone Disassembly Engine */
+/* By Nguyen Anh Quynh <aquynh@gmail.com>, 2013-2019 */
+/*    Rot127 <unisono@quyllur.org>, 2022-2023 */
+
+#ifndef CS_MAPPING_H
+#define CS_MAPPING_H
+
+#if defined(CAPSTONE_HAS_OSXKERNEL)
+#include <libkern/libkern.h>
+#else
+#include "include/capstone/capstone.h"
+#include <stddef.h>
+#endif
+#include "cs_priv.h"
+#include <assert.h>
+#include <string.h>
+
+// map instruction to its characteristics
+typedef struct insn_map {
+	unsigned short id;		    // The LLVM instruction id
+	unsigned short mapid;		    // The Capstone instruction id
+#ifndef CAPSTONE_DIET
+	uint16_t regs_use[MAX_IMPL_R_REGS]; ///< list of implicit registers used by
+					    ///< this instruction
+	uint16_t regs_mod[MAX_IMPL_W_REGS]; ///< list of implicit registers modified
+					    ///< by this instruction
+	unsigned char groups
+		[MAX_NUM_GROUPS]; ///< list of group this instruction belong to
+	bool branch;		  // branch instruction?
+	bool indirect_branch;	  // indirect branch instruction?
+	union {
+		ppc_suppl_info ppc;
+		loongarch_suppl_info loongarch;
+		aarch64_suppl_info aarch64;
+		systemz_suppl_info systemz;
+	} suppl_info; // Supplementary information for each instruction.
+#endif
+} insn_map;
+
+// look for @id in @m, given its size in @max. first time call will update
+// @cache. return 0 if not found
+unsigned short insn_find(const insn_map *m, unsigned int max, unsigned int id,
+			 unsigned short **cache);
+
+unsigned int find_cs_id(unsigned MC_Opcode, const insn_map *imap,
+			unsigned imap_size);
+
+#define MAX_NO_DATA_TYPES 16
+
+///< A LLVM<->CS Mapping entry of an MCOperand.
+typedef struct {
+	uint8_t /* cs_op_type */ type;	 ///< Operand type (e.g.: reg, imm, mem)
+	uint8_t /* cs_ac_type */ access; ///< The access type (read, write)
+	uint8_t				 /* cs_data_type */
+		dtypes[MAX_NO_DATA_TYPES]; ///< List of op types. Terminated by
+					   ///< CS_DATA_TYPE_LAST
+} mapping_op;
+
+#define MAX_NO_INSN_MAP_OPS 16
+
+///< MCOperands of an instruction.
+typedef struct {
+	mapping_op
+		ops[MAX_NO_INSN_MAP_OPS]; ///< NULL terminated array of insn_op.
+} map_insn_ops;
+
+/// Only usable by `auto-sync` archs!
+const cs_op_type mapping_get_op_type(MCInst *MI, unsigned OpNum,
+				     const map_insn_ops *insn_ops_map,
+				     size_t map_size);
+
+/// Only usable by `auto-sync` archs!
+const cs_ac_type mapping_get_op_access(MCInst *MI, unsigned OpNum,
+				       const map_insn_ops *insn_ops_map,
+				       size_t map_size);
+
+/// Macro for easier access of operand types from the map.
+/// Assumes the istruction operands map is called "insn_operands"
+/// Only usable by `auto-sync` archs!
+#ifndef CAPSTONE_DIET
+#define map_get_op_type(MI, OpNum) \
+	mapping_get_op_type(MI, OpNum, (const map_insn_ops *)insn_operands, \
+			    sizeof(insn_operands) / sizeof(insn_operands[0]))
+#else
+#define map_get_op_type(MI, OpNum) \
+	CS_OP_INVALID
+#endif
+
+/// Macro for easier access of operand access flags from the map.
+/// Assumes the istruction operands map is called "insn_operands"
+/// Only usable by `auto-sync` archs!
+#ifndef CAPSTONE_DIET
+#define map_get_op_access(MI, OpNum) \
+	mapping_get_op_access(MI, OpNum, (const map_insn_ops *)insn_operands, \
+			      sizeof(insn_operands) / \
+				      sizeof(insn_operands[0]))
+#else
+#define map_get_op_access(MI, OpNum) \
+	CS_AC_INVALID
+#endif
+
+///< Map for ids to their string
+typedef struct name_map {
+	unsigned int id;
+	const char *name;
+} name_map;
+
+// map a name to its ID
+// return 0 if not found
+int name2id(const name_map *map, int max, const char *name);
+
+// map ID to a name
+// return NULL if not found
+const char *id2name(const name_map *map, int max, const unsigned int id);
+
+void map_add_implicit_write(MCInst *MI, uint32_t Reg);
+void map_add_implicit_read(MCInst *MI, uint32_t Reg);
+void map_remove_implicit_write(MCInst *MI, uint32_t Reg);
+
+void map_implicit_reads(MCInst *MI, const insn_map *imap);
+
+void map_implicit_writes(MCInst *MI, const insn_map *imap);
+
+void add_group(MCInst *MI, unsigned /* arch_group */ group);
+
+void map_groups(MCInst *MI, const insn_map *imap);
+
+void map_cs_id(MCInst *MI, const insn_map *imap, unsigned int imap_size);
+
+const void *map_get_suppl_info(MCInst *MI, const insn_map *imap);
+
+#define DECL_get_detail_op(arch, ARCH) \
+	cs_##arch##_op *ARCH##_get_detail_op(MCInst *MI, int offset);
+
+DECL_get_detail_op(arm, ARM);
+DECL_get_detail_op(ppc, PPC);
+DECL_get_detail_op(tricore, TriCore);
+DECL_get_detail_op(aarch64, AArch64);
+DECL_get_detail_op(alpha, Alpha);
+DECL_get_detail_op(hppa, HPPA);
+DECL_get_detail_op(loongarch, LoongArch);
+DECL_get_detail_op(mips, Mips);
+DECL_get_detail_op(riscv, RISCV);
+DECL_get_detail_op(systemz, SystemZ);
+DECL_get_detail_op(xtensa, Xtensa);
+
+/// Increments the detail->arch.op_count by one.
+#define DEFINE_inc_detail_op_count(arch, ARCH) \
+	static inline void ARCH##_inc_op_count(MCInst *MI) \
+	{ \
+		MI->flat_insn->detail->arch.op_count++; \
+	}
+
+/// Decrements the detail->arch.op_count by one.
+#define DEFINE_dec_detail_op_count(arch, ARCH) \
+	static inline void ARCH##_dec_op_count(MCInst *MI) \
+	{ \
+		MI->flat_insn->detail->arch.op_count--; \
+	}
+
+DEFINE_inc_detail_op_count(arm, ARM);
+DEFINE_dec_detail_op_count(arm, ARM);
+DEFINE_inc_detail_op_count(ppc, PPC);
+DEFINE_dec_detail_op_count(ppc, PPC);
+DEFINE_inc_detail_op_count(tricore, TriCore);
+DEFINE_dec_detail_op_count(tricore, TriCore);
+DEFINE_inc_detail_op_count(aarch64, AArch64);
+DEFINE_dec_detail_op_count(aarch64, AArch64);
+DEFINE_inc_detail_op_count(alpha, Alpha);
+DEFINE_dec_detail_op_count(alpha, Alpha);
+DEFINE_inc_detail_op_count(hppa, HPPA);
+DEFINE_dec_detail_op_count(hppa, HPPA);
+DEFINE_inc_detail_op_count(loongarch, LoongArch);
+DEFINE_dec_detail_op_count(loongarch, LoongArch);
+DEFINE_inc_detail_op_count(mips, Mips);
+DEFINE_dec_detail_op_count(mips, Mips);
+DEFINE_inc_detail_op_count(riscv, RISCV);
+DEFINE_dec_detail_op_count(riscv, RISCV);
+DEFINE_inc_detail_op_count(systemz, SystemZ);
+DEFINE_dec_detail_op_count(systemz, SystemZ);
+DEFINE_inc_detail_op_count(xtensa, Xtensa);
+DEFINE_dec_detail_op_count(xtensa, Xtensa);
+
+/// Returns true if a memory operand is currently edited.
+static inline bool doing_mem(const MCInst *MI)
+{
+	return MI->csh->doing_mem;
+}
+
+/// Sets the doing_mem flag to @status.
+static inline void set_doing_mem(const MCInst *MI, bool status)
+{
+	MI->csh->doing_mem = status;
+}
+
+/// Returns detail->arch
+#define DEFINE_get_arch_detail(arch, ARCH) \
+	static inline cs_##arch *ARCH##_get_detail(const MCInst *MI) \
+	{ \
+		assert(MI && MI->flat_insn && MI->flat_insn->detail); \
+		return &MI->flat_insn->detail->arch; \
+	}
+
+DEFINE_get_arch_detail(arm, ARM);
+DEFINE_get_arch_detail(ppc, PPC);
+DEFINE_get_arch_detail(tricore, TriCore);
+DEFINE_get_arch_detail(aarch64, AArch64);
+DEFINE_get_arch_detail(alpha, Alpha);
+DEFINE_get_arch_detail(hppa, HPPA);
+DEFINE_get_arch_detail(loongarch, LoongArch);
+DEFINE_get_arch_detail(mips, Mips);
+DEFINE_get_arch_detail(riscv, RISCV);
+DEFINE_get_arch_detail(systemz, SystemZ);
+DEFINE_get_arch_detail(xtensa, Xtensa);
+
+#define DEFINE_check_safe_inc(Arch, ARCH) \
+	static inline void Arch##_check_safe_inc(const MCInst *MI) { \
+		assert(Arch##_get_detail(MI)->op_count + 1 < NUM_##ARCH##_OPS); \
+	}
+
+DEFINE_check_safe_inc(ARM, ARM);
+DEFINE_check_safe_inc(PPC, PPC);
+DEFINE_check_safe_inc(TriCore, TRICORE);
+DEFINE_check_safe_inc(AArch64, AARCH64);
+DEFINE_check_safe_inc(Alpha, ALPHA);
+DEFINE_check_safe_inc(HPPA, HPPA);
+DEFINE_check_safe_inc(LoongArch, LOONGARCH);
+DEFINE_check_safe_inc(RISCV, RISCV);
+DEFINE_check_safe_inc(SystemZ, SYSTEMZ);
+DEFINE_check_safe_inc(Mips, MIPS);
+
+static inline bool detail_is_set(const MCInst *MI)
+{
+	assert(MI && MI->flat_insn);
+	return MI->flat_insn->detail != NULL && MI->csh->detail_opt & CS_OPT_ON;
+}
+
+static inline cs_detail *get_detail(const MCInst *MI)
+{
+	assert(MI && MI->flat_insn);
+	return MI->flat_insn->detail;
+}
+
+/// Returns if the given instruction is an alias instruction.
+#define RETURN_IF_INSN_IS_ALIAS(MI) \
+do { \
+	if (MI->isAliasInstr) \
+		return; \
+} while(0)
+
+void map_set_fill_detail_ops(MCInst *MI, bool Val);
+
+static inline bool map_fill_detail_ops(MCInst *MI) {
+	assert(MI);
+	return MI->fillDetailOps;
+}
+
+void map_set_is_alias_insn(MCInst *MI, bool Val, uint64_t Alias);
+
+bool map_use_alias_details(const MCInst *MI);
+
+void map_set_alias_id(MCInst *MI, const SStream *O, const name_map *alias_mnem_id_map, int map_size);
+
+/// Mapping from Capstone enumeration identifiers and their values.
+///
+/// This map MUST BE sorted to allow binary searches.
+/// Please always ensure the map is sorted after you added a value.
+///
+/// You can sort the map with Python.
+/// Copy the map into a file and run:
+///
+/// ```python
+/// with open("/tmp/file_with_map_entries") as f:
+///     text = f.readlines()
+///
+/// text.sort()
+/// print(''.join(text))
+/// ```
+typedef struct {
+	const char *str; ///< The name of the enumeration identifier
+	uint64_t val;	 ///< The value of the identifier
+} cs_enum_id_map;
+
+uint64_t enum_map_bin_search(const cs_enum_id_map *map, size_t map_len,
+			     const char *id, bool *found);
+
+#endif // CS_MAPPING_H
---
+++ Source/ThirdParty/capstone/cs_priv.h
@@ -0,0 +1,141 @@
+/* Capstone Disassembly Engine */
+/* By Nguyen Anh Quynh <aquynh@gmail.com>, 2013-2019 */
+
+#ifndef CS_PRIV_H
+#define CS_PRIV_H
+
+#ifdef CAPSTONE_DEBUG
+#include <assert.h>
+#endif
+#include <capstone/capstone.h>
+
+#include "MCInst.h"
+#include "SStream.h"
+
+typedef void (*Printer_t)(MCInst *MI, SStream *OS, void *info);
+
+// function to be called after Printer_t
+// this is the best time to gather insn's characteristics
+typedef void (*PostPrinter_t)(csh handle, cs_insn *, SStream *mnem, MCInst *mci);
+
+typedef bool (*Disasm_t)(csh handle, const uint8_t *code, size_t code_len, MCInst *instr, uint16_t *size, uint64_t address, void *info);
+
+typedef const char *(*GetName_t)(csh handle, unsigned int id);
+
+typedef void (*GetID_t)(cs_struct *h, cs_insn *insn, unsigned int id);
+
+// return registers accessed by instruction
+typedef void (*GetRegisterAccess_t)(const cs_insn *insn,
+		cs_regs regs_read, uint8_t *regs_read_count,
+		cs_regs regs_write, uint8_t *regs_write_count);
+
+// for ARM only
+typedef struct ARM_ITBlock {
+	unsigned char ITStates[8];
+	unsigned int size;
+} ARM_ITBlock;
+
+typedef struct ARM_VPTBlock {
+	unsigned char VPTStates[8];
+	unsigned int size;
+} ARM_VPTBlock;
+
+// Customize mnemonic for instructions with alternative name.
+struct customized_mnem {
+	// ID of instruction to be customized.
+	unsigned int id;
+	// Customized instruction mnemonic.
+	char mnemonic[CS_MNEMONIC_SIZE];
+};
+
+struct insn_mnem {
+	struct customized_mnem insn;
+	struct insn_mnem *next;	// linked list of customized mnemonics
+};
+
+struct cs_struct {
+	cs_arch arch;
+	cs_mode mode;
+	Printer_t printer;	// asm printer
+	void *printer_info; // aux info for printer
+	Disasm_t disasm;	// disassembler
+	void *getinsn_info; // auxiliary info for printer
+	GetName_t reg_name;
+	GetName_t insn_name;
+	GetName_t group_name;
+	GetID_t insn_id;
+	PostPrinter_t post_printer;
+	cs_err errnum;
+	ARM_ITBlock ITBlock;	// for Arm only
+	ARM_VPTBlock VPTBlock;  // for ARM only
+	bool PrintBranchImmAsAddress;
+	bool ShowVSRNumsAsVR;
+	cs_opt_value detail_opt, imm_unsigned;
+	int syntax;	// asm syntax for simple printer such as ARM, Mips & PPC
+	bool doing_mem;	// handling memory operand in InstPrinter code
+	bool doing_SME_Index; // handling a SME instruction that has index
+	unsigned short *insn_cache;	// index caching for mapping.c
+	bool skipdata;	// set this to True if we skip data when disassembling
+	uint8_t skipdata_size;	// how many bytes to skip
+	cs_opt_skipdata skipdata_setup;	// user-defined skipdata setup
+	const uint8_t *regsize_map;	// map to register size (x86-only for now)
+	GetRegisterAccess_t reg_access;
+	struct insn_mnem *mnem_list;	// linked list of customized instruction mnemonic
+	uint32_t LITBASE;
+};
+
+#define MAX_ARCH CS_ARCH_MAX
+
+// Returns a bool (0 or 1) whether big endian is enabled for a mode
+#define MODE_IS_BIG_ENDIAN(mode) (((mode) & CS_MODE_BIG_ENDIAN) != 0)
+
+/// Returns true of the 16bit flag is set.
+#define IS_16BIT(mode) ((mode & CS_MODE_16) != 0)
+/// Returns true of the 32bit flag is set.
+#define IS_32BIT(mode) ((mode & CS_MODE_32) != 0)
+/// Returns true of the 64bit flag is set.
+#define IS_64BIT(mode) ((mode & CS_MODE_64) != 0)
+
+extern cs_malloc_t cs_mem_malloc;
+extern cs_calloc_t cs_mem_calloc;
+extern cs_realloc_t cs_mem_realloc;
+extern cs_free_t cs_mem_free;
+extern cs_vsnprintf_t cs_vsnprintf;
+
+/// By defining CAPSTONE_DEBUG assertions can be used.
+/// For the release build the @expr is not included.
+#ifdef CAPSTONE_DEBUG
+#define CS_ASSERT(expr) assert(expr)
+#else
+#define CS_ASSERT(expr)
+#endif
+
+/// If compiled in debug mode it will assert(@expr).
+/// In the release build it will check the @expr and return @val if false.
+#ifdef CAPSTONE_DEBUG
+#define CS_ASSERT_RET_VAL(expr, val) assert(expr)
+#else
+#define CS_ASSERT_RET_VAL(expr, val) \
+do { \
+	if (!(expr)) { \
+		fprintf(stderr, "Hit assert: " #expr "\n"); \
+		return val; \
+	} \
+} while(0)
+#endif
+
+/// If compiled in debug mode it will assert(@expr).
+/// In the release build it will check the @expr and return if false.
+#ifdef CAPSTONE_DEBUG
+#define CS_ASSERT_RET(expr) assert(expr)
+#else
+#define CS_ASSERT_RET(expr) \
+do { \
+	if (!(expr)) { \
+		fprintf(stderr, "Hit assert: " #expr "\n"); \
+		return; \
+	} \
+} while(0)
+#endif
+
+#endif
