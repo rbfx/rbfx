d7fc8475e5a1f37cb2fe00816013a53c6ae68af8
Rokas Kupstys <rokups@zoho.com>
2025-10-19 11:12:09 +0300

    Misc: A workaround for a build failure with GCC15 in release builds.

--- Source/ThirdParty/tracy/cmake/server.cmake
+++ Source/ThirdParty/tracy/cmake/server.cmake
@@ -30,6 +30,13 @@ list(TRANSFORM TRACY_SERVER_SOURCES PREPEND "${TRACY_SERVER_DIR}/")
 add_library(TracyServer STATIC EXCLUDE_FROM_ALL ${TRACY_COMMON_SOURCES} ${TRACY_SERVER_SOURCES})
 target_include_directories(TracyServer PUBLIC ${TRACY_COMMON_DIR} ${TRACY_SERVER_DIR})
 target_link_libraries(TracyServer PUBLIC TracyCapstone libzstd PPQSort::PPQSort)
+
+# Fix GCC 15+ inlining issues with xxHash SSE2 intrinsics in tracy_xxhash.h
+# GCC 15 fails to inline XXH3_accumulate_sse2/XXH3_scrambleAcc_sse2 with always_inline attribute
+if(CMAKE_CXX_COMPILER_ID STREQUAL "GNU" AND CMAKE_CXX_COMPILER_VERSION VERSION_GREATER_EQUAL "15.0")
+    target_compile_definitions(TracyServer PUBLIC XXH_NO_INLINE_HINTS=1)
+endif()
+
 if(NO_STATISTICS)
     target_compile_definitions(TracyServer PUBLIC TRACY_NO_STATISTICS)
 endif()
