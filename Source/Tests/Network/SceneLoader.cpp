9a3803af780c7ae8c1b9bf293b72f0b28cdf2801
Eugene Kozlov <nekoeuge@gmail.com>
2026-09-13 21:01:32 +0200

    Compact license comments in files.

--- Source/Tests/Network/SceneLoader.cpp
+++ Source/Tests/Network/SceneLoader.cpp
@@ -1,8 +1,6 @@
-//
 // Copyright (c) 2026-2026 the rbfx project.
 // This work is licensed under the terms of the MIT license.
 // For a copy, see <https://opensource.org/licenses/MIT> or the accompanying LICENSE file.
-//
 
 #include "../CommonUtils.h"
 #include "../NetworkUtils.h"
